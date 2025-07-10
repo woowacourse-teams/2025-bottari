d1730639ba4bcfed08f8dd4f7ac5660fc263d8a1
Yehyeok Bang <qkddpgur318@gmail.com>
2025-07-10 17:40:39 +0900

    init: 백엔드 프로젝트 초기 설정

---
+++ backend/bottari/src/main/java/com/bottari/BottariApplication.java
@@ -0,0 +1,12 @@
+package com.bottari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BottariApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(BottariApplication.class, args);
+    }
+}
---
+++ backend/bottari/src/test/java/com/bottari/BottariApplicationTests.java
@@ -0,0 +1,12 @@
+package com.bottari;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BottariApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
