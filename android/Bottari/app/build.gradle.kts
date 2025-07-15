0238c65848905f1377d370e55f55d4bf3c2400ba
cucumber99 <dlsguq1@gmail.com>
2025-07-15 21:04:19 +0900

    build: App 모듈 테스트 관련 라이브러리 의존성 추가
    - JUnit5, kotest, MockK

--- android/Bottari/app/build.gradle.kts
+++ android/Bottari/app/build.gradle.kts
@@ -44,7 +44,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    testImplementation(libs.junit)
+    testImplementation(libs.bundles.test)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
