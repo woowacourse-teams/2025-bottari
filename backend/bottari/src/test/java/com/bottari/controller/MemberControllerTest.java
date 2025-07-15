3bec1185221982a079988b563d01d6eae5644039
Yehyeok Bang <qkddpgur318@gmail.com>
2025-07-15 14:00:26 +0900

    feat: 사용자 생성 API 구현
    Co-authored-by: Sung-june27 <kmymy27@gmail.com>
    Co-authored-by: unh6unh6 <ruddbs3308@naver.com>
    Co-authored-by: jaehyeon2650 <ind07152@naver.com>

---
+++ backend/bottari/src/test/java/com/bottari/controller/MemberControllerTest.java
@@ -0,0 +1,47 @@
+package com.bottari.controller;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.bottari.dto.CreateMemberRequest;
+import com.bottari.service.MemberService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(MemberController.class)
+class MemberControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private MemberService memberService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @DisplayName("멤버를 생성한다")
+    @Test
+    void register() throws Exception {
+        // given
+        final CreateMemberRequest request = new CreateMemberRequest("ssaid", "name");
+        given(memberService.create(request))
+                .willReturn(1L);
+
+        // when & then
+        mockMvc.perform(post("/members")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isCreated())
+                .andExpect(header().string(HttpHeaders.LOCATION, "/members/1"));
+    }
+}
