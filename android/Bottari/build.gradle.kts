76b59a5e69851e3fbf24724329ae75653a13979a
moondev03 <moondev03@gmail.com>
2025-07-15 18:21:37 +0900

    feat: ktlint 설정
    - ktlint 플러그인을 Gradle Version Catalogs에 추가하고 모든 프로젝트에 적용되도록 설정

--- android/Bottari/build.gradle.kts
+++ android/Bottari/build.gradle.kts
@@ -4,4 +4,9 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.jetbrains.kotlin.jvm) apply false
+    alias(libs.plugins.ktlint) apply false
+}
+
+allprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
 }
