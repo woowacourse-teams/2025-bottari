ca70aeed4f62010e077374ab33e890a67e41fef8
Sia <lyl8661@naver.com>
2025-07-16 16:33:54 +0900

    feat: Home 화면 BottomNavigation 구현
    Co-authored-by: cucumber99 <dlsguq1@gmail.com>
    Co-authored-by: moondev03 <moondev03@gmail.com>

--- android/Bottari/presentation/src/main/java/com/bottari/presentation/view/home/HomeActivity.kt
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/view/home/HomeActivity.kt
@@ -1,11 +1,6 @@
 package com.bottari.presentation.view.home
 
-import android.os.Bundle
 import com.bottari.presentation.base.BaseActivity
 import com.bottari.presentation.databinding.ActivityHomeBinding
 
-class HomeActivity : BaseActivity<ActivityHomeBinding>(ActivityHomeBinding::inflate) {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-}
+class HomeActivity : BaseActivity<ActivityHomeBinding>(ActivityHomeBinding::inflate)
