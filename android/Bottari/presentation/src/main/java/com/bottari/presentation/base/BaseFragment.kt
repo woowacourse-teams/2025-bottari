fea45c066bae5da4f8fb6bd8972f2d507c591977
cucumber99 <dlsguq1@gmail.com>
2025-07-16 11:21:08 +0900

    feat: BaseFragment 작성
    Co-authored-by: moondev03 <moondev03@gmail.com>
    Co-authored-by: Leeyerin0210 <lyl8661@naver.com>

---
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/base/BaseFragment.kt
@@ -0,0 +1,35 @@
+package com.bottari.presentation.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.StringRes
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+import com.google.android.material.snackbar.Snackbar
+
+abstract class BaseFragment<VB : ViewBinding> : Fragment() {
+    private var _binding: VB? = null
+
+    protected val binding: VB
+        get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = inflate()
+        return binding.root
+    }
+
+    protected abstract fun inflate(): VB
+
+    protected fun showSnackbar(
+        @StringRes message: Int,
+        duration: Int = Snackbar.LENGTH_SHORT,
+    ) {
+        view?.let { Snackbar.make(it, message, duration).show() }
+    }
+}
