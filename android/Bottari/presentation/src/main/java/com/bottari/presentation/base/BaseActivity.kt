ef4be29322be270f28c7e8131fcba5bed2e6ed6c
cucumber99 <dlsguq1@gmail.com>
2025-07-16 13:34:48 +0900

    refactor: 바인딩 객체 초기화 방식 변경
    Co-authored-by: moondev03 <moondev03@gmail.com>
    Co-authored-by: Leeyerin0210 <lyl8661@naver.com>

--- android/Bottari/presentation/src/main/java/com/bottari/presentation/base/BaseActivity.kt
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/base/BaseActivity.kt
@@ -1,24 +1,25 @@
 package com.bottari.presentation.base
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.viewbinding.ViewBinding
 
-abstract class BaseActivity<VB : ViewBinding> : AppCompatActivity() {
+abstract class BaseActivity<VB : ViewBinding>(
+    private val bindingFactory: (LayoutInflater) -> VB,
+) : AppCompatActivity() {
     protected lateinit var binding: VB
         private set
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = inflate()
+        binding = bindingFactory(layoutInflater)
         setContentView(binding.root)
         setWindowInsets()
     }
-
-    protected abstract fun inflate(): VB
 
     private fun setWindowInsets() {
         enableEdgeToEdge()
--- android/Bottari/presentation/src/main/java/com/bottari/presentation/base/BaseFragment.kt
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/base/BaseFragment.kt
@@ -9,19 +9,18 @@ import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
 import com.google.android.material.snackbar.Snackbar
 
-abstract class BaseFragment<VB : ViewBinding> : Fragment() {
-    @Suppress("ktlint:standard:backing-property-naming")
+abstract class BaseFragment<VB : ViewBinding>(
+    private val bindingFactory: (LayoutInflater, ViewGroup?, Boolean) -> VB,
+) : Fragment() {
     private var _binding: VB? = null
-
-    protected val binding: VB
-        get() = _binding!!
+    val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
-        _binding = inflate()
+        _binding = bindingFactory(inflater, container, false)
         return binding.root
     }
 
@@ -29,8 +28,6 @@ abstract class BaseFragment<VB : ViewBinding> : Fragment() {
         super.onDestroyView()
         _binding = null
     }
-
-    protected abstract fun inflate(): VB
 
     protected fun showSnackbar(
         @StringRes message: Int,
