2180913f820f96e83583fc8ccad39dea68f4ed8a
Sia <lyl8661@naver.com>
2025-07-16 14:13:30 +0900

    feat: 홈화면 관련 파일 추가
    Co-authored-by: cucumber99 <dlsguq1@gmail.com>
    Co-authored-by: moondev03 <moondev03@gmail.com>

---
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/view/home/market/MarketFragment.kt
@@ -0,0 +1,6 @@
+package com.bottari.presentation.view.home.market
+
+import com.bottari.presentation.base.BaseFragment
+import com.bottari.presentation.databinding.FragmentMarketBinding
+
+class MarketFragment : BaseFragment<FragmentMarketBinding>(FragmentMarketBinding::inflate)
---
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/view/home/profile/ProfileFragment.kt
@@ -0,0 +1,6 @@
+package com.bottari.presentation.view.home.profile
+
+import com.bottari.presentation.base.BaseFragment
+import com.bottari.presentation.databinding.FragmentProfileBinding
+
+class ProfileFragment : BaseFragment<FragmentProfileBinding>(FragmentProfileBinding::inflate)
