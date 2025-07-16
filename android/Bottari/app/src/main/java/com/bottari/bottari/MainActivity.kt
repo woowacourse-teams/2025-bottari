364dfd6348c061fcb57dfbb562ca65c76b5ec9c6
Sia <lyl8661@naver.com>
2025-07-16 16:33:30 +0900

    refactor: MainActivity 상속 및 화면 전환 구현
    Co-authored-by: cucumber99 <dlsguq1@gmail.com>
    Co-authored-by: moondev03 <moondev03@gmail.com>

--- android/Bottari/app/src/main/java/com/bottari/bottari/MainActivity.kt
+++ android/Bottari/app/src/main/java/com/bottari/bottari/MainActivity.kt
@@ -1,20 +1,16 @@
 package com.bottari.bottari
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.bottari.bottari.databinding.ActivityMainBinding
+import com.bottari.presentation.base.BaseActivity
+import com.bottari.presentation.view.home.HomeActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
     }
 }
