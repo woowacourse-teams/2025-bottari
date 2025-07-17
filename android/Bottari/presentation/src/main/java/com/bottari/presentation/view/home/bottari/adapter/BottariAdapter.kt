d14f0bf66d996d9b3f7a3c18b83358a727c02c19
cucumber99 <dlsguq1@gmail.com>
2025-07-17 13:39:03 +0900

    feat: 보따리 화면 어댑터 구현
    Co-authored-by: moondev03 <moondev03@gmail.com>
    Co-authored-by: Leeyerin0210 <lyl8661@naver.com>

---
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/view/home/bottari/adapter/BottariAdapter.kt
@@ -0,0 +1,35 @@
+package com.bottari.presentation.view.home.bottari.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import com.bottari.presentation.model.BottariUiModel
+
+class BottariAdapter : ListAdapter<BottariUiModel, BottariViewHolder>(DiffUtil) {
+    override fun onBindViewHolder(
+        holder: BottariViewHolder,
+        position: Int,
+    ) {
+        holder.bind(getItem(position))
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int,
+    ): BottariViewHolder = BottariViewHolder.from(parent)
+
+    companion object {
+        private val DiffUtil =
+            object : DiffUtil.ItemCallback<BottariUiModel>() {
+                override fun areContentsTheSame(
+                    oldItem: BottariUiModel,
+                    newItem: BottariUiModel,
+                ): Boolean = oldItem == newItem
+
+                override fun areItemsTheSame(
+                    oldItem: BottariUiModel,
+                    newItem: BottariUiModel,
+                ): Boolean = oldItem.title == newItem.title
+            }
+    }
+}
