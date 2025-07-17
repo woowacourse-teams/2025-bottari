aa35f7d47c550b9b4d92335e9aa39a1a94e75d85
민경윤 <ruddbs3308@naver.com>
2025-07-17 17:31:04 +0900

    test: 보따리 상세 조회 검증 로직 테스트
    Co-authored-by: yehyeokbang <qkddpgur318@gmail.com>
    Co-authored-by: Sung-june27 <kmymy27@gmail.com>
    Co-authored-by: jaehyeon2650 <ind07152@naver.com>

--- backend/bottari/src/test/java/com/bottari/domain/MemberTest.java
+++ backend/bottari/src/test/java/com/bottari/domain/MemberTest.java
@@ -1,9 +1,11 @@
 package com.bottari.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class MemberTest {
@@ -16,5 +18,25 @@ class MemberTest {
         assertThatThrownBy(() -> new Member("ssaid", name))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("사용자 이름은 3글자 이상 10글자 이하여야 합니다.");
+    }
+
+    @DisplayName("같은 ssaid인지 비교한다.")
+    @ParameterizedTest
+    @CsvSource({
+            "same_ssaid, true",
+            "diff_ssaid, false"
+    })
+    void isSameBySsaid(
+            final String ssaid,
+            final boolean expected
+    ) {
+        // given
+        final Member member = new Member("same_ssaid", "name");
+
+        // when
+        final boolean actual = member.isSameBySsaid(ssaid);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
     }
 }
