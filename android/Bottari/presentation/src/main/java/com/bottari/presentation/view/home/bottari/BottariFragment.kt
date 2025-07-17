c7209291026590187abc87a31f711aafdf9b8631
cucumber99 <dlsguq1@gmail.com>
2025-07-17 13:39:56 +0900

    feat: 보따리 화면 초기 구성
    Co-authored-by: moondev03 <moondev03@gmail.com>
    Co-authored-by: Leeyerin0210 <lyl8661@naver.com>

--- android/Bottari/presentation/src/main/java/com/bottari/presentation/view/home/bottari/BottariFragment.kt
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/view/home/bottari/BottariFragment.kt
@@ -1,6 +1,43 @@
 package com.bottari.presentation.view.home.bottari
 
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bottari.presentation.R
 import com.bottari.presentation.base.BaseFragment
+import com.bottari.presentation.base.UiState
 import com.bottari.presentation.databinding.FragmentBottariBinding
+import com.bottari.presentation.model.BottariUiModel
+import com.bottari.presentation.view.home.bottari.adapter.BottariAdapter
 
-class BottariFragment : BaseFragment<FragmentBottariBinding>(FragmentBottariBinding::inflate)
+class BottariFragment : BaseFragment<FragmentBottariBinding>(FragmentBottariBinding::inflate) {
+    private val viewModel: BottariViewModel by viewModels()
+    private val adapter: BottariAdapter by lazy { BottariAdapter() }
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+        setupObserver()
+        setupUI()
+    }
+
+    private fun setupObserver() {
+        viewModel.bottaries.observe(viewLifecycleOwner) { uiState -> handleBottariState(uiState) }
+    }
+
+    private fun setupUI() {
+        binding.rvBottari.adapter = adapter
+        binding.rvBottari.layoutManager = LinearLayoutManager(requireContext())
+    }
+
+    private fun handleBottariState(uiState: UiState<List<BottariUiModel>>) {
+        when (uiState) {
+            is UiState.Loading -> showSnackbar(R.string.home_nav_market_title)
+            is UiState.Success -> adapter.submitList(uiState.data)
+            is UiState.Failure -> showSnackbar(R.string.home_nav_profile_title)
+        }
+    }
+}
