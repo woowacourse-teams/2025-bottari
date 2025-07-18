a176296b8d7c017150d9ffd881bcf904c0217567
sung-june27 <kmymy27@gmail.com>
2025-07-18 17:54:54 +0900

    feat: 보따리 물품 일괄 추가 및 삭제 기능 구현
    Co-authored-by: jaehyeon2650 <ind07152@naver.com>

--- backend/bottari/src/test/java/com/bottari/service/BottariItemServiceTest.java
+++ backend/bottari/src/test/java/com/bottari/service/BottariItemServiceTest.java
@@ -8,6 +8,7 @@ import com.bottari.domain.Bottari;
 import com.bottari.domain.BottariItem;
 import com.bottari.domain.Member;
 import com.bottari.dto.CreateBottariItemRequest;
+import com.bottari.dto.EditBottariItemsRequest;
 import com.bottari.dto.ReadBottariItemResponse;
 import jakarta.persistence.EntityManager;
 import java.util.List;
@@ -121,6 +122,160 @@ class BottariItemServiceTest {
         assertThatThrownBy(() -> bottariItemService.create(bottari.getId(), request))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("중복된 보따리 물품명입니다.");
+    }
+
+    @DisplayName("보따리 물품을 수정한다.")
+    @Test
+    void update() {
+        // given
+        final Member member = new Member("ssaid", "name");
+        entityManager.persist(member);
+
+        final Bottari bottari = new Bottari("title", member);
+        entityManager.persist(bottari);
+
+        final BottariItem bottariItem_1 = new BottariItem("name_1", bottari);
+        final BottariItem bottariItem_2 = new BottariItem("name_2", bottari);
+        final BottariItem bottariItem_3 = new BottariItem("name_3", bottari);
+        entityManager.persist(bottariItem_1);
+        entityManager.persist(bottariItem_2);
+        entityManager.persist(bottariItem_3);
+
+        final Bottari anotherBottari = new Bottari("another_title", member);
+        entityManager.persist(anotherBottari);
+
+        final BottariItem anotherBottariItem = new BottariItem("another_name", anotherBottari);
+        entityManager.persist(anotherBottariItem);
+
+        final Long updateBottariId = bottari.getId();
+
+        final List<Long> deleteIds = List.of(bottariItem_1.getId(), bottariItem_2.getId());
+        final List<String> createNames = List.of("newName_1", "newName_2");
+        final EditBottariItemsRequest request = new EditBottariItemsRequest(deleteIds, createNames);
+
+        // when
+        bottariItemService.update(updateBottariId, request);
+
+        // then
+        final List<BottariItem> actual = entityManager.createQuery(
+                        "select i from BottariItem i where i.bottari.id = :bottariId", BottariItem.class)
+                .setParameter("bottariId", updateBottariId)
+                .getResultList();
+
+        final List<BottariItem> anotherActual = entityManager.createQuery(
+                        "select i from BottariItem i where i.bottari.id = :bottariId", BottariItem.class)
+                .setParameter("bottariId", anotherBottari.getId())
+                .getResultList();
+
+        assertAll(() -> {
+            assertThat(actual).hasSize(3);
+            assertThat(actual.stream().map(BottariItem::getName).toList()).contains("name_3", "newName_1", "newName_2");
+            assertThat(anotherActual).hasSize(1);
+            assertThat(anotherActual.getFirst().getName()).isEqualTo("another_name");
+        });
+    }
+
+    @DisplayName("수정 시 보따리가 존재하지 않는 경우, 예외를 던진다.")
+    @Test
+    void update_Exception_NotFoundBottari() {
+        // given
+        final Long invalidBottariId = -1L;
+        final EditBottariItemsRequest request = new EditBottariItemsRequest(List.of(), List.of());
+
+        // when & then
+        assertThatThrownBy(() -> bottariItemService.update(invalidBottariId, request))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("보따리를 찾을 수 없습니다.");
+    }
+
+    @DisplayName("수정 시 삭제하려는 물품이 보따리 내에 존재하지 않는 경우, 예외를 던진다.")
+    @Test
+    void update_Exception_NotExistsItemInBottari() {
+        // given
+        final Member member = new Member("ssaid", "name");
+        entityManager.persist(member);
+
+        final Bottari bottari = new Bottari("title", member);
+        entityManager.persist(bottari);
+
+        final BottariItem bottariItem_1 = new BottariItem("name_1", bottari);
+        final BottariItem bottariItem_2 = new BottariItem("name_2", bottari);
+        final BottariItem bottariItem_3 = new BottariItem("name_3", bottari);
+        entityManager.persist(bottariItem_1);
+        entityManager.persist(bottariItem_2);
+        entityManager.persist(bottariItem_3);
+
+        final Long bottariId = bottari.getId();
+
+        final List<Long> deleteIds = List.of(
+                bottariItem_1.getId(),
+                bottariItem_2.getId(),
+                Long.MAX_VALUE
+        );
+
+        final EditBottariItemsRequest request = new EditBottariItemsRequest(deleteIds, List.of());
+
+        // when & then
+        assertThatThrownBy(() -> bottariItemService.update(bottariId, request))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("보따리 안에 없는 물품은 삭제할 수 없습니다.");
+    }
+
+    @DisplayName("수정 시 추가하려는 물품명에 중복이 존재하는 경우, 예외를 던진다.")
+    @Test
+    void update_Exception_DuplicateItemNameInRequest() {
+        // given
+        final Member member = new Member("ssaid", "name");
+        entityManager.persist(member);
+
+        final Bottari bottari = new Bottari("title", member);
+        entityManager.persist(bottari);
+
+        final Long bottariId = bottari.getId();
+
+        final List<String> duplicateCreateNames = List.of(
+                "newName",
+                "duplicate_name",
+                "duplicate_name"
+        );
+
+        final EditBottariItemsRequest request = new EditBottariItemsRequest(List.of(), duplicateCreateNames);
+
+        // when & then
+        assertThatThrownBy(() -> bottariItemService.update(bottariId, request))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("중복된 물품이 존재합니다.");
+    }
+
+    @DisplayName("수정 시 추가하려는 물품이 이미 존재하는 경우, 예외를 던진다.")
+    @Test
+    void update_Exception_AlreadyExistsItemName() {
+        // given
+        final Member member = new Member("ssaid", "name");
+        entityManager.persist(member);
+
+        final Bottari bottari = new Bottari("title", member);
+        entityManager.persist(bottari);
+
+        final String duplicateName = "duplicateName";
+        final BottariItem bottariItem = new BottariItem(duplicateName, bottari);
+        entityManager.persist(bottariItem);
+
+        final Long bottariId = bottari.getId();
+
+        final List<String> duplicateNameInDB = List.of(
+                "newName",
+                duplicateName
+        );
+        final EditBottariItemsRequest request = new EditBottariItemsRequest(
+                List.of(),
+                duplicateNameInDB
+        );
+
+        // when & then
+        assertThatThrownBy(() -> bottariItemService.update(bottariId, request))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("중복된 물품이 존재합니다.");
     }
 
     @DisplayName("보따리 물품을 삭제한다.")
