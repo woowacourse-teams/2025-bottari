9576f05020e209bb03f3b2a4cfd534f701e4546f
cucumber99 <dlsguq1@gmail.com>
2025-07-18 19:59:23 +0900

    feat: 보따리 생성 다이얼로그 구현
    Co-authored-by: moondev03 <moondev03@gmail.com>
    Co-authored-by: Leeyerin0210 <lyl8661@naver.com>

---
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/view/home/bottari/create/BottariCreateViewModel.kt
@@ -0,0 +1,8 @@
+package com.bottari.presentation.view.home.bottari.create
+
+import androidx.lifecycle.ViewModel
+
+class BottariCreateViewModel : ViewModel() {
+    fun createBottari(name: String) {
+    }
+}
