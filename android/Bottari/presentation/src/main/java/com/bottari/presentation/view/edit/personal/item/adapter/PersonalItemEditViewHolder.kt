c2af9589c07e898edefe7673f2d270007580a1d1
moondev03 <moondev03@gmail.com>
2025-07-19 19:02:50 +0900

    feat: 개인 보따리 물건 수정 화면 RecyclerView 어댑터 구현
    - 개인 소지품 목록을 표시하는 RecyclerView 어댑터 및 ViewHolder 구현
    - 소지품 항목을 표시하는 레이아웃 파일(item_edit_personal_item.xml) 추가
    - 소지품 삭제 버튼 클릭 시 이벤트 처리를 위한 리스너 인터페이스 정의

---
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/view/edit/personal/item/adapter/PersonalItemEditViewHolder.kt
@@ -0,0 +1,34 @@
+package com.bottari.presentation.view.edit.personal.item.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bottari.presentation.databinding.ItemEditPersonalItemBinding
+import com.bottari.presentation.model.ItemUiModel
+import com.bottari.presentation.view.edit.personal.item.listener.OnEditItemClickListener
+
+class PersonalItemEditViewHolder(
+    private val binding: ItemEditPersonalItemBinding,
+    listener: OnEditItemClickListener
+) : RecyclerView.ViewHolder(binding.root) {
+    private var itemId: Long? = null
+
+    init {
+        binding.btnPersonalItemDelete.setOnClickListener {
+            itemId?.let { listener.onClick(it) }
+        }
+    }
+
+    fun bind(item: ItemUiModel) {
+        itemId = item.id
+        binding.tvPersonalItemName.text = item.name
+    }
+
+    companion object {
+        fun from(parent: ViewGroup, listener: OnEditItemClickListener): PersonalItemEditViewHolder {
+            val layoutInflater = LayoutInflater.from(parent.context)
+            val binding = ItemEditPersonalItemBinding.inflate(layoutInflater, parent, false)
+            return PersonalItemEditViewHolder(binding, listener)
+        }
+    }
+}
