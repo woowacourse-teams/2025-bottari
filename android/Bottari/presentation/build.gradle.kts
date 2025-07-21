09d84baacc3e44c52bf785c1d7a0ca230246c21a
Sia <lyl8661@naver.com>
2025-07-21 14:14:14 +0900

    feat: Flexbox 라이브러리 추가
    Co-authored-by: cucumber99 <dlsguq1@gmail.com>
    Co-authored-by: moondev03 <moondev03@gmail.com>

--- android/Bottari/presentation/build.gradle.kts
+++ android/Bottari/presentation/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.material)
     implementation(libs.cardstackview)
+    implementation (libs.flexbox)
     testImplementation(libs.bundles.test)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
