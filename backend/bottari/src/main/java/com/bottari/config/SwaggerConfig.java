5328f61044070f78b66d0890a04aa9dd394d3f28
민경윤 <ruddbs3308@naver.com>
2025-07-21 17:38:25 +0900

    docs: Swagger Security Scheme 설정
    Co-authored-by: yehyeokbang <qkddpgur318@gmail.com>
    Co-authored-by: Sung-june27 <kmymy27@gmail.com>
    Co-authored-by: jaehyeon2650 <ind07152@naver.com>

---
+++ backend/bottari/src/main/java/com/bottari/config/SwaggerConfig.java
@@ -0,0 +1,46 @@
+package com.bottari.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(apiInfo())
+                .addSecurityItem(getSecurityRequirement())
+                .components(getComponents());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Bottari API docs")
+                .description("Bottari API docs")
+                .version("1.0.0");
+    }
+
+    private SecurityRequirement getSecurityRequirement() {
+        return new SecurityRequirement()
+                .addList("ssaid");
+    }
+
+    private Components getComponents() {
+        return new Components()
+                .addSecuritySchemes("ssaid", getSecurityScheme());
+    }
+
+    private SecurityScheme getSecurityScheme() {
+        return new SecurityScheme()
+                .type(SecurityScheme.Type.APIKEY)
+                .in(SecurityScheme.In.HEADER)
+                .name("ssaid")
+                .description("SSAID를 입력해주세요.");
+    }
+}
