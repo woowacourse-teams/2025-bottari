3e5fc218ea45f711e8ff632412435d2ba17673bc
moondev03 <moondev03@gmail.com>
2025-07-22 21:20:19 +0900

    feat: SSAID를 반환하는 확장 함수 추가

--- android/Bottari/presentation/src/main/java/com/bottari/presentation/extension/ContextExtension.kt
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/extension/ContextExtension.kt
@@ -1,5 +1,11 @@
 package com.bottari.presentation.extension
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.provider.Settings
 
 fun Context.dpToPx(dp: Int): Int = (dp * resources.displayMetrics.density).toInt()
+
+@SuppressLint("HardwareIds")
+fun Context.getSSAID(): String =
+    Settings.Secure.getString(contentResolver, Settings.Secure.ANDROID_ID)
