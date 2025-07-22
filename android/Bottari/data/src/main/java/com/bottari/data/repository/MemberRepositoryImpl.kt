18a57adf153c1d5a5fb40cb13a3583087542e815
moondev03 <moondev03@gmail.com>
2025-07-22 21:44:43 +0900

    feat: 유저 등록 Data Layer 구현
    - MemberRemoteDataSource 인터페이스 및 구현체 추가
    - MemberRepository 구현체 추가
    - MemberRepository 인터페이스 정의

---
+++ android/Bottari/data/src/main/java/com/bottari/data/repository/MemberRepositoryImpl.kt
@@ -0,0 +1,14 @@
+package com.bottari.data.repository
+
+import com.bottari.data.mapper.MemberMapper.toRequest
+import com.bottari.data.source.remote.MemberRemoteDataSource
+import com.bottari.domain.model.member.Member
+import com.bottari.domain.repository.MemberRepository
+
+class MemberRepositoryImpl(
+    private val memberRemoteDataSource: MemberRemoteDataSource,
+) : MemberRepository {
+    override suspend fun registerMember(member: Member): Result<Boolean> {
+        return memberRemoteDataSource.registerMember(member.toRequest()).map { true }
+    }
+}
