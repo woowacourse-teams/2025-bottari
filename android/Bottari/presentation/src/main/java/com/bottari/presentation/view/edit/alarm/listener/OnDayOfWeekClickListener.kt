ca197a6bd692b79d29fee0ce7def88dc7cbbf55b
cucumber99 <dlsguq1@gmail.com>
2025-07-22 13:18:45 +0900

    feat: 요일 클릭 리스너 구현

---
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/view/edit/alarm/listener/OnDayOfWeekClickListener.kt
@@ -0,0 +1,7 @@
+package com.bottari.presentation.view.edit.alarm.listener
+
+import com.bottari.presentation.model.DayOfWeekUiModel
+
+fun interface OnDayOfWeekClickListener {
+    fun onClick(dayOfWeek: DayOfWeekUiModel)
+}
