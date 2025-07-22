9bbb639a972dbe30231760c04a0872a731d49d41
민경윤 <ruddbs3308@naver.com>
2025-07-22 15:02:23 +0900

    feat: 보따리 템플릿으로 보따리 생성 기능 구현
    Co-authored-by: jaehyeon2650 <ind07152@naver.com>

--- backend/bottari/src/main/java/com/bottari/repository/BottariTemplateItemRepository.java
+++ backend/bottari/src/main/java/com/bottari/repository/BottariTemplateItemRepository.java
@@ -1,7 +1,10 @@
 package com.bottari.repository;
 
 import com.bottari.domain.BottariTemplateItem;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BottariTemplateItemRepository extends JpaRepository<BottariTemplateItem, Long> {
+
+    List<BottariTemplateItem> findAllByBottariTemplateId(final Long id);
 }
--- backend/bottari/src/main/java/com/bottari/service/BottariTemplateService.java
+++ backend/bottari/src/main/java/com/bottari/service/BottariTemplateService.java
@@ -1,9 +1,13 @@
 package com.bottari.service;
 
+import com.bottari.domain.Bottari;
+import com.bottari.domain.BottariItem;
 import com.bottari.domain.BottariTemplate;
 import com.bottari.domain.BottariTemplateItem;
 import com.bottari.domain.Member;
 import com.bottari.dto.CreateBottariTemplateRequest;
+import com.bottari.repository.BottariItemRepository;
+import com.bottari.repository.BottariRepository;
 import com.bottari.repository.BottariTemplateItemRepository;
 import com.bottari.repository.BottariTemplateRepository;
 import com.bottari.repository.MemberRepository;
@@ -20,6 +24,8 @@ public class BottariTemplateService {
 
     private final BottariTemplateRepository bottariTemplateRepository;
     private final BottariTemplateItemRepository bottariTemplateItemRepository;
+    private final BottariRepository bottariRepository;
+    private final BottariItemRepository bottariItemRepository;
     private final MemberRepository memberRepository;
 
     @Transactional
@@ -38,6 +44,26 @@ public class BottariTemplateService {
         bottariTemplateItemRepository.saveAll(bottariTemplateItems);
 
         return savedBottariTemplate.getId();
+    }
+
+    @Transactional
+    public Long createBottari(
+            final Long id,
+            final String ssaid
+    ) {
+        final BottariTemplate bottariTemplate = bottariTemplateRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 템플릿을 찾을 수 없습니다."));
+        final List<BottariTemplateItem> bottariTemplateItems = bottariTemplateItemRepository.findAllByBottariTemplateId(id);
+        final Member member = memberRepository.findBySsaid(ssaid)
+                .orElseThrow(() -> new IllegalArgumentException("해당 ssaid로 가입된 사용자가 없습니다."));
+        final Bottari bottari = new Bottari(bottariTemplate.getTitle(),member);
+        final Bottari savedBottari = bottariRepository.save(bottari);
+        final List<BottariItem> bottariItems = bottariTemplateItems.stream()
+                .map(item -> new BottariItem(item.getName(),bottari))
+                .toList();
+        bottariItemRepository.saveAll(bottariItems);
+
+        return savedBottari.getId();
     }
 
     private void validateDuplicateItemNames(final List<String> itemNames) {
