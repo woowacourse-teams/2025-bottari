375280ae274bd7539afc4f6f82860e377535b759
sung-june27 <kmymy27@gmail.com>
2025-07-22 15:34:15 +0900

    fix: 불필요한 어노테이션 제거
    Co-authored-by: yehyeokbang <qkddpgur318@gmail.com>

--- backend/bottari/src/main/java/com/bottari/BottariApplication.java
+++ backend/bottari/src/main/java/com/bottari/BottariApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@EnableJpaAuditing
 public class BottariApplication {
 
     public static void main(final String[] args) {
--- backend/bottari/src/main/java/com/bottari/domain/BottariTemplate.java
+++ backend/bottari/src/main/java/com/bottari/domain/BottariTemplate.java
@@ -15,8 +15,8 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-@EntityListeners(AuditingEntityListener.class)
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class BottariTemplate {
