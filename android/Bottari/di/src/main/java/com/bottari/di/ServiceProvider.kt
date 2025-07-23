f06f6a847b9e2f0f0e9f3e501ce2f22dc1f2b566
moondev03 <moondev03@gmail.com>
2025-07-23 17:32:41 +0900

    feat: BottariItemService Provider 추가

--- android/Bottari/di/src/main/java/com/bottari/di/ServiceProvider.kt
+++ android/Bottari/di/src/main/java/com/bottari/di/ServiceProvider.kt
@@ -1,10 +1,12 @@
 package com.bottari.di
 
 import com.bottari.data.network.RetrofitClient
+import com.bottari.data.service.BottariItemService
 import com.bottari.data.service.BottariService
 import com.bottari.data.service.MemberService
 
 object ServiceProvider {
     val memberService: MemberService by lazy { RetrofitClient.create() }
     val bottariService: BottariService by lazy { RetrofitClient.create() }
+    val bottariItemService: BottariItemService by lazy { RetrofitClient.create() }
 }
