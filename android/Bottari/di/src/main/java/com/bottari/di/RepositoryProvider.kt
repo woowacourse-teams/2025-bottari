4e4d7857ab2e55eb8a7c2a4e0f3dde14e85e44f4
moondev03 <moondev03@gmail.com>
2025-07-23 16:32:36 +0900

    feat: BottariRepository 구현

--- android/Bottari/di/src/main/java/com/bottari/di/RepositoryProvider.kt
+++ android/Bottari/di/src/main/java/com/bottari/di/RepositoryProvider.kt
@@ -1,7 +1,9 @@
 package com.bottari.di
 
+import com.bottari.data.repository.BottariRepositoryImpl
 import com.bottari.data.repository.MemberRepositoryImpl
 
 object RepositoryProvider {
     val memberRepository by lazy { MemberRepositoryImpl(DataSourceProvider.memberRemoteDataSource) }
+    val bottariRepository by lazy { BottariRepositoryImpl(DataSourceProvider.bottariRemoteDataSource) }
 }
