a368a3146d9903bd33a31d741fb4c928b00a5509
moondev03 <moondev03@gmail.com>
2025-07-23 10:32:51 +0900

    refactor: Member 도메인 모델 에러 메시지 상수명 변경
    - 회원 도메인 모델(`Member.kt`)의 에러 메시지 상수명을 더 명확하게 변경했습니다.
    - `ERR_MSG_SSAID_BLANK` -> `SSAID_BLANK_ERROR`
    - `ERR_MSG_NICKNAME_LENGTH` -> `NICKNAME_LENGTH_ERROR`

--- android/Bottari/domain/src/main/java/com/bottari/domain/model/member/Member.kt
+++ android/Bottari/domain/src/main/java/com/bottari/domain/model/member/Member.kt
@@ -5,15 +5,15 @@ data class Member(
     val nickname: String,
 ) {
     init {
-        require(ssaid.isNotBlank()) { ERR_MSG_SSAID_BLANK }
-        require(nickname.length in MIN_NICKNAME_LENGTH..MAX_NICKNAME_LENGTH) { ERR_MSG_NICKNAME_LENGTH }
+        require(ssaid.isNotBlank()) { SSAID_BLANK_ERROR }
+        require(nickname.length in MIN_NICKNAME_LENGTH..MAX_NICKNAME_LENGTH) { NICKNAME_LENGTH_ERROR }
     }
 
     companion object {
         private const val MIN_NICKNAME_LENGTH = 2
         private const val MAX_NICKNAME_LENGTH = 10
-        private const val ERR_MSG_SSAID_BLANK = "ssaid는 공백일 수 없습니다."
-        private const val ERR_MSG_NICKNAME_LENGTH =
+        private const val SSAID_BLANK_ERROR = "ssaid는 공백일 수 없습니다."
+        private const val NICKNAME_LENGTH_ERROR =
             "닉네임은 최소 $MIN_NICKNAME_LENGTH 최대 $MAX_NICKNAME_LENGTH 글자 가능합니다."
     }
 }
