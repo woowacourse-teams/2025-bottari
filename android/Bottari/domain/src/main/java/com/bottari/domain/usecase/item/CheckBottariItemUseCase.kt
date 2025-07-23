4113319a37bd2398f5147c53afb8b7650e042643
moondev03 <moondev03@gmail.com>
2025-07-23 17:45:52 +0900

    feat: 체크리스트 조회 및 체크 상태 변경 유스케이스 구현

---
+++ android/Bottari/domain/src/main/java/com/bottari/domain/usecase/item/CheckBottariItemUseCase.kt
@@ -0,0 +1,10 @@
+package com.bottari.domain.usecase.item
+
+import com.bottari.domain.repository.BottariItemRepository
+
+class CheckBottariItemUseCase(
+    private val bottariItemRepository: BottariItemRepository,
+) {
+    suspend operator fun invoke(ssaid: String, bottariItemId: Long): Result<Unit> =
+        bottariItemRepository.checkBottariItem(ssaid, bottariItemId)
+}
---
+++ android/Bottari/domain/src/main/java/com/bottari/domain/usecase/item/UnCheckBottariItemUseCase.kt
@@ -0,0 +1,10 @@
+package com.bottari.domain.usecase.item
+
+import com.bottari.domain.repository.BottariItemRepository
+
+class UnCheckBottariItemUseCase(
+    private val bottariItemRepository: BottariItemRepository,
+) {
+    suspend operator fun invoke(ssaid: String, bottariItemId: Long): Result<Unit> =
+        bottariItemRepository.uncheckBottariItem(ssaid, bottariItemId)
+}
