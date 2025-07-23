7034b579cb904d8696ca21dc985562e283296bc9
cucumber99 <dlsguq1@gmail.com>
2025-07-23 22:36:45 +0900

    feat: 템플릿 관련 DTO 구현

---
+++ android/Bottari/data/src/main/java/com/bottari/data/model/template/FetchBottariTemplateResponse.kt
@@ -0,0 +1,16 @@
+package com.bottari.data.model.template
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class FetchBottariTemplateResponse(
+    @SerialName("id")
+    val id: Long,
+    @SerialName("title")
+    val title: String,
+    @SerialName("items")
+    val items: List<BottariTemplateItemResponse>,
+    @SerialName("author")
+    val author: String,
+)
