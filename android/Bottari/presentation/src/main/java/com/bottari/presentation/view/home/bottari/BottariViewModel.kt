ea8f82ef699f5a858a0800acffc7e6214dcb6c45
moondev03 <moondev03@gmail.com>
2025-07-23 16:33:14 +0900

    feat: 보따리 조회 기능 구현
    - 보따리 조회 기능을 ViewModel에 구현
    - SSAID를 통해 해당 유저의 보따리를 조회하도록 수정
    - Fragment에서 ViewModelFactory를 통해 SSAID 전달
    - 보따리 목록 상태에 따라 UI 업데이트

--- android/Bottari/presentation/src/main/java/com/bottari/presentation/view/home/bottari/BottariViewModel.kt
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/view/home/bottari/BottariViewModel.kt
@@ -2,21 +2,58 @@ package com.bottari.presentation.view.home.bottari
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.bottari.di.UseCaseProvider
+import com.bottari.domain.usecase.bottari.FetchBottariesUseCase
 import com.bottari.presentation.base.UiState
+import com.bottari.presentation.mapper.BottariMapper.toUiModel
 import com.bottari.presentation.model.BottariUiModel
+import kotlinx.coroutines.launch
 
-class BottariViewModel : ViewModel() {
+class BottariViewModel(
+    stateHandle: SavedStateHandle,
+    private val fetchBottariesUseCase: FetchBottariesUseCase,
+) : ViewModel() {
     private val _bottaries: MutableLiveData<UiState<List<BottariUiModel>>> =
         MutableLiveData(UiState.Loading)
     val bottaries: LiveData<UiState<List<BottariUiModel>>> get() = _bottaries
 
     init {
-        fetchBottaries()
+        val ssaid = stateHandle.get<String>(EXTRA_SSAID) ?: error("SSAID를 확인할 수 없음")
+        fetchBottaries(ssaid)
     }
 
-    private fun fetchBottaries() {
-        val uiState = UiState.Success(emptyList<BottariUiModel>())
-        _bottaries.value = uiState
+    private fun fetchBottaries(ssaid: String) {
+        viewModelScope.launch {
+            fetchBottariesUseCase(ssaid)
+                .onSuccess { bottaries ->
+                    val bottariUiModels = bottaries.map { bottari -> bottari.toUiModel() }
+                    _bottaries.value = UiState.Success(bottariUiModels)
+                }
+                .onFailure {
+                    _bottaries.value = UiState.Failure(it.message)
+                }
+        }
     }
+
+    companion object {
+        private const val EXTRA_SSAID = "SSAID"
+
+        fun Factory(ssaid: String): ViewModelProvider.Factory =
+            viewModelFactory {
+                initializer {
+                    val stateHandle = createSavedStateHandle()
+                    stateHandle[EXTRA_SSAID] = ssaid
+                    BottariViewModel(stateHandle, UseCaseProvider.fetchBottariesUseCase)
+                }
+            }
+    }
+
 }
+
