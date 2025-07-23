4b4b4042efaca1fdcd26b44f5ea810ab55adb626
moondev03 <moondev03@gmail.com>
2025-07-23 22:05:03 +0900

    feat: 보따리 생성 API 추가

--- android/Bottari/data/src/main/java/com/bottari/data/service/BottariService.kt
+++ android/Bottari/data/src/main/java/com/bottari/data/service/BottariService.kt
@@ -1,13 +1,24 @@
 package com.bottari.data.service
 
+import com.bottari.data.model.bottari.CreateBottariRequest
 import com.bottari.data.model.bottari.FetchBottariesResponse
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.POST
 
 interface BottariService {
     @GET("/bottaries")
     suspend fun fetchBottaries(
         @Header("ssaid") ssaid: String,
     ): Response<List<FetchBottariesResponse>>
+
+    @POST("/bottaries")
+    suspend fun createBottari(
+        @Header("ssaid") ssaid: String,
+        @Body body: CreateBottariRequest,
+    ): Response<Unit>
 }
