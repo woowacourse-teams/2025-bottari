97a9f68c284214fe650d087a2691018bf27bed9e
moondev03 <moondev03@gmail.com>
2025-07-23 17:40:18 +0900

    feat: 보따리 아이템 Repository 및 Provider 추가

---
+++ android/Bottari/domain/src/main/java/com/bottari/domain/repository/BottariItemRepository.kt
@@ -0,0 +1,9 @@
+package com.bottari.domain.repository
+
+import com.bottari.domain.model.bottari.BottariItem
+
+interface BottariItemRepository {
+    suspend fun fetchChecklist(ssaid: String, bottariId: Long): Result<List<BottariItem>>
+    suspend fun uncheckBottariItem(ssaid: String, bottariItemId: Long): Result<Unit>
+    suspend fun checkBottariItem(ssaid: String, bottariItemId: Long): Result<Unit>
+}
