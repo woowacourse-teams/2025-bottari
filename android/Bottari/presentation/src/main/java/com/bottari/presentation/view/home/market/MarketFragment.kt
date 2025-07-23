1f6109e17125e088ff0dc649276113b37f057f66
cucumber99 <dlsguq1@gmail.com>
2025-07-23 22:35:15 +0900

    feat: 마켓 화면 구현

--- android/Bottari/presentation/src/main/java/com/bottari/presentation/view/home/market/MarketFragment.kt
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/view/home/market/MarketFragment.kt
@@ -1,6 +1,47 @@
 package com.bottari.presentation.view.home.market
 
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.bottari.presentation.base.BaseFragment
+import com.bottari.presentation.base.UiState
 import com.bottari.presentation.databinding.FragmentMarketBinding
+import com.bottari.presentation.model.BottariTemplateUiModel
+import com.bottari.presentation.view.home.market.adapter.MarketAdapter
 
-class MarketFragment : BaseFragment<FragmentMarketBinding>(FragmentMarketBinding::inflate)
+class MarketFragment : BaseFragment<FragmentMarketBinding>(FragmentMarketBinding::inflate) {
+    private val viewModel: MarketViewModel by viewModels {
+        MarketViewModel.Factory()
+    }
+    private val adapter: MarketAdapter by lazy { MarketAdapter(::navigateToBottariTemplateDetail) }
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+        setupObserver()
+        setupUI()
+    }
+
+    private fun setupObserver() {
+        viewModel.bottariTemplates.observe(viewLifecycleOwner, ::handleBottariTemplateState)
+    }
+
+    private fun setupUI() {
+        binding.rvBottariTemplate.adapter = adapter
+        binding.rvBottariTemplate.layoutManager = LinearLayoutManager(requireContext())
+    }
+
+    private fun handleBottariTemplateState(uiState: UiState<List<BottariTemplateUiModel>>) {
+        when (uiState) {
+            is UiState.Loading -> Unit
+            is UiState.Success -> adapter.submitList(uiState.data)
+            is UiState.Failure -> Unit
+        }
+    }
+
+    private fun navigateToBottariTemplateDetail(bottariTemplateId: Long) {
+    }
+}
