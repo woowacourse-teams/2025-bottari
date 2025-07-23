4cd0528e75a29cc563272a40ac93d59885d7fc51
cucumber99 <dlsguq1@gmail.com>
2025-07-23 22:36:24 +0900

    feat: 템플릿 관련 도메인 모델 구현

---
+++ android/Bottari/domain/src/main/java/com/bottari/domain/model/template/BottariTemplate.kt
@@ -0,0 +1,8 @@
+package com.bottari.domain.model.template
+
+data class BottariTemplate(
+    val id: Long,
+    val title: String,
+    val items: List<BottariTemplateItem>,
+    val author: String,
+)
