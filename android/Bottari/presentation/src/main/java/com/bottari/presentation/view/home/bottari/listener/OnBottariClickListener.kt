5378ca21ea5680f72d83cdbbf798e18292246d14
moondev03 <moondev03@gmail.com>
2025-07-23 19:07:36 +0900

    feat: 보따리 클릭 리스너 인터페이스 수정
    - 보따리 클릭 시 ID와 함께 제목도 전달하도록 `onClick` 메서드의 파라미터를 변경했습니다.

--- android/Bottari/presentation/src/main/java/com/bottari/presentation/view/home/bottari/listener/OnBottariClickListener.kt
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/view/home/bottari/listener/OnBottariClickListener.kt
@@ -1,7 +1,7 @@
 package com.bottari.presentation.view.home.bottari.listener
 
 interface OnBottariClickListener {
-    fun onClick(bottariId: Long)
+    fun onClick(bottariId: Long, bottariTitle: String)
 
     fun onMoreClick(bottariId: Long)
 }
