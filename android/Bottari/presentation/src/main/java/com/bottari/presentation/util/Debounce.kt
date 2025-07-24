6f0e2285a4fb3f8d329b887840b167f4a5e3f1ca
cucumber99 <dlsguq1@gmail.com>
2025-07-24 12:45:33 +0900

    feat: 디바운스 메소드 구현

---
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/util/Debounce.kt
@@ -0,0 +1,22 @@
+package com.bottari.presentation.util
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+fun <T> debounce(
+    timeMillis: Long,
+    coroutineScope: CoroutineScope,
+    block: (T) -> Unit,
+): (T) -> Unit {
+    var debounceJob: Job? = null
+    return { param: T ->
+        debounceJob?.cancel()
+        debounceJob =
+            coroutineScope.launch {
+                delay(timeMillis)
+                block(param)
+            }
+    }
+}
