e99ded3a7b83d83f408e23ffa8f99bf312b999d4
Sia <lyl8661@naver.com>
2025-07-24 13:52:07 +0900

    refactor: 중복 mapper 삭제 및 id null 주입 삭제

--- android/Bottari/data/src/main/java/com/bottari/data/mapper/BottariMapper.kt
+++ android/Bottari/data/src/main/java/com/bottari/data/mapper/BottariMapper.kt
@@ -1,15 +1,9 @@
 package com.bottari.data.mapper
 
 import com.bottari.data.mapper.AlarmMapper.toDomain
-import com.bottari.data.model.bottari.AlarmResponse
 import com.bottari.data.model.bottari.BottariResponse
 import com.bottari.data.model.bottari.FetchBottariesResponse
 import com.bottari.data.model.bottari.ItemResponse
-import com.bottari.data.model.bottari.LocationResponse
-import com.bottari.data.model.bottari.RoutineResponse
-import com.bottari.domain.model.alarm.Alarm
-import com.bottari.domain.model.alarm.AlarmType
-import com.bottari.domain.model.alarm.LocationAlarm
 import com.bottari.domain.model.bottari.Bottari
 import com.bottari.domain.model.bottari.BottariDetail
 import com.bottari.domain.model.bottari.BottariItem
@@ -25,33 +19,6 @@ object BottariMapper {
             title = title,
             totalQuantity = totalItemsCount,
         )
-
-    private fun AlarmResponse.toDomain(): Alarm? {
-        val alarmType = routine.toDomainType() ?: return null
-        return Alarm(
-            id = null,
-            isActive = isActive,
-            time = routine.time,
-            alarmType = alarmType,
-            location = location?.toDomain(),
-        )
-    }
-
-    private fun LocationResponse.toDomain(): LocationAlarm =
-        LocationAlarm(
-            isActive = isActive,
-            latitude = latitude,
-            longitude = longitude,
-            radius = radius,
-        )
-
-    private fun RoutineResponse.toDomainType(): AlarmType? =
-        when (type) {
-            "NON_REPEAT" -> date?.let { AlarmType.NonRepeat(it) }
-            "EVERY_DAY_REPEAT" -> AlarmType.EveryDayRepeat
-            "EVERY_WEEK_REPEAT" -> AlarmType.EveryWeekRepeat(dayOfWeeks)
-            else -> null
-        }
 
     fun BottariResponse.toDomain(): BottariDetail =
         BottariDetail(
