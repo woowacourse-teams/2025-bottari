23a347ade140fdf316303d8eec994a464d05f87f
moondev03 <moondev03@gmail.com>
2025-07-24 16:16:22 +0900

    feat: SaveBottariItemsRequest 추가

---
+++ android/Bottari/data/src/main/java/com/bottari/data/model/item/SaveBottariItemsRequest.kt
@@ -0,0 +1,12 @@
+package com.bottari.data.model.item
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class SaveBottariItemsRequest(
+    @SerialName("createItemNames")
+    val createItemNames: List<String>,
+    @SerialName("deleteItemIds")
+    val deleteItemIds: List<Int>,
+)
