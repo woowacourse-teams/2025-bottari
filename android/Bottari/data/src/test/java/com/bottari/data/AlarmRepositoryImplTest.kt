276c69c3171ff80c309450d9769d8275e79bb1af
Sia <lyl8661@naver.com>
2025-07-24 21:04:55 +0900

    style: Ktlint 적용

--- android/Bottari/data/src/test/java/com/bottari/data/AlarmRepositoryImplTest.kt
+++ android/Bottari/data/src/test/java/com/bottari/data/AlarmRepositoryImplTest.kt
@@ -3,19 +3,17 @@ package com.bottari.data
 import com.bottari.data.repository.AlarmRepositoryImpl
 import com.bottari.data.source.remote.AlarmRemoteDataSource
 import io.mockk.MockKAnnotations
-import io.mockk.mockk
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import org.junit.Before
-import org.junit.Test
-import kotlinx.coroutines.test.runTest
 import io.mockk.coEvery
 import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertTrue
-
+import org.junit.Before
+import org.junit.Test
 
 @ExperimentalCoroutinesApi
 class AlarmRepositoryImplTest {
-
     private val mockRemoteDataSource = mockk<AlarmRemoteDataSource>()
 
     private val repository = AlarmRepositoryImpl(mockRemoteDataSource)
@@ -26,37 +24,38 @@ class AlarmRepositoryImplTest {
     }
 
     @Test
-    fun `알람을 활성화 할 수 있다`() = runTest {
-        // given
-        coEvery {
-            mockRemoteDataSource.activeAlarmState(any(), any())
-        } returns Result.success(Unit)
+    fun `알람을 활성화 할 수 있다`() =
+        runTest {
+            // given
+            coEvery {
+                mockRemoteDataSource.activeAlarmState(any(), any())
+            } returns Result.success(Unit)
 
-        // when
-        val result = repository.activeAlarm("ssaid123", 1L)
+            // when
+            val result = repository.activeAlarm("ssaid123", 1L)
 
-        // then
-        assertTrue(result.isSuccess)
+            // then
+            assertTrue(result.isSuccess)
 
-        // verify
-        coVerify { mockRemoteDataSource.activeAlarmState(1L, "ssaid123") }
-    }
-
+            // verify
+            coVerify { mockRemoteDataSource.activeAlarmState(1L, "ssaid123") }
+        }
 
     @Test
-    fun `알람을 비활성화 할 수 있다`() = runTest {
-        // given
-        coEvery {
-            mockRemoteDataSource.inactiveAlarmState(any(), any())
-        } returns Result.success(Unit)
+    fun `알람을 비활성화 할 수 있다`() =
+        runTest {
+            // given
+            coEvery {
+                mockRemoteDataSource.inactiveAlarmState(any(), any())
+            } returns Result.success(Unit)
 
-        // when
-        val result = repository.inactiveAlarm("ssaid123", 1L)
+            // when
+            val result = repository.inactiveAlarm("ssaid123", 1L)
 
-        // then
-        assertTrue(result.isSuccess)
+            // then
+            assertTrue(result.isSuccess)
 
-        // verify
-        coVerify { mockRemoteDataSource.inactiveAlarmState(1L, "ssaid123") }
-    }
+            // verify
+            coVerify { mockRemoteDataSource.inactiveAlarmState(1L, "ssaid123") }
+        }
 }
