9503f102c320680992266a10d722691beda785b0
Sia <lyl8661@naver.com>
2025-07-24 12:17:36 +0900

    style: ktlint 적용

--- android/Bottari/di/src/main/java/com/bottari/di/DataSourceProvider.kt
+++ android/Bottari/di/src/main/java/com/bottari/di/DataSourceProvider.kt
@@ -1,9 +1,9 @@
 package com.bottari.di
 
-import com.bottari.data.source.remote.BottariItemRemoteDataSource
-import com.bottari.data.source.remote.BottariItemRemoteDataSourceImpl
 import com.bottari.data.source.remote.AlarmRemoteDataSourceImpl
 import com.bottari.data.source.remote.BottariDetailRemoteDataSourceImpl
+import com.bottari.data.source.remote.BottariItemRemoteDataSource
+import com.bottari.data.source.remote.BottariItemRemoteDataSourceImpl
 import com.bottari.data.source.remote.BottariRemoteDataSource
 import com.bottari.data.source.remote.BottariRemoteDataSourceImpl
 import com.bottari.data.source.remote.MemberRemoteDataSource
--- android/Bottari/di/src/main/java/com/bottari/di/UseCaseProvider.kt
+++ android/Bottari/di/src/main/java/com/bottari/di/UseCaseProvider.kt
@@ -2,10 +2,10 @@ package com.bottari.di
 
 import com.bottari.domain.usecase.alarm.ToggleAlarmStateUseCase
 import com.bottari.domain.usecase.bottari.FetchBottariesUseCase
+import com.bottari.domain.usecase.bottariDetail.FindBottariDetailUseCase
 import com.bottari.domain.usecase.item.CheckBottariItemUseCase
 import com.bottari.domain.usecase.item.FetchChecklistUseCase
 import com.bottari.domain.usecase.item.UnCheckBottariItemUseCase
-import com.bottari.domain.usecase.bottariDetail.FindBottariDetailUseCase
 import com.bottari.domain.usecase.member.RegisterMemberUseCase
 import com.bottari.domain.util.NicknameGenerator
 
