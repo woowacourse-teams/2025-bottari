b9eeb39cc98bbb98ae094541205afec330168f8d
moondev03 <moondev03@gmail.com>
2025-07-24 16:06:24 +0900

    feat: UiModel에 Parcelable 구현

--- android/Bottari/presentation/src/main/java/com/bottari/presentation/model/BottariItemUiModel.kt
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/model/BottariItemUiModel.kt
@@ -1,7 +1,11 @@
 package com.bottari.presentation.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class BottariItemUiModel(
     val id: Long,
     val name: String,
     val isChecked: Boolean,
-)
+) : Parcelable
--- android/Bottari/presentation/src/main/java/com/bottari/presentation/model/DayOfWeekUiModel.kt
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/model/DayOfWeekUiModel.kt
@@ -1,8 +1,11 @@
 package com.bottari.presentation.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 import java.time.DayOfWeek
 
+@Parcelize
 data class DayOfWeekUiModel(
     val dayOfWeek: DayOfWeek,
     val isChecked: Boolean,
-)
+) : Parcelable
