1522056d94d19c684d135fdb6f2533d47d619d91
cucumber99 <dlsguq1@gmail.com>
2025-07-24 19:38:21 +0900

    feat: 알람 DataSource에 알람 수정 및 생성 기능 구현

--- android/Bottari/data/src/main/java/com/bottari/data/source/remote/AlarmRemoteDataSource.kt
+++ android/Bottari/data/src/main/java/com/bottari/data/source/remote/AlarmRemoteDataSource.kt
@@ -1,6 +1,20 @@
 package com.bottari.data.source.remote
 
+import com.bottari.data.model.bottari.AlarmRequest
+
 interface AlarmRemoteDataSource {
+    suspend fun saveAlarm(
+        ssaid: String,
+        id: Long,
+        alarmRequest: AlarmRequest,
+    ): Result<Unit>
+
+    suspend fun createAlarm(
+        ssaid: String,
+        bottariId: Long,
+        alarmRequest: AlarmRequest,
+    ): Result<Unit>
+
     suspend fun activeAlarmState(
         id: Long,
         ssaid: String,
