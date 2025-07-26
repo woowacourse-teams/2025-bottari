20ac86261889eaddb3c6fd9b1413f7af935cf223
Yehyeok Bang <qkddpgur318@gmail.com>
2025-07-27 00:30:29 +0900

    feat: 사용자 SSAID 유니크 제약조건 추가

--- backend/bottari/src/main/java/com/bottari/domain/Member.java
+++ backend/bottari/src/main/java/com/bottari/domain/Member.java
@@ -18,6 +18,7 @@ public class Member {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
     private String ssaid;
 
     @Column(unique = true)
