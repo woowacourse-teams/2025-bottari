820aa8088e4ed1eb12432d6c9b86d272935ac7aa
Sia <lyl8661@naver.com>
2025-07-27 01:46:43 +0900

    feat: 보따리 이름 변경 기능 구현

---
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/view/edit/personal/main/rename/BottariRenameDialog.kt
@@ -0,0 +1,132 @@
+package com.bottari.presentation.view.edit.personal.main.rename
+
+import android.content.res.Resources
+import android.graphics.Color
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.graphics.drawable.toDrawable
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.viewModels
+import com.bottari.presentation.base.UiState
+import com.bottari.presentation.databinding.DialogBottariRenameBinding
+import com.bottari.presentation.extension.getSSAID
+import com.google.android.material.snackbar.Snackbar
+
+class BottariRenameDialog :
+    DialogFragment(),
+    TextWatcher {
+    private val viewModel: BottariRenameViewModel by viewModels { BottariRenameViewModel.Factory() }
+    private var _binding: DialogBottariRenameBinding? = null
+    val binding: DialogBottariRenameBinding get() = _binding!!
+
+    private var bottariId: Long = -1
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = DialogBottariRenameBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+        setupObserver()
+        setupListener()
+        bottariId = arguments?.getLong(EXTRA_BOTTARI_ID) ?: -1
+    }
+
+    override fun onStart() {
+        super.onStart()
+        setupDialog()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun afterTextChanged(s: Editable?) {}
+
+    override fun beforeTextChanged(
+        s: CharSequence?,
+        start: Int,
+        count: Int,
+        after: Int,
+    ) {
+    }
+
+    override fun onTextChanged(
+        s: CharSequence?,
+        start: Int,
+        before: Int,
+        count: Int,
+    ) {
+        val isEnabled = s.isNullOrBlank().not()
+        val alphaValue = if (isEnabled) ENABLED_ALPHA_VALUE else DISABLED_ALPHA_VALUE
+        binding.btnBottariCreate.isClickable = isEnabled
+        binding.btnBottariCreate.alpha = alphaValue
+    }
+
+    private fun setupObserver() {
+        viewModel.renameSuccess.observe(viewLifecycleOwner, ::handleRenameState)
+    }
+
+    private fun setupListener() {
+        binding.etBottariCreateName.addTextChangedListener(this)
+        binding.btnBottariCreateClose.setOnClickListener { dismiss() }
+        binding.btnBottariCreate.setOnClickListener {
+            val title = binding.etBottariCreateName.text.toString()
+            viewModel.renameBottari(bottariId, requireContext().getSSAID(), title)
+        }
+    }
+
+    private fun setupDialog() {
+        val metrics = Resources.getSystem().displayMetrics
+        val width = (metrics.widthPixels * WIDTH_RATIO).toInt()
+        dialog?.run {
+            setCancelable(false)
+            window?.setBackgroundDrawable(Color.TRANSPARENT.toDrawable())
+            window?.setLayout(
+                width,
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+            )
+        }
+    }
+
+    private fun handleRenameState(uiState: UiState<Unit?>) {
+        when (uiState) {
+            is UiState.Loading -> Unit
+            is UiState.Success -> dismiss()
+            is UiState.Failure -> {
+                Snackbar
+                    .make(binding.root, uiState.message.toString(), Snackbar.LENGTH_SHORT)
+                    .show()
+            }
+        }
+    }
+
+    companion object {
+        private const val WIDTH_RATIO = 0.9
+        private const val DISABLED_ALPHA_VALUE = 0.4f
+        private const val ENABLED_ALPHA_VALUE = 1f
+
+        private const val EXTRA_BOTTARI_ID = "EXTRA_BOTTARI_ID"
+
+        fun newInstance(bottariId: Long): BottariRenameDialog =
+            BottariRenameDialog().apply {
+                arguments =
+                    Bundle().apply {
+                        putLong(EXTRA_BOTTARI_ID, bottariId)
+                    }
+            }
+    }
+}
