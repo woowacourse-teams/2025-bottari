a035475116a7b0e7b82bd3772cb4ccd8640bf843
cucumber99 <dlsguq1@gmail.com>
2025-07-28 14:44:46 +0900

    feat: 닉네임 최소 글자 수 및 기본값 부여

--- android/Bottari/domain/src/main/java/com/bottari/domain/model/member/Member.kt
+++ android/Bottari/domain/src/main/java/com/bottari/domain/model/member/Member.kt
@@ -2,7 +2,7 @@ package com.bottari.domain.model.member
 
 data class Member(
     val ssaid: String,
-    val nickname: String,
+    val nickname: String = "",
 ) {
     init {
         require(ssaid.isNotBlank()) { SSAID_BLANK_ERROR }
@@ -10,7 +10,7 @@ data class Member(
     }
 
     companion object {
-        private const val MIN_NICKNAME_LENGTH = 2
+        private const val MIN_NICKNAME_LENGTH = 3
         private const val MAX_NICKNAME_LENGTH = 10
         private const val SSAID_BLANK_ERROR = "ssaid는 공백일 수 없습니다."
         private const val NICKNAME_LENGTH_ERROR =
