6065fb51b8ea816a0b320c1dc9501f86f5ed8466
moondev03 <moondev03@gmail.com>
2025-07-28 15:37:26 +0900

    feat: 내 보따리 템플릿 이벤트 리스너 인터페이스 추가
    - `MyBottariTemplateEventListener` 인터페이스를 추가하여 보따리 템플릿 삭제 및 상세 보기 클릭 이벤트를 처리할 수 있도록 함.

---
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/view/market/my/listener/MyBottariTemplateEventListener.kt
@@ -0,0 +1,7 @@
+package com.bottari.presentation.view.market.my.listener
+
+interface MyBottariTemplateEventListener {
+    fun onDeleteClick(bottariTemplateId: Long)
+
+    fun onDetailClick(bottariTemplateId: Long)
+}
