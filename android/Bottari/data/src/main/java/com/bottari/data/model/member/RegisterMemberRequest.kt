0ee9c13d81d3eb19a52e5dbf9aef7b859306cddb
cucumber99 <dlsguq1@gmail.com>
2025-07-28 17:18:24 +0900

    refactor: 유저 등록 API 스펙 변경에 따른 코드 개선

--- android/Bottari/data/src/main/java/com/bottari/data/model/member/RegisterMemberRequest.kt
+++ android/Bottari/data/src/main/java/com/bottari/data/model/member/RegisterMemberRequest.kt
@@ -5,8 +5,6 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class RegisterMemberRequest(
-    @SerialName("name")
-    val name: String,
     @SerialName("ssaid")
     val ssaid: String,
 )
--- android/Bottari/domain/src/main/java/com/bottari/domain/usecase/member/RegisterMemberUseCase.kt
+++ android/Bottari/domain/src/main/java/com/bottari/domain/usecase/member/RegisterMemberUseCase.kt
@@ -2,14 +2,12 @@ package com.bottari.domain.usecase.member
 
 import com.bottari.domain.model.member.Member
 import com.bottari.domain.repository.MemberRepository
-import com.bottari.domain.util.NicknameGenerator
 
 class RegisterMemberUseCase(
     private val memberRepository: MemberRepository,
-    private val nicknameGenerator: NicknameGenerator,
 ) {
-    suspend operator fun invoke(ssaid: String): Result<Boolean> {
-        val member = Member(ssaid, nicknameGenerator.generate())
+    suspend operator fun invoke(ssaid: String): Result<Unit> {
+        val member = Member(ssaid)
         return memberRepository.registerMember(member)
     }
 }
