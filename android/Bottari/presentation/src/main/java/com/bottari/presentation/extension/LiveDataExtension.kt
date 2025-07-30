11e64e3881227706f5fadb352294e8f14b0767bf
moondev03 <moondev03@gmail.com>
2025-07-30 20:09:20 +0900

    refactor: LiveData 확장 함수 추가

--- android/Bottari/presentation/src/main/java/com/bottari/presentation/extension/LiveDataExtension.kt
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/extension/LiveDataExtension.kt
@@ -1,7 +1,10 @@
 package com.bottari.presentation.extension
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 
 inline fun <T> MutableLiveData<T>.update(block: T.() -> T) {
     value = value?.block()
 }
+
+fun <T> LiveData<T>.requireValue(): T = requireNotNull(value)
