106c76dc15b07f816fd9aa834d8cc235363ebeb9
moondev03 <moondev03@gmail.com>
2025-07-31 16:37:31 +0900

    feat: 알람 편집 화면 로딩 인디케이터 기능 연동

--- android/Bottari/presentation/src/main/java/com/bottari/presentation/view/edit/alarm/AlarmUiState.kt
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/view/edit/alarm/AlarmUiState.kt
@@ -3,5 +3,6 @@ package com.bottari.presentation.view.edit.alarm
 import com.bottari.presentation.model.AlarmUiModel
 
 data class AlarmUiState(
+    val isLoading: Boolean = false,
     val alarm: AlarmUiModel,
 )
