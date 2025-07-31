8c55d6d0ddebddd1f7880e703c36e491d339ee1f
wogus <ind07152@naver.com>
2025-07-31 12:50:07 +0900

    feat: 헬스 체크 API 추가

---
+++ backend/bottari/src/main/java/com/bottari/controller/docs/HealthCheckApiDocs.java
@@ -0,0 +1,17 @@
+package com.bottari.controller.docs;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+
+@Tag(name = "Health Check", description = "헬스 체크 API")
+public interface HealthCheckApiDocs {
+
+    @Operation(summary = "서버 상태 체크")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "서버 상태 양호"),
+    })
+    ResponseEntity<Void> healthCheck();
+}
