dc7231c4271274cd2c681b6053ca59f8f195e53d
sung-june27 <kmymy27@gmail.com>
2025-08-02 20:37:58 +0900

    feat: 정렬 기준 열거형 생성

---
+++ backend/bottari/src/main/java/com/bottari/service/SortProperty.java
@@ -0,0 +1,29 @@
+package com.bottari.service;
+
+import lombok.Getter;
+
+@Getter
+public enum SortProperty {
+    CREATED_AT("createdAt"),
+    TAKEN_COUNT("takenCount"),
+    ;
+
+    private final String property;
+
+    SortProperty(final String property) {
+        this.property = property;
+    }
+
+    public boolean equalsProperty(final String property) {
+        return this.property.equals(property);
+    }
+
+    public static SortProperty fromProperty(final String property) {
+        for (final SortProperty value : SortProperty.values()) {
+            if (value.equalsProperty(property)) {
+                return value;
+            }
+        }
+        throw new IllegalArgumentException("존재하지 않는 정렬 기준입니다.");
+    }
+}
