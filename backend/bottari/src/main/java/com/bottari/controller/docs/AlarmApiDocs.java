a93a8ef45cce70798460e263bffabbcfbdaf4f32
Yehyeok Bang <qkddpgur318@gmail.com>
2025-08-03 01:47:35 +0900

    docs: API 문서에 에러 코드 추가

--- backend/bottari/src/main/java/com/bottari/controller/docs/AlarmApiDocs.java
+++ backend/bottari/src/main/java/com/bottari/controller/docs/AlarmApiDocs.java
@@ -2,13 +2,13 @@ package com.bottari.controller.docs;
 
 import com.bottari.dto.CreateAlarmRequest;
 import com.bottari.dto.UpdateAlarmRequest;
+import com.bottari.error.ApiErrorCodes;
+import com.bottari.error.ErrorCode;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Tag(name = "Alarm", description = "알람 API")
 public interface AlarmApiDocs {
@@ -17,33 +17,49 @@ public interface AlarmApiDocs {
     @ApiResponses(value = {
             @ApiResponse(responseCode = "201", description = "알람 생성 성공"),
     })
+    @ApiErrorCodes({
+            ErrorCode.BOTTARI_NOT_FOUND,
+            ErrorCode.ALARM_LOCATION_REQUIRES_ROUTINE
+    })
     ResponseEntity<Void> create(
-            @PathVariable final Long bottariId,
-            @RequestBody final CreateAlarmRequest request
+            final Long bottariId,
+            final CreateAlarmRequest request
     );
 
     @Operation(summary = "알람 수정")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "204", description = "알람 수정 성공"),
     })
+    @ApiErrorCodes({
+            ErrorCode.ALARM_NOT_FOUND,
+            ErrorCode.ALARM_LOCATION_REQUIRES_ROUTINE
+    })
     ResponseEntity<Void> update(
-            @PathVariable final Long id,
-            @RequestBody final UpdateAlarmRequest request
+            final Long id,
+            final UpdateAlarmRequest request
     );
 
     @Operation(summary = "알람 활성화")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "204", description = "알람 활성화 성공"),
     })
+    @ApiErrorCodes({
+            ErrorCode.ALARM_NOT_FOUND,
+            ErrorCode.ALARM_ALREADY_ACTIVE
+    })
     ResponseEntity<Void> active(
-            @PathVariable final Long id
+            final Long id
     );
 
     @Operation(summary = "알람 비활성화")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "204", description = "알람 비활성화 성공"),
     })
+    @ApiErrorCodes({
+            ErrorCode.ALARM_NOT_FOUND,
+            ErrorCode.ALARM_ALREADY_INACTIVE
+    })
     ResponseEntity<Void> inactive(
-            @PathVariable final Long id
+            final Long id
     );
 }
--- backend/bottari/src/main/java/com/bottari/controller/docs/BottariTemplateApiDocs.java
+++ backend/bottari/src/main/java/com/bottari/controller/docs/BottariTemplateApiDocs.java
@@ -2,6 +2,8 @@ package com.bottari.controller.docs;
 
 import com.bottari.dto.CreateBottariTemplateRequest;
 import com.bottari.dto.ReadBottariTemplateResponse;
+import com.bottari.error.ApiErrorCodes;
+import com.bottari.error.ErrorCode;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
@@ -17,23 +19,43 @@ public interface BottariTemplateApiDocs {
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "보따리 템플릿 상세 조회 성공"),
     })
-    ResponseEntity<ReadBottariTemplateResponse> read(final Long id);
+    @ApiErrorCodes({
+            ErrorCode.BOTTARI_TEMPLATE_NOT_FOUND
+    })
+    ResponseEntity<ReadBottariTemplateResponse> read(
+            final Long id
+    );
 
     @Operation(summary = "내 보따리 템플릿 목록 조회")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "내 보따리 템플릿 목록 조회 성공")
     })
-    ResponseEntity<List<ReadBottariTemplateResponse>> readMine(final HttpServletRequest httpServletRequest);
+    @ApiErrorCodes({
+            ErrorCode.MEMBER_NOT_FOUND
+    })
+    ResponseEntity<List<ReadBottariTemplateResponse>> readMine(
+            final HttpServletRequest httpServletRequest
+    );
 
     @Operation(summary = "보따리 템플릿 목록 조회")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "보따리 템플릿 목록 조회 성공"),
     })
-    ResponseEntity<List<ReadBottariTemplateResponse>> readAll(final String query);
+    ResponseEntity<List<ReadBottariTemplateResponse>> readAll(
+            final String query
+    );
 
     @Operation(summary = "보따리 템플릿 생성")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "201", description = "보따리 템플릿 생성 성공"),
+    })
+    @ApiErrorCodes({
+            ErrorCode.MEMBER_NOT_FOUND,
+            ErrorCode.BOTTARI_TEMPLATE_TITLE_BLANK,
+            ErrorCode.BOTTARI_TEMPLATE_TITLE_TOO_LONG,
+            ErrorCode.BOTTARI_TEMPLATE_ITEM_DUPLICATE_IN_REQUEST,
+            ErrorCode.BOTTARI_TEMPLATE_ITEM_NAME_BLANK,
+            ErrorCode.BOTTARI_TEMPLATE_ITEM_NAME_TOO_LONG
     })
     ResponseEntity<Void> create(
             final CreateBottariTemplateRequest request,
@@ -44,6 +66,14 @@ public interface BottariTemplateApiDocs {
     @ApiResponses(value = {
             @ApiResponse(responseCode = "201", description = "보따리 템플릿으로 보따리 생성 성공"),
     })
+    @ApiErrorCodes({
+            ErrorCode.MEMBER_NOT_FOUND,
+            ErrorCode.BOTTARI_TEMPLATE_NOT_FOUND,
+            ErrorCode.BOTTARI_TITLE_BLANK,
+            ErrorCode.BOTTARI_TITLE_TOO_LONG,
+            ErrorCode.BOTTARI_ITEM_NAME_BLANK,
+            ErrorCode.BOTTARI_ITEM_NAME_TOO_LONG
+    })
     ResponseEntity<Void> createBottari(
             final Long id,
             final HttpServletRequest httpServletRequest
@@ -52,6 +82,10 @@ public interface BottariTemplateApiDocs {
     @Operation(summary = "보따리 템플릿 삭제")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "204", description = "보따리 템플릿 삭제 성공")
+    })
+    @ApiErrorCodes({
+            ErrorCode.BOTTARI_TEMPLATE_NOT_FOUND,
+            ErrorCode.BOTTARI_TEMPLATE_NOT_OWNED
     })
     ResponseEntity<Void> delete(
             final Long id,
