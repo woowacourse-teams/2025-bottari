628973b8a1caf547e8073daf22ecf56ba7fb437f
Sia <lyl8661@naver.com>
2025-08-04 20:04:35 +0900

    refactor: 미사용 확장함수 제거

--- android/Bottari/presentation/src/main/java/com/bottari/presentation/common/extension/LiveDataExtension.kt
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/common/extension/LiveDataExtension.kt
@@ -1,25 +1,7 @@
 package com.bottari.presentation.common.extension
 
-import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Observer
 
 inline fun <T> MutableLiveData<T>.update(block: T.() -> T) {
     value = value?.block()
-}
-
-fun <T> LiveData<T>.observeOnce(
-    owner: LifecycleOwner,
-    observer: Observer<T>,
-) {
-    observe(
-        owner,
-        object : Observer<T> {
-            override fun onChanged(value: T) {
-                observer.onChanged(value)
-                removeObserver(this)
-            }
-        },
-    )
 }
