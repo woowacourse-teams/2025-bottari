e20041f284e159732bf7a81f3e98c80be40a856b
Sia <lyl8661@naver.com>
2025-08-04 15:33:09 +0900

    feat: 체크리스트 아이템 전체 스와이프 시 UI 이벤트 추가

--- android/Bottari/presentation/src/main/java/com/bottari/presentation/view/checklist/ChecklistUiEvent.kt
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/view/checklist/ChecklistUiEvent.kt
@@ -4,4 +4,8 @@ sealed interface ChecklistUiEvent {
     data object FetchChecklistFailure : ChecklistUiEvent
 
     data object CheckItemFailure : ChecklistUiEvent
+
+    data object AllSwipedAllChecked : ChecklistUiEvent
+
+    data object AllSwipedNotAllChecked : ChecklistUiEvent
 }
