3186df20e8e7a32b55c2f1e09e8ce9e5859db995
Sia <lyl8661@naver.com>
2025-08-04 20:33:01 +0900

    refactor: 미사용 uiEvent 삭제

--- android/Bottari/presentation/src/main/java/com/bottari/presentation/view/checklist/main/MainChecklistFragment.kt
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/view/checklist/main/MainChecklistFragment.kt
@@ -43,8 +43,6 @@ class MainChecklistFragment : BaseFragment<FragmentChecklistBinding>(FragmentChe
             when (uiEvent) {
                 ChecklistUiEvent.FetchChecklistFailure -> showSnackbar(R.string.checklist_fetch_failure_text)
                 ChecklistUiEvent.CheckItemFailure -> showSnackbar(R.string.checklist_check_failure_text)
-                ChecklistUiEvent.AllSwipedAllChecked -> {}
-                ChecklistUiEvent.AllSwipedNotAllChecked -> {}
             }
         }
     }
