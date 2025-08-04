bdc65b2a0b77f605242a3f25f36edbd3099bd847
Sia <lyl8661@naver.com>
2025-08-04 15:32:52 +0900

    feat: LiveData.observeOnce() 확장 함수 추가

--- android/Bottari/presentation/src/main/java/com/bottari/presentation/common/extension/LiveDataExtension.kt
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/common/extension/LiveDataExtension.kt
@@ -1,7 +1,25 @@
 package com.bottari.presentation.common.extension
 
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 
 inline fun <T> MutableLiveData<T>.update(block: T.() -> T) {
     value = value?.block()
+}
+
+fun <T> LiveData<T>.observeOnce(
+    owner: LifecycleOwner,
+    observer: Observer<T>,
+) {
+    observe(
+        owner,
+        object : Observer<T> {
+            override fun onChanged(value: T) {
+                observer.onChanged(value)
+                removeObserver(this)
+            }
+        },
+    )
 }
