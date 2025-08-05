004524b8fb0c29c0e755816bf6cedaff7051d1d2
cucumber99 <dlsguq1@gmail.com>
2025-08-05 11:59:25 +0900

    feat: 알람 리시버 구현

---
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/receiver/AlarmReceiver.kt
@@ -0,0 +1,53 @@
+package com.bottari.presentation.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import com.bottari.presentation.common.extension.getParcelableCompat
+import com.bottari.presentation.model.AlarmTypeUiModel
+import com.bottari.presentation.model.NotificationUiModel
+import com.bottari.presentation.util.AlarmScheduler
+import com.bottari.presentation.util.NotificationHelper
+import java.time.LocalDate
+
+class AlarmReceiver : BroadcastReceiver() {
+    private val scheduler: AlarmScheduler by lazy { AlarmScheduler() }
+
+    override fun onReceive(
+        context: Context?,
+        intent: Intent,
+    ) {
+        val notification = intent.getParcelableCompat<NotificationUiModel>(EXTRA_NOTIFICATION)
+        NotificationHelper().sendNotification(notification.id, notification.title)
+        scheduleNextAlarm(notification)
+    }
+
+    private fun scheduleNextAlarm(notification: NotificationUiModel) {
+        if (notification.alarm.type == AlarmTypeUiModel.NON_REPEAT) return
+        val availableDays =
+            notification.alarm.daysOfWeek
+                .filter { dayOfWeek -> dayOfWeek.isChecked }
+                .map { it.dayOfWeek }
+        val today = LocalDate.now().dayOfWeek
+        if (availableDays.contains(today)) {
+            val nextAlarmDate = LocalDate.now().plusWeeks(WEEKS_TO_ADD)
+            scheduler.scheduleAlarm(notification.copy(alarm = notification.alarm.copy(date = nextAlarmDate)))
+        }
+    }
+
+    companion object {
+        private const val EXTRA_NOTIFICATION = "EXTRA_NOTIFICATION"
+        private const val WEEKS_TO_ADD = 1L
+
+        fun newIntent(
+            context: Context,
+            notification: NotificationUiModel,
+        ): Intent =
+            Intent(
+                context,
+                AlarmReceiver::class.java,
+            ).apply {
+                putExtra(EXTRA_NOTIFICATION, notification)
+            }
+    }
+}
