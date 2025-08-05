91c3769f5bdace079b7f7fc4e2eeae30a4850d99
cucumber99 <dlsguq1@gmail.com>
2025-08-05 11:57:37 +0900

    feat: NotificationHelper 클래스 구현

---
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/util/NotificationHelper.kt
@@ -0,0 +1,94 @@
+package com.bottari.presentation.util
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.media.AudioAttributes
+import android.media.RingtoneManager
+import androidx.core.app.NotificationCompat
+import com.bottari.di.ApplicationContextProvider
+import com.bottari.presentation.R
+import com.bottari.presentation.view.checklist.ChecklistActivity
+
+class NotificationHelper(
+    private val context: Context = ApplicationContextProvider.applicationContext,
+) {
+    private val manager: NotificationManager =
+        context.getSystemService(NotificationManager::class.java)
+
+    fun sendNotification(
+        bottariId: Long,
+        bottariTitle: String,
+    ) {
+        createNotificationChannel()
+        val pendingIntent = createClickIntent(bottariId, bottariTitle)
+        val notification = createNotification(pendingIntent, bottariTitle)
+        manager.notify(bottariTitle.hashCode(), notification)
+    }
+
+    private fun createNotificationChannel() {
+        val audioAttributes =
+            AudioAttributes
+                .Builder()
+                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                .setUsage(AudioAttributes.USAGE_NOTIFICATION)
+                .build()
+        val channel =
+            NotificationChannel(
+                BOTTARI_CHANNEL_ID,
+                context.getString(R.string.common_bottari_notification_channel_name),
+                NotificationManager.IMPORTANCE_HIGH,
+            ).apply {
+                enableLights(true)
+                enableVibration(true)
+                vibrationPattern = VIBRATION_PATTERN
+                setSound(
+                    RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION),
+                    audioAttributes,
+                )
+            }
+        manager.createNotificationChannel(channel)
+    }
+
+    private fun createClickIntent(
+        bottariId: Long,
+        bottariTitle: String,
+    ): PendingIntent {
+        val intent =
+            ChecklistActivity.newIntentForNotification(
+                context,
+                bottariId,
+                bottariTitle,
+            )
+        return PendingIntent.getActivity(
+            context,
+            bottariTitle.hashCode(),
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE,
+        )
+    }
+
+    private fun createNotification(
+        intent: PendingIntent,
+        bottariTitle: String,
+    ): Notification =
+        NotificationCompat
+            .Builder(context, BOTTARI_CHANNEL_ID)
+            .setSmallIcon(R.drawable.bottari)
+            .setContentTitle(context.getString(R.string.common_bottari_notification_title))
+            .setContentText(
+                context
+                    .getString(R.string.common_bottari_notification_message)
+                    .format(bottariTitle),
+            ).setContentIntent(intent)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setAutoCancel(true)
+            .build()
+
+    companion object {
+        private const val BOTTARI_CHANNEL_ID = "BOTTARI_CHANNEL_ID"
+        private val VIBRATION_PATTERN = longArrayOf(0, 300, 200, 300)
+    }
+}
