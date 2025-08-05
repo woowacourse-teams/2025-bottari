07790a0e133c29317d3003fae8c54ceb19147478
cucumber99 <dlsguq1@gmail.com>
2025-08-05 18:02:29 +0900

    refactor: 유효 날짜 계산 메소드 분리

--- android/Bottari/presentation/src/main/java/com/bottari/presentation/receiver/AlarmReceiver.kt
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/receiver/AlarmReceiver.kt
@@ -5,35 +5,40 @@ import android.content.Context
 import android.content.Intent
 import com.bottari.presentation.common.extension.getParcelableCompat
 import com.bottari.presentation.model.AlarmTypeUiModel
+import com.bottari.presentation.model.DayOfWeekUiModel
 import com.bottari.presentation.model.NotificationUiModel
 import com.bottari.presentation.util.AlarmScheduler
 import com.bottari.presentation.util.NotificationHelper
+import java.time.DayOfWeek
 import java.time.LocalDate
 
 class AlarmReceiver : BroadcastReceiver() {
     private val scheduler: AlarmScheduler by lazy { AlarmScheduler() }
+    private val notificationHelper: NotificationHelper by lazy { NotificationHelper() }
 
     override fun onReceive(
         context: Context?,
         intent: Intent,
     ) {
         val notification = intent.getParcelableCompat<NotificationUiModel>(EXTRA_NOTIFICATION)
-        NotificationHelper().sendNotification(notification.id, notification.title)
+        notificationHelper.sendNotification(notification.id, notification.title)
         scheduleNextAlarm(notification)
     }
 
     private fun scheduleNextAlarm(notification: NotificationUiModel) {
         if (notification.alarm.type == AlarmTypeUiModel.NON_REPEAT) return
-        val availableDays =
-            notification.alarm.daysOfWeek
-                .filter { dayOfWeek -> dayOfWeek.isChecked }
-                .map { it.dayOfWeek }
-        val today = LocalDate.now().dayOfWeek
-        if (availableDays.contains(today)) {
-            val nextAlarmDate = LocalDate.now().plusWeeks(WEEKS_TO_ADD)
+        val availableDays = calculateAvailableDays(notification.alarm.daysOfWeek)
+        val today = LocalDate.now()
+        if (availableDays.contains(today.dayOfWeek)) {
+            val nextAlarmDate = today.plusWeeks(WEEKS_TO_ADD)
             scheduler.scheduleAlarm(notification.copy(alarm = notification.alarm.copy(date = nextAlarmDate)))
         }
     }
+
+    private fun calculateAvailableDays(daysOfWeek: List<DayOfWeekUiModel>): List<DayOfWeek> =
+        daysOfWeek
+            .filter { dayOfWeek -> dayOfWeek.isChecked }
+            .map { it.dayOfWeek }
 
     companion object {
         private const val EXTRA_NOTIFICATION = "EXTRA_NOTIFICATION"
