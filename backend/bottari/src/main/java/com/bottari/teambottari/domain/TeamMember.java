d535780db70deb74a11748db6af7233b2e6560a1
민경윤 <ruddbs3308@naver.com>
2025-08-06 17:27:37 +0900

    feat: 팀 보따리 멤버 엔티티 추가

---
+++ backend/bottari/src/main/java/com/bottari/teambottari/domain/TeamMember.java
@@ -0,0 +1,38 @@
+package com.bottari.teambottari.domain;
+
+import com.bottari.member.domain.Member;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class TeamMember {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "team_bottari_id")
+    private TeamBottari teamBottari;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    public TeamMember(
+            final TeamBottari teamBottari,
+            final Member member
+    ) {
+        this.teamBottari = teamBottari;
+        this.member = member;
+    }
+}
