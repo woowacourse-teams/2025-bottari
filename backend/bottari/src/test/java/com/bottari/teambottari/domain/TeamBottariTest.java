c1b282e7465642b3c00fe61f89cea2c6bb53ce29
민경윤 <144558971+unh6unh6@users.noreply.github.com>
2025-08-07 11:11:14 +0900

    [BE] 팀 보따리 관련 엔티티 설계 (#258)
    * feat: 팀 보따리 엔티티 및 검증 로직 추가
    
    * feat: 팀 보따리 멤버 엔티티 추가
    
    * feat: 팀 보따리 공통 물품 정보 엔티티 및 검증 로직 추가
    
    * feat: 팀 보따리 담당 물품 정보 엔티티 및 검증 로직 추가
    
    * feat: 팀 보따리 담당 및 공유 물품 엔티티 추가
    
    * feat: 팀 개인 보따리 물품 엔티티 및 검증 로직 추가
    
    * feat: 팀 보따리 관련 엔티티의 FetchType.LAZY 설정 추가
    
    * feat: 팀 보따리 생성일시 필드 추가

---
+++ backend/bottari/src/test/java/com/bottari/teambottari/domain/TeamBottariTest.java
@@ -0,0 +1,40 @@
+package com.bottari.teambottari.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import com.bottari.bottari.domain.Bottari;
+import com.bottari.error.BusinessException;
+import com.bottari.fixture.MemberFixture;
+import com.bottari.member.domain.Member;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class TeamBottariTest {
+
+    @DisplayName("팀 보따리 제목이 공백인 경우, 예외를 던진다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"", "     "})
+    void validateTitle_Blank(final String title) {
+        // given
+        final Member member = MemberFixture.MEMBER.get();
+
+        // when & then
+        assertThatThrownBy(() -> new TeamBottari(title, member, "inviteCode"))
+                .isInstanceOf(BusinessException.class)
+                .hasMessage("팀 보따리 제목은 공백일 수 없습니다.");
+    }
+
+    @DisplayName("팀 보따리 제목이 15자를 초과하는 경우, 예외를 던진다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"열다섯글자가넘는보따리이름입니다", "열다섯글자가넘는보따리이름입니다!"})
+    void validateTitle_TooLong(final String title) {
+        // given
+        final Member member = MemberFixture.MEMBER.get();
+
+        // when & then
+        assertThatThrownBy(() -> new TeamBottari(title, member, "inviteCode"))
+                .isInstanceOf(BusinessException.class)
+                .hasMessage("팀 보따리 제목이 너무 깁니다. - 최대 15자까지 입력 가능합니다.");
+    }
+}
