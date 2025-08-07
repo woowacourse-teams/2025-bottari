ae8efdc48850131b5c5f201cb33d0d4a5ff7f406
moondev03 <moondev03@gmail.com>
2025-08-07 22:12:21 +0900

    feat: BottariViewModel BaseViewModel 적용

--- android/Bottari/presentation/src/main/java/com/bottari/presentation/view/edit/personal/main/rename/BottariRenameViewModel.kt
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/view/edit/personal/main/rename/BottariRenameViewModel.kt
@@ -1,64 +1,53 @@
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.createSavedStateHandle
-import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.bottari.di.UseCaseProvider
 import com.bottari.domain.usecase.bottari.SaveBottariTitleUseCase
-import com.bottari.presentation.common.event.SingleLiveEvent
-import com.bottari.presentation.common.extension.update
+import com.bottari.presentation.common.base.BaseViewModel
 import com.bottari.presentation.view.edit.personal.main.rename.BottariRenameUiEvent
 import com.bottari.presentation.view.edit.personal.main.rename.BottariRenameUiState
-import kotlinx.coroutines.launch
 
 class BottariRenameViewModel(
     savedStateHandle: SavedStateHandle,
     private val saveBottariTitleUseCase: SaveBottariTitleUseCase,
-) : ViewModel() {
-    private val ssaid: String =
-        savedStateHandle[KEY_SSAID] ?: error(ERROR_REQUIRE_SSAID)
-    private val id: Long =
-        savedStateHandle[KEY_BOTTARI_ID] ?: error(ERROR_REQUIRE_BOTTARI_ID)
-    private val oldTitle: String =
-        savedStateHandle[KEY_OLD_TITLE] ?: error(ERROR_REQUIRE_OLD_TITLE)
+) : BaseViewModel<BottariRenameUiState, BottariRenameUiEvent>(
+        BottariRenameUiState(
+            title = savedStateHandle[KEY_INITIAL_TITLE] ?: error(ERROR_REQUIRE_OLD_TITLE),
+        ),
+    ) {
+    private val ssaid: String = savedStateHandle[KEY_SSAID] ?: error(ERROR_REQUIRE_SSAID)
 
-    private val _uiState: MutableLiveData<BottariRenameUiState> =
-        MutableLiveData(BottariRenameUiState())
-    val uiState: LiveData<BottariRenameUiState> = _uiState
-
-    private val _uiEvent: SingleLiveEvent<BottariRenameUiEvent> = SingleLiveEvent()
-    val uiEvent: LiveData<BottariRenameUiEvent> = _uiEvent
-
-    fun saveBottariTitle(newTitle: String) {
+    fun saveBottariTitle(
+        bottariId: Long,
+        newTitle: String,
+    ) {
         if (!isValidTitle(newTitle)) return
 
-        _uiState.update { copy(isLoading = true) }
+        updateState { copy(isLoading = true) }
 
-        viewModelScope.launch {
-            saveBottariTitleUseCase(id, ssaid, newTitle)
+        launch {
+            saveBottariTitleUseCase(bottariId, ssaid, newTitle)
                 .onSuccess {
-                    _uiState.update { copy(title = newTitle) }
-                    _uiEvent.value = BottariRenameUiEvent.SaveBottariTitleSuccess
+                    updateState { copy(title = newTitle) }
+                    emitEvent(BottariRenameUiEvent.SaveBottariTitleSuccess)
                 }.onFailure {
-                    _uiEvent.value = BottariRenameUiEvent.SaveBottariTitleFailure
+                    emitEvent(BottariRenameUiEvent.SaveBottariTitleFailure)
                 }
-            _uiState.update { copy(isLoading = false) }
+            updateState { copy(isLoading = false) }
         }
     }
 
     fun cacheTitleInput(newTitle: String) {
-        _uiState.update { copy(title = newTitle) }
+        updateState { copy(title = newTitle) }
     }
 
     private fun isValidTitle(newTitle: String): Boolean =
         when {
             newTitle.isBlank() -> false
-            newTitle == oldTitle -> {
-                _uiEvent.value = BottariRenameUiEvent.SaveBottariTitleSuccess
+            newTitle == currentState.initialTitle -> {
+                emitEvent(BottariRenameUiEvent.SaveBottariTitleSuccess)
                 false
             }
 
@@ -66,24 +55,20 @@ class BottariRenameViewModel(
         }
 
     companion object {
-        private const val KEY_BOTTARI_ID = "KEY_BOTTARI_ID"
-        private const val KEY_OLD_TITLE = "KEY_OLD_TITLE"
+        private const val KEY_INITIAL_TITLE = "KEY_INITIAL_TITLE"
         private const val KEY_SSAID = "KEY_SSAID"
 
-        private const val ERROR_REQUIRE_BOTTARI_ID = "보따리 ID가 없습니다"
-        private const val ERROR_REQUIRE_OLD_TITLE = "보따리 이름이 없습니다"
-        private const val ERROR_REQUIRE_SSAID = "SSAID가 없습니다"
+        private const val ERROR_REQUIRE_OLD_TITLE = "[ERROR] 보따리 이름이 없습니다"
+        private const val ERROR_REQUIRE_SSAID = "[ERROR] SSAID가 없습니다"
 
         fun Factory(
             ssaid: String,
-            bottariId: Long,
-            oldTitle: String,
+            initialTitle: String,
         ): ViewModelProvider.Factory =
             viewModelFactory {
                 initializer {
                     val handle = createSavedStateHandle()
-                    handle[KEY_BOTTARI_ID] = bottariId
-                    handle[KEY_OLD_TITLE] = oldTitle
+                    handle[KEY_INITIAL_TITLE] = initialTitle
                     handle[KEY_SSAID] = ssaid
 
                     BottariRenameViewModel(
