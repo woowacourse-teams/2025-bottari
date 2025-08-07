193c988f09f1ddb2b213e93b374a006548052e88
민경윤 <144558971+unh6unh6@users.noreply.github.com>
2025-08-07 11:11:14 +0900

    [BE] 팀 보따리 관련 엔티티 설계 (#258)
    * feat: 팀 보따리 엔티티 및 검증 로직 추가
    
    * feat: 팀 보따리 멤버 엔티티 추가
    
    * feat: 팀 보따리 공통 물품 정보 엔티티 및 검증 로직 추가
    
    * feat: 팀 보따리 담당 물품 정보 엔티티 및 검증 로직 추가
    
    * feat: 팀 보따리 담당 및 공유 물품 엔티티 추가
    
    * feat: 팀 개인 보따리 물품 엔티티 및 검증 로직 추가
    
    * feat: 팀 보따리 관련 엔티티의 FetchType.LAZY 설정 추가
    
    * feat: 팀 보따리 생성일시 필드 추가

---
+++ backend/bottari/src/main/java/com/bottari/teambottari/domain/TeamBottari.java
@@ -0,0 +1,61 @@
+package com.bottari.teambottari.domain;
+
+import com.bottari.error.BusinessException;
+import com.bottari.error.ErrorCode;
+import com.bottari.member.domain.Member;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class TeamBottari {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "owner_id")
+    private Member owner;
+
+    private String inviteCode;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    public TeamBottari(
+            final String title,
+            final Member owner,
+            final String inviteCode
+    ) {
+        validateTitle(title);
+        this.title = title;
+        this.owner = owner;
+        this.inviteCode = inviteCode;
+    }
+
+    private void validateTitle(final String title) {
+        if (title.isBlank()) {
+            throw new BusinessException(ErrorCode.TEAM_BOTTARI_TITLE_BLANK);
+        }
+        if (title.length() > 15) {
+            throw new BusinessException(ErrorCode.TEAM_BOTTARI_TITLE_TOO_LONG, "최대 15자까지 입력 가능합니다.");
+        }
+    }
+}
---
+++ backend/bottari/src/main/java/com/bottari/teambottari/domain/TeamMember.java
@@ -0,0 +1,39 @@
+package com.bottari.teambottari.domain;
+
+import com.bottari.member.domain.Member;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class TeamMember {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_bottari_id")
+    private TeamBottari teamBottari;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    public TeamMember(
+            final TeamBottari teamBottari,
+            final Member member
+    ) {
+        this.teamBottari = teamBottari;
+        this.member = member;
+    }
+}
