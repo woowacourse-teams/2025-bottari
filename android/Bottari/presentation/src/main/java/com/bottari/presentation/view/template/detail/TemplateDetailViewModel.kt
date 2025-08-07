87700a3f52b7b66117d3301b4c0d3dc2b705eb56
moondev03 <moondev03@gmail.com>
2025-08-07 21:56:12 +0900

    feat: TemplateDetailViewModel BaseViewModel 적용

--- android/Bottari/presentation/src/main/java/com/bottari/presentation/view/template/detail/TemplateDetailViewModel.kt
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/view/template/detail/TemplateDetailViewModel.kt
@@ -1,38 +1,25 @@
 package com.bottari.presentation.view.template.detail
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.createSavedStateHandle
-import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.bottari.di.UseCaseProvider
 import com.bottari.domain.usecase.template.FetchBottariTemplateDetailUseCase
 import com.bottari.domain.usecase.template.TakeBottariTemplateDetailUseCase
-import com.bottari.presentation.common.event.SingleLiveEvent
-import com.bottari.presentation.common.extension.update
+import com.bottari.presentation.common.base.BaseViewModel
 import com.bottari.presentation.mapper.BottariTemplateMapper.toUiModel
-import kotlinx.coroutines.launch
 
 class TemplateDetailViewModel(
     stateHandle: SavedStateHandle,
     private val fetchBottariTemplateDetailUseCase: FetchBottariTemplateDetailUseCase,
     private val takeBottariTemplateDetailUseCase: TakeBottariTemplateDetailUseCase,
-) : ViewModel() {
-    private val _uiState: MutableLiveData<TemplateDetailUiState> =
-        MutableLiveData(
-            TemplateDetailUiState(
-                templateId = stateHandle[KEY_TEMPLATE_ID] ?: error(ERROR_REQUIRE_TEMPLATE_ID),
-            ),
-        )
-    val uiState: LiveData<TemplateDetailUiState> get() = _uiState
-
-    private val _uiEvent: SingleLiveEvent<TemplateDetailUiEvent> = SingleLiveEvent()
-    val uiEvent: LiveData<TemplateDetailUiEvent> get() = _uiEvent
-
+) : BaseViewModel<TemplateDetailUiState, TemplateDetailUiEvent>(
+        TemplateDetailUiState(
+            templateId = stateHandle[KEY_TEMPLATE_ID] ?: error(ERROR_REQUIRE_TEMPLATE_ID),
+        ),
+    ) {
     private val ssaid: String = stateHandle[KEY_SSAID] ?: error(ERROR_REQUIRE_SSAID)
 
     init {
@@ -40,34 +27,35 @@ class TemplateDetailViewModel(
     }
 
     fun takeBottariTemplate() {
-        _uiState.update { copy(isLoading = true) }
+        updateState { copy(isLoading = true) }
 
-        viewModelScope.launch {
-            takeBottariTemplateDetailUseCase(ssaid, _uiState.value!!.templateId)
+        launch {
+            takeBottariTemplateDetailUseCase(ssaid, currentState.templateId)
                 .onSuccess { bottariId ->
-                    _uiEvent.value =
-                        TemplateDetailUiEvent.TakeBottariTemplateSuccess(bottariId)
+                    emitEvent(
+                        TemplateDetailUiEvent.TakeBottariTemplateSuccess(bottariId),
+                    )
                 }.onFailure {
-                    _uiEvent.value = TemplateDetailUiEvent.TakeBottariTemplateFailure
+                    emitEvent(TemplateDetailUiEvent.TakeBottariTemplateFailure)
                 }
 
-            _uiState.update { copy(isLoading = false) }
+            updateState { copy(isLoading = false) }
         }
     }
 
     private fun fetchBottariTemplateDetail() {
-        _uiState.update { copy(isLoading = true) }
+        updateState { copy(isLoading = true) }
 
-        viewModelScope.launch {
-            fetchBottariTemplateDetailUseCase(_uiState.value!!.templateId)
+        launch {
+            fetchBottariTemplateDetailUseCase(currentState.templateId)
                 .onSuccess { template ->
                     val itemUiModels = template.items.map { it.toUiModel() }
-                    _uiState.update { copy(title = template.title, items = itemUiModels) }
+                    updateState { copy(title = template.title, items = itemUiModels) }
                 }.onFailure {
-                    _uiEvent.value = TemplateDetailUiEvent.FetchBottariDetailFailure
+                    emitEvent(TemplateDetailUiEvent.FetchBottariDetailFailure)
                 }
 
-            _uiState.update { copy(isLoading = false) }
+            updateState { copy(isLoading = false) }
         }
     }
 
