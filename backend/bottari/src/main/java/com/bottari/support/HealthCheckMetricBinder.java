4ff0fd2ea32955303962ca13f8a380933ff29844
장재현 <121144710+jaehyeon2650@users.noreply.github.com>
2025-08-10 11:36:23 +0900

    [BE] 헬스 체크 API 로그 제외 (#279)
    * build: 메트릭 전송 의존성 추가
    
    * feat: 헬스 체크 매트릭 추가

---
+++ backend/bottari/src/main/java/com/bottari/support/HealthCheckMetricBinder.java
@@ -0,0 +1,32 @@
+package com.bottari.support;
+
+import io.micrometer.core.instrument.Gauge;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.binder.MeterBinder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.actuate.health.HealthComponent;
+import org.springframework.boot.actuate.health.HealthEndpoint;
+import org.springframework.boot.actuate.health.Status;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class HealthCheckMetricBinder implements MeterBinder {
+
+    private final HealthEndpoint healthEndpoint;
+
+    @Override
+    public void bindTo(final MeterRegistry meterRegistry) {
+        Gauge.builder(
+                "app.health.status",
+                () -> {
+                    final HealthComponent health = healthEndpoint.health();
+                    final Status status = health.getStatus();
+                    if (Status.UP.equals(status)) {
+                        return 1;
+                    }
+                    return 0;
+                }
+        );
+    }
+}
