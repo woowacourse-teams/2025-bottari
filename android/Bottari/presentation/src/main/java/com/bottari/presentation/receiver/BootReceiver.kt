354ea8b0286ab24d7a5674fe0a3ffd89745033a4
cucumber99 <dlsguq1@gmail.com>
2025-08-11 18:35:33 +0900

    feat: 부팅 이벤트 처리 클래스 구현

---
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/receiver/BootReceiver.kt
@@ -0,0 +1,20 @@
+package com.bottari.presentation.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import com.bottari.presentation.worker.NotificationWorker
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(
+        context: Context,
+        intent: Intent,
+    ) {
+        if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
+            val workRequest = OneTimeWorkRequestBuilder<NotificationWorker>().build()
+            WorkManager.getInstance(context).enqueue(workRequest)
+        }
+    }
+}
