953fd21f33bb944cf19c414d49c8d40f2572c55b
cucumber99 <dlsguq1@gmail.com>
2025-08-11 16:46:14 +0900

    feat: NotificationDatabase 정의

---
+++ android/Bottari/data/src/main/java/com/bottari/data/local/notification/NotificationDatabase.kt
@@ -0,0 +1,34 @@
+package com.bottari.data.local.notification
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.bottari.data.common.util.LocalDateConverter
+import com.bottari.data.common.util.LocalTimeConverter
+import com.bottari.data.model.notification.NotificationEntity
+
+@Database(entities = [NotificationEntity::class], version = 1)
+@TypeConverters(LocalTimeConverter::class, LocalDateConverter::class)
+abstract class NotificationDatabase : RoomDatabase() {
+    abstract fun notificationDao(): NotificationDao
+
+    companion object {
+        private const val DATABASE_NAME = "Notification"
+
+        @Volatile
+        private var instance: NotificationDatabase? = null
+
+        fun getDatabase(context: Context): NotificationDatabase =
+            instance ?: synchronized(this) {
+                Room
+                    .databaseBuilder(
+                        context,
+                        NotificationDatabase::class.java,
+                        DATABASE_NAME,
+                    ).build()
+                    .also { instance = it }
+            }
+    }
+}
