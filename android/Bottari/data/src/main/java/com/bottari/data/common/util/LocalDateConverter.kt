7b6357e87427f92298f5a0c0ba28091574b982aa
cucumber99 <dlsguq1@gmail.com>
2025-08-11 16:45:57 +0900

    feat: LocalDate 및 LocalTime 변환 클래스 구현

---
+++ android/Bottari/data/src/main/java/com/bottari/data/common/util/LocalDateConverter.kt
@@ -0,0 +1,12 @@
+package com.bottari.data.common.util
+
+import androidx.room.TypeConverter
+import java.time.LocalDate
+
+class LocalDateConverter {
+    @TypeConverter
+    fun fromDate(date: LocalDate): String = date.toString()
+
+    @TypeConverter
+    fun toDate(value: String): LocalDate = LocalDate.parse(value)
+}
