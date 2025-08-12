d783bfa4afb06d4ecb453f413c1eca4ff23ff8c3
cucumber99 <dlsguq1@gmail.com>
2025-08-12 15:41:31 +0900

    feat: 팀원 관리 화면 UI State 구현

---
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/view/edit/team/management/TeamManagementUiState.kt
@@ -0,0 +1,14 @@
+package com.bottari.presentation.view.edit.team.management
+
+data class TeamManagementUiState(
+    val isLoading: Boolean = false,
+    val inviteCode: String = "",
+    val teamMemberHeadCount: Int = DEFAULT_VALUE,
+    val maxHeadCount: Int = DEFAULT_VALUE,
+    val hostName: String = "",
+    val memberNicknames: List<String> = emptyList(),
+) {
+    companion object {
+        private const val DEFAULT_VALUE = 0
+    }
+}
