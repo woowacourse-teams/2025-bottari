5f0168c2bb45fdc2eded600548e311e4b392ccdb
민경윤 <ruddbs3308@naver.com>
2025-08-12 15:11:12 +0900

    feat: TeamMember 조회 시 Fetch Join으로 쿼리 최적화

--- backend/bottari/src/main/java/com/bottari/teambottari/repository/TeamMemberRepository.java
+++ backend/bottari/src/main/java/com/bottari/teambottari/repository/TeamMemberRepository.java
@@ -3,8 +3,15 @@ package com.bottari.teambottari.repository;
 import com.bottari.teambottari.domain.TeamMember;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface TeamMemberRepository extends JpaRepository<TeamMember, Long> {
 
+    @Query("""
+            SELECT tm
+            FROM TeamMember tm
+            JOIN FETCH tm.member m
+            WHERE tm.teamBottari.id = :teamBottariId
+           """)
     List<TeamMember> findAllByTeamBottariId(final Long teamBottariId);
 }
