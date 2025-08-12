9406b5e3d328fea2cf15eecb117a21c1be4dbab5
cucumber99 <dlsguq1@gmail.com>
2025-08-12 16:10:51 +0900

    feat: 팀원 관리 화면 구현

--- android/Bottari/presentation/src/main/java/com/bottari/presentation/view/edit/team/management/TeamManagementFragment.kt
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/view/edit/team/management/TeamManagementFragment.kt
@@ -1,13 +1,55 @@
 package com.bottari.presentation.view.edit.team.management
 
+import android.os.Bundle
+import android.view.View
 import androidx.core.os.bundleOf
+import androidx.fragment.app.viewModels
+import com.bottari.presentation.R
 import com.bottari.presentation.common.base.BaseFragment
+import com.bottari.presentation.common.extension.showSnackbar
 import com.bottari.presentation.databinding.FragmentTeamManagementBinding
+import com.bottari.presentation.view.edit.team.management.adapter.TeamMemberAdapter
 
 class TeamManagementFragment :
     BaseFragment<FragmentTeamManagementBinding>(
         FragmentTeamManagementBinding::inflate,
     ) {
+    private val viewModel: TeamManagementViewModel by viewModels {
+        TeamManagementViewModel.Factory(requireArguments().getLong(ARG_TEAM_BOTTARI_ID))
+    }
+    private lateinit var adapter: TeamMemberAdapter
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+        setupObserver()
+        setupListener()
+    }
+
+    private fun setupObserver() {
+        viewModel.uiState.observe(viewLifecycleOwner) { uiState ->
+            adapter = TeamMemberAdapter(uiState.hostName)
+            adapter.submitList(uiState.nonDuplicateNicknames)
+            binding.tvTeamMemberHeadCount.text =
+                getString(
+                    R.string.team_management_member_head_count,
+                    uiState.teamMemberHeadCount,
+                    uiState.maxHeadCount,
+                )
+        }
+        viewModel.uiEvent.observe(viewLifecycleOwner) { uiEvent ->
+            when (uiEvent) {
+                is TeamManagementUiEvent.FetchTeamMembersFailure -> requireView().showSnackbar(R.string.team_management_fetch_failure_text)
+            }
+        }
+    }
+
+    private fun setupListener() {
+        binding.btnClipboard.setOnClickListener { }
+    }
+
     companion object {
         private const val ARG_TEAM_BOTTARI_ID = "ARG_TEAM_BOTTARI_ID"
 
