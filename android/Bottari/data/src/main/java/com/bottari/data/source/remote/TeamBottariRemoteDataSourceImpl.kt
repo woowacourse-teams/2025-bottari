830babc6423095687b0f548d115e65d7a62056c9
cucumber99 <dlsguq1@gmail.com>
2025-08-12 15:16:47 +0900

    refactor: 데이터 소스 네이밍 개선

--- android/Bottari/data/src/main/java/com/bottari/data/source/remote/TeamBottariRemoteDataSourceImpl.kt
+++ android/Bottari/data/src/main/java/com/bottari/data/source/remote/TeamBottariRemoteDataSourceImpl.kt
@@ -5,6 +5,7 @@ import com.bottari.data.common.util.safeApiCall
 import com.bottari.data.model.common.ErrorResponse
 import com.bottari.data.model.team.CreateTeamBottariRequest
 import com.bottari.data.model.team.FetchTeamBottariResponse
+import com.bottari.data.model.team.TeamMembersResponse
 import com.bottari.data.service.TeamBottariService
 
 class TeamBottariRemoteDataSourceImpl(
@@ -25,6 +26,8 @@ class TeamBottariRemoteDataSourceImpl(
         safeApiCall {
             teamBottariService.fetchTeamBottaries()
         }
+
+    override suspend fun fetchTeamMembers(id: Long): Result<TeamMembersResponse> = safeApiCall { teamBottariService.fetchTeamMembers(id) }
 
     companion object {
         private const val HEADER_TEAM_BOTTARI_ID_PREFIX = "/team-bottaries/"
