5c24fe2d42440c46ccdbb6e6d2da27d6040c3ac0
cucumber99 <dlsguq1@gmail.com>
2025-08-12 15:07:10 +0900

    feat: 팀원 정보 리포지토리 선언 및 구현

--- android/Bottari/domain/src/main/java/com/bottari/domain/repository/TeamBottariRepository.kt
+++ android/Bottari/domain/src/main/java/com/bottari/domain/repository/TeamBottariRepository.kt
@@ -1,9 +1,12 @@
 package com.bottari.domain.repository
 
 import com.bottari.domain.model.bottari.TeamBottari
+import com.bottari.domain.model.team.TeamMembers
 
 interface TeamBottariRepository {
     suspend fun createTeamBottari(title: String): Result<Long?>
 
     suspend fun fetchTeamBottaries(): Result<List<TeamBottari>>
+
+    suspend fun fetchTeamMembers(id: Long): Result<TeamMembers>
 }
