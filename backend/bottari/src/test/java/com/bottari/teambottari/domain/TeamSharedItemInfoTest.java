9ea482f326a04e30b8595096ae0e35db5c41394b
sung-june27 <kmymy27@gmail.com>
2025-08-12 14:39:46 +0900

    test: 팀 공통 물품 도메인 테스트 추가 작성

--- backend/bottari/src/test/java/com/bottari/teambottari/domain/TeamSharedItemInfoTest.java
+++ backend/bottari/src/test/java/com/bottari/teambottari/domain/TeamSharedItemInfoTest.java
@@ -1,17 +1,16 @@
 package com.bottari.teambottari.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
-import com.bottari.bottari.domain.Bottari;
-import com.bottari.bottari.domain.BottariItem;
 import com.bottari.error.BusinessException;
-import com.bottari.fixture.BottariFixture;
 import com.bottari.fixture.MemberFixture;
 import com.bottari.fixture.TeamBottariFixture;
 import com.bottari.member.domain.Member;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class TeamSharedItemInfoTest {
@@ -42,5 +41,97 @@ class TeamSharedItemInfoTest {
         assertThatThrownBy(() -> new TeamSharedItemInfo(name, teamBottari))
                 .isInstanceOf(BusinessException.class)
                 .hasMessage("팀 보따리 물품명이 너무 깁니다. - 최대 20자까지 입력 가능합니다.");
+    }
+
+    @DisplayName("보따리 물품을 체크한다.")
+    @Test
+    void check() {
+        // given
+        final Member member = new Member("ssaid", "name");
+        final TeamBottari teamBottari = TeamBottariFixture.TEAM_BOTTARI.get(member);
+        final TeamMember teamMember = new TeamMember(teamBottari, member);
+        final TeamSharedItemInfo teamSharedItemInfo = new TeamSharedItemInfo(member.getName(), teamBottari);
+        final TeamSharedItem teamSharedItem = new TeamSharedItem(teamSharedItemInfo, teamMember);
+
+        // when
+        teamSharedItem.check();
+
+        // then
+        assertThat(teamSharedItem.isChecked()).isTrue();
+    }
+
+    @DisplayName("이미 체크 되어있는 물품을 체크하면, 예외를 던진다.")
+    @Test
+    void check_Exception_AlreadyCheck() {
+        // given
+        final Member member = new Member("ssaid", "name");
+        final TeamBottari teamBottari = TeamBottariFixture.TEAM_BOTTARI.get(member);
+        final TeamMember teamMember = new TeamMember(teamBottari, member);
+        final TeamSharedItemInfo teamSharedItemInfo = new TeamSharedItemInfo(member.getName(), teamBottari);
+        final TeamSharedItem teamSharedItem = new TeamSharedItem(teamSharedItemInfo, teamMember);
+        teamSharedItem.check();
+
+        // when & then
+        assertThatThrownBy(teamSharedItem::check)
+                .isInstanceOf(BusinessException.class)
+                .hasMessage("해당 팀 보따리 물품은 이미 체크되어 있습니다. - 공통");
+    }
+
+    @DisplayName("보따리 물품을 체크 해제한다.")
+    @Test
+    void uncheck() {
+        // given
+        final Member member = new Member("ssaid", "name");
+        final TeamBottari teamBottari = TeamBottariFixture.TEAM_BOTTARI.get(member);
+        final TeamMember teamMember = new TeamMember(teamBottari, member);
+        final TeamSharedItemInfo teamSharedItemInfo = new TeamSharedItemInfo(member.getName(), teamBottari);
+        final TeamSharedItem teamSharedItem = new TeamSharedItem(teamSharedItemInfo, teamMember);
+        teamSharedItem.check();
+
+        // when
+        teamSharedItem.uncheck();
+
+        // then
+        assertThat(teamSharedItem.isChecked()).isFalse();
+    }
+
+    @DisplayName("이미 체크 해제되어있는 물품을 체크 해제하면, 예외를 던진다.")
+    @Test
+    void uncheck_Exception_AlreadyUncheck() {
+        // given
+        final Member member = new Member("ssaid", "name");
+        final TeamBottari teamBottari = TeamBottariFixture.TEAM_BOTTARI.get(member);
+        final TeamMember teamMember = new TeamMember(teamBottari, member);
+        final TeamSharedItemInfo teamSharedItemInfo = new TeamSharedItemInfo(member.getName(), teamBottari);
+        final TeamSharedItem teamSharedItem = new TeamSharedItem(teamSharedItemInfo, teamMember);
+
+        // when & then
+        assertThatThrownBy(teamSharedItem::uncheck)
+                .isInstanceOf(BusinessException.class)
+                .hasMessage("해당 팀 보따리 물품은 이미 체크 해제되어 있습니다. - 공통");
+    }
+
+    @DisplayName("본인의 보따리 물품인지 확인한다.")
+    @ParameterizedTest
+    @CsvSource({
+            "same_ssaid, true",
+            "diff_ssaid, false"
+    })
+    void isOwner(
+            final String ssaid,
+            final boolean expected
+    ) {
+        // given
+        final Member member = new Member("same_ssaid", "name");
+        final TeamBottari teamBottari = TeamBottariFixture.TEAM_BOTTARI.get(member);
+        final TeamMember teamMember = new TeamMember(teamBottari, member);
+        final TeamSharedItemInfo teamSharedItemInfo = new TeamSharedItemInfo(member.getName(), teamBottari);
+        final TeamSharedItem teamSharedItem = new TeamSharedItem(teamSharedItemInfo, teamMember);
+
+        // when
+        final boolean actual = teamSharedItem.isOwner(ssaid);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
     }
 }
