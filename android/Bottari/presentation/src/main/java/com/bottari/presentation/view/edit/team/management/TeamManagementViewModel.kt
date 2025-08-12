5cdf8685e59f456d113c6133e86c6900064414ae
cucumber99 <dlsguq1@gmail.com>
2025-08-12 15:42:20 +0900

    feat: 팀원 관리 화면 ViewModel 구현

---
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/view/edit/team/management/TeamManagementViewModel.kt
@@ -0,0 +1,64 @@
+package com.bottari.presentation.view.edit.team.management
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.bottari.di.UseCaseProvider
+import com.bottari.domain.model.team.TeamMembers
+import com.bottari.domain.usecase.team.FetchTeamMembersUseCase
+import com.bottari.presentation.common.base.BaseViewModel
+
+class TeamManagementViewModel(
+    stateHandle: SavedStateHandle,
+    private val fetchTeamMembersUseCase: FetchTeamMembersUseCase,
+) : BaseViewModel<TeamManagementUiState, TeamManagementUiEvent>(
+        TeamManagementUiState(),
+    ) {
+    private val teamBottariId: Long =
+        stateHandle[KEY_TEAM_BOTTARI_ID] ?: error(ERROR_REQUIRE_TEAM_BOTTARI_ID)
+
+    init {
+        fetchTeamMembers()
+    }
+
+    private fun fetchTeamMembers() {
+        updateState { copy(isLoading = true) }
+        launch {
+            fetchTeamMembersUseCase(teamBottariId)
+                .onSuccess { teamMembers ->
+                    updateState { copyFromTeamMembers(teamMembers) }
+                }.onFailure {
+                    emitEvent(TeamManagementUiEvent.FetchTeamMembersFailure)
+                }
+            updateState { copy(isLoading = false) }
+        }
+    }
+
+    private fun TeamManagementUiState.copyFromTeamMembers(teamMembers: TeamMembers): TeamManagementUiState =
+        copy(
+            inviteCode = teamMembers.inviteCode,
+            teamMemberHeadCount = teamMembers.teamMemberHeadCount.value,
+            maxHeadCount = teamMembers.teamMemberHeadCount.maxValue,
+            hostName = teamMembers.hostName.value,
+            memberNicknames = teamMembers.memberNicknames.map { nickname -> nickname.value },
+        )
+
+    companion object {
+        private const val KEY_TEAM_BOTTARI_ID = "KEY_TEAM_BOTTARI_ID"
+        private const val ERROR_REQUIRE_TEAM_BOTTARI_ID = "[ERROR] 팀 보따리 ID가 존재하지 않습니다."
+
+        fun Factory(teamBottariId: Long): ViewModelProvider.Factory =
+            viewModelFactory {
+                initializer {
+                    val stateHandle = createSavedStateHandle()
+                    stateHandle[KEY_TEAM_BOTTARI_ID] = teamBottariId
+                    TeamManagementViewModel(
+                        stateHandle = stateHandle,
+                        fetchTeamMembersUseCase = UseCaseProvider.fetchTeamMembersUseCase,
+                    )
+                }
+            }
+    }
+}
