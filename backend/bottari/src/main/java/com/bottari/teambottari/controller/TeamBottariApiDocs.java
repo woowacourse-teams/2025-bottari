b5bb832e8d68c80011e1d4c289501914ea50f3ef
Yehyeok Bang <qkddpgur318@gmail.com>
2025-08-12 13:25:36 +0900

    feat: 팀 보따리 생성 API 구현

---
+++ backend/bottari/src/main/java/com/bottari/teambottari/controller/TeamBottariApiDocs.java
@@ -0,0 +1,27 @@
+package com.bottari.teambottari.controller;
+
+import com.bottari.error.ApiErrorCodes;
+import com.bottari.error.ErrorCode;
+import com.bottari.teambottari.dto.CreateTeamBottariRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.ResponseEntity;
+
+public interface TeamBottariApiDocs {
+
+    @Operation(summary = "보따리 생성")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "팀 보따리 생성 성공"),
+    })
+    @ApiErrorCodes({
+            ErrorCode.MEMBER_NOT_FOUND,
+            ErrorCode.TEAM_BOTTARI_ITEM_NAME_BLANK,
+            ErrorCode.TEAM_BOTTARI_TITLE_TOO_LONG
+    })
+    ResponseEntity<Void> create(
+            final CreateTeamBottariRequest request,
+            @Parameter(hidden = true) final String ssaid
+    );
+}
