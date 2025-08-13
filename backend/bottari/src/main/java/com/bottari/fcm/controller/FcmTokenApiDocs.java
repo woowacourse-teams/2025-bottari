48ddfe6681da720ba6c8b7c6da14d25cbfa509cc
wogus <ind07152@naver.com>
2025-08-13 10:39:04 +0900

    feat: FCM 토큰 업데이트 API 문서화

---
+++ backend/bottari/src/main/java/com/bottari/fcm/controller/FcmTokenApiDocs.java
@@ -0,0 +1,26 @@
+package com.bottari.fcm.controller;
+
+import com.bottari.error.ApiErrorCodes;
+import com.bottari.error.ErrorCode;
+import com.bottari.fcm.dto.UpdateFcmRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+
+@Tag(name = "Fcm Token", description = "Fcm Token API")
+public interface FcmTokenApiDocs {
+
+    @Operation(summary = "FCM 토큰 업데이트")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "FCM 토큰 업데이트 성공"),
+    })
+    @ApiErrorCodes({
+            ErrorCode.FCM_TOKEN_NOT_FOUND
+    })
+    ResponseEntity<Void> updateFcmToken(
+            final String ssaid,
+            final UpdateFcmRequest request
+    );
+}
