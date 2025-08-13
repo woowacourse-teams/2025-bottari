f7409e3f433e1aea91a1816d7fca37bdb12f563f
wogus <ind07152@naver.com>
2025-08-13 10:29:17 +0900

    feat: FCM 토큰 업데이트 API 추가

---
+++ backend/bottari/src/main/java/com/bottari/fcm/controller/FcmTokenController.java
@@ -0,0 +1,27 @@
+package com.bottari.fcm.controller;
+
+import com.bottari.config.MemberIdentifier;
+import com.bottari.fcm.dto.UpdateFcmRequest;
+import com.bottari.fcm.service.FcmTokenService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Controller
+@RequiredArgsConstructor
+public class FcmTokenController {
+
+    private final FcmTokenService fcmTokenService;
+
+    @PatchMapping("/fcm")
+    public ResponseEntity<Void> updateFcmToken(
+            @MemberIdentifier final String ssaid,
+            @RequestBody final UpdateFcmRequest request
+    ) {
+        fcmTokenService.updateFcmToken(ssaid, request);
+
+        return ResponseEntity.noContent().build();
+    }
+}
