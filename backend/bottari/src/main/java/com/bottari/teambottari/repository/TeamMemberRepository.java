04d545e323b61ba80d04e5426803505f283f3fa8
Yehyeok Bang <107793780+YehyeokBang@users.noreply.github.com>
2025-08-13 13:21:23 +0900

    [BE] 내 팀 보따리 목록 조회 기능 구현 (#333)
    * docs: API 문서 Tag 수정
    
    * feat: 팀 보따리 목록 조회 기능 구현
    
    * feat: 팀 보따리 목록 조회 API 구현
    
    * fix: 컨벤션 수정
    
    * refactor: 변수명 명확하게 수정
    
    * fix: 수정된 쿼리에서 팀 보따리와 멤버의 조인 조건 개선
    
    * refactor: 프로젝션용 DTO 패키지 위치 변경
    
    * fix: 오타 수정

--- backend/bottari/src/main/java/com/bottari/teambottari/repository/TeamMemberRepository.java
+++ backend/bottari/src/main/java/com/bottari/teambottari/repository/TeamMemberRepository.java
@@ -1,6 +1,8 @@
 package com.bottari.teambottari.repository;
 
+import com.bottari.teambottari.domain.TeamBottari;
 import com.bottari.teambottari.domain.TeamMember;
+import com.bottari.teambottari.repository.dto.TeamBottariMemberCountProjection;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -19,4 +21,20 @@ public interface TeamMemberRepository extends JpaRepository<TeamMember, Long> {
             WHERE tm.teamBottari.id = :teamBottariId
            """)
     List<TeamMember> findAllByTeamBottariId(final Long teamBottariId);
+
+    @Query("""
+            SELECT tm
+            FROM TeamMember tm
+            JOIN FETCH tm.teamBottari t
+            WHERE tm.member.id = :memberId
+           """)
+    List<TeamMember> findAllByMemberId(final Long memberId);
+
+    @Query("""
+            SELECT tm.teamBottari.id AS teamBottariId, COUNT(tm) AS memberCount
+            FROM TeamMember tm
+            WHERE tm.teamBottari IN :teamBottaries
+            GROUP BY tm.teamBottari
+           """)
+    List<TeamBottariMemberCountProjection> countMembersByTeamBottariIn(final List<TeamBottari> teamBottaries);
 }
--- backend/bottari/src/main/java/com/bottari/teambottari/repository/TeamSharedItemRepository.java
+++ backend/bottari/src/main/java/com/bottari/teambottari/repository/TeamSharedItemRepository.java
@@ -1,11 +1,14 @@
 package com.bottari.teambottari.repository;
 
+import com.bottari.teambottari.domain.TeamMember;
 import com.bottari.teambottari.domain.TeamSharedItem;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface TeamSharedItemRepository extends JpaRepository<TeamSharedItem, Long> {
+
+    List<TeamSharedItem> findAllByTeamMemberIn(final List<TeamMember> teamMembers);  
 
     @Query("""
             SELECT tsi
