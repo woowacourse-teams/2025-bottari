cbf06835234babbb27cedcd413276cf9808e36df
wogus <ind07152@naver.com>
2025-08-13 21:13:32 +0900

    feat: fcm 알림 기능 추가

--- backend/bottari/src/main/java/com/bottari/config/FirebaseConfig.java
+++ backend/bottari/src/main/java/com/bottari/config/FirebaseConfig.java
@@ -5,11 +5,13 @@ import com.bottari.error.ErrorCode;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
 import jakarta.annotation.PostConstruct;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -29,5 +31,10 @@ public class FirebaseConfig {
         } catch (IOException e) {
             throw new BusinessException(ErrorCode.FCM_INITIALIZED_FAIL);
         }
+    }
+
+    @Bean
+    public FirebaseMessaging firebaseMessaging() {
+        return FirebaseMessaging.getInstance();
     }
 }
--- backend/bottari/src/main/java/com/bottari/fcm/repository/FcmTokenRepository.java
+++ backend/bottari/src/main/java/com/bottari/fcm/repository/FcmTokenRepository.java
@@ -1,10 +1,21 @@
 package com.bottari.fcm.repository;
 
 import com.bottari.fcm.domain.FcmToken;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface FcmTokenRepository extends JpaRepository<FcmToken, Long> {
 
-    Optional<FcmToken> findByMemberSsaid(String ssaid);
+    Optional<FcmToken> findByMemberSsaid(final String ssaid);
+
+    Optional<FcmToken> findByMemberId(final Long memberId);
+
+    List<FcmToken> findByMemberIdIn(final List<Long> memberIds);
+
+    @Modifying(flushAutomatically = true, clearAutomatically = true)
+    @Query("UPDATE FcmToken f SET f.deletedAt = CURRENT_TIMESTAMP WHERE f.id IN :ids")
+    void deleteByIds(final List<Long> ids);
 }
