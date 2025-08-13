7b24a89c6f714c81b6d89269fb62a88ea04c25a2
Inhyeop Lee <58465973+cucumber99@users.noreply.github.com>
2025-08-13 13:07:22 +0900

    [AN] 알람이 생성되지 않는 문제 해결 | Release v1.0.2 (#345)
    * fix: 알람 생성 및 저장 시 로딩 상태 처리 개선
    
    * build: Version Code 갱신
    
    - Version Code를 4에서 5로 변경합니다

--- android/Bottari/app/build.gradle.kts
+++ android/Bottari/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.bottari.bottari"
         minSdk = 28
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.0.1"
+        versionCode = 5
+        versionName = "1.0.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
