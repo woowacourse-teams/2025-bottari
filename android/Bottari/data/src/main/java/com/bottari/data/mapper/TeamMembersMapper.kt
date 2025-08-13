598f802da296238d32d195225ea8e447add45827
cucumber99 <dlsguq1@gmail.com>
2025-08-13 15:39:46 +0900

    feat: 멤버 현황 매핑 함수 구현

--- android/Bottari/data/src/main/java/com/bottari/data/mapper/TeamMembersMapper.kt
+++ android/Bottari/data/src/main/java/com/bottari/data/mapper/TeamMembersMapper.kt
@@ -1,8 +1,11 @@
 package com.bottari.data.mapper
 
+import com.bottari.data.mapper.BottariItemMapper.toDomain
+import com.bottari.data.model.team.TeamMemberStatusResponse
 import com.bottari.data.model.team.TeamMembersResponse
 import com.bottari.domain.model.member.Nickname
 import com.bottari.domain.model.team.HeadCount
+import com.bottari.domain.model.team.TeamMemberStatus
 import com.bottari.domain.model.team.TeamMembers
 
 object TeamMembersMapper {
@@ -12,5 +15,15 @@ object TeamMembersMapper {
             teamMemberHeadCount = HeadCount(teamMemberCount),
             hostName = Nickname(ownerName),
             memberNicknames = teamMemberNames.map { name -> Nickname(name) },
+        )
+
+    fun TeamMemberStatusResponse.toDomain(): TeamMemberStatus =
+        TeamMemberStatus(
+            nickname = Nickname(nickname),
+            isHost = isOwner,
+            totalItemsCount = totalItemsCount,
+            checkedItemsCount = checkedItemsCount,
+            sharedItems = sharedItems.map { sharedItem -> sharedItem.toDomain() },
+            assignedItems = assignedItems.map { assignedItem -> assignedItem.toDomain() },
         )
 }
