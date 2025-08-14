b3781609b4c4b1e840bbc4b028e657c5c5bbe0d5
Sia <lyl8661@naver.com>
2025-08-14 19:29:02 +0900

    feat: 팀 체크리스트 상세 조회 어댑터 및 뷰홀더 추가

---
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/view/checklist/team/status/adapter/TeamProductStatusDetailAdapter.kt
@@ -0,0 +1,38 @@
+package com.bottari.presentation.view.checklist.team.status.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import com.bottari.presentation.view.checklist.team.status.MemberCheckStatusUiModel
+
+class TeamProductStatusDetailAdapter :
+    ListAdapter<MemberCheckStatusUiModel, TeamProductStatusDetailViewHolder>(
+        DiffUtil,
+    ) {
+    override fun onBindViewHolder(
+        holder: TeamProductStatusDetailViewHolder,
+        position: Int,
+    ) {
+        holder.bind(getItem(position))
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int,
+    ): TeamProductStatusDetailViewHolder = TeamProductStatusDetailViewHolder.from(parent)
+
+    companion object {
+        private val DiffUtil =
+            object : DiffUtil.ItemCallback<MemberCheckStatusUiModel>() {
+                override fun areContentsTheSame(
+                    oldItem: MemberCheckStatusUiModel,
+                    newItem: MemberCheckStatusUiModel,
+                ): Boolean = oldItem == newItem
+
+                override fun areItemsTheSame(
+                    oldItem: MemberCheckStatusUiModel,
+                    newItem: MemberCheckStatusUiModel,
+                ): Boolean = oldItem.name == newItem.name
+            }
+    }
+}
---
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/view/checklist/team/status/adapter/TeamProductStatusDetailViewHolder.kt
@@ -0,0 +1,37 @@
+package com.bottari.presentation.view.checklist.team.status.adapter
+
+import android.content.res.ColorStateList
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.bottari.presentation.R
+import com.bottari.presentation.databinding.ItemChecklistMiniBinding
+import com.bottari.presentation.view.checklist.team.status.MemberCheckStatusUiModel
+
+class TeamProductStatusDetailViewHolder private constructor(
+    private val binding: ItemChecklistMiniBinding,
+) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(item: MemberCheckStatusUiModel) {
+        binding.tvChecklistItemMiniTitle.text = item.name
+        setCheckedStatus(item.checked)
+    }
+
+    private fun setCheckedStatus(isChecked: Boolean) {
+        if (isChecked) {
+            binding.clChecklistItem.backgroundTintList =
+                ColorStateList.valueOf(ContextCompat.getColor(binding.root.context, R.color.primary))
+            return
+        }
+        binding.clChecklistItem.backgroundTintList =
+            ColorStateList.valueOf(ContextCompat.getColor(binding.root.context, R.color.gray_787878))
+    }
+
+    companion object {
+        fun from(parent: ViewGroup): TeamProductStatusDetailViewHolder {
+            val inflater = LayoutInflater.from(parent.context)
+            val binding = ItemChecklistMiniBinding.inflate(inflater, parent, false)
+            return TeamProductStatusDetailViewHolder(binding)
+        }
+    }
+}
