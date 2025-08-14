7198f63458b481e26a93f6283a28ccdee6291b9a
wogus <ind07152@naver.com>
2025-08-14 13:51:31 +0900

    refactor: 정규 표현식 상수화

--- backend/bottari/src/main/java/com/bottari/support/BadWordValidator.java
+++ backend/bottari/src/main/java/com/bottari/support/BadWordValidator.java
@@ -5,9 +5,10 @@ import com.vane.badwordfiltering.BadWordFiltering;
 public final class BadWordValidator {
 
     private static final BadWordFiltering badWordFiltering = new BadWordFiltering();
+    private static final String KOREAN_AND_ENGLISH_ONLY_REGEX = "[^가-힣ㄱ-ㅎㅏ-ㅣa-zA-Z]";
 
     public static boolean hasBadWord(final String input) {
-        final String lettersOnly = input.replaceAll("[^가-힣ㄱ-ㅎㅏ-ㅣa-zA-Z]", "");
+        final String lettersOnly = input.replaceAll(KOREAN_AND_ENGLISH_ONLY_REGEX, "");
 
         return badWordFiltering.check(lettersOnly);
     }
