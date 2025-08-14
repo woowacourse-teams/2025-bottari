46f25682feb96ea12f5170bf1e750b06a0b81ab7
Sia <lyl8661@naver.com>
2025-08-14 11:56:05 +0900

    chore: 불필요한 코드 제거

--- android/Bottari/presentation/src/main/java/com/bottari/presentation/view/checklist/team/MemberStatusFragment.kt
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/view/checklist/team/MemberStatusFragment.kt
@@ -6,13 +6,6 @@ import com.bottari.presentation.common.base.BaseFragment
 import com.bottari.presentation.databinding.FragmentMemberStatusBinding
 
 class MemberStatusFragment : BaseFragment<FragmentMemberStatusBinding>(FragmentMemberStatusBinding::inflate) {
-    override fun onViewCreated(
-        view: View,
-        savedInstanceState: Bundle?,
-    ) {
-        super.onViewCreated(view, savedInstanceState)
-    }
-
     companion object {
         fun newInstance(): MemberStatusFragment = MemberStatusFragment()
     }
--- android/Bottari/presentation/src/main/java/com/bottari/presentation/view/checklist/team/TeamStatusFragment.kt
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/view/checklist/team/TeamStatusFragment.kt
@@ -6,13 +6,6 @@ import com.bottari.presentation.common.base.BaseFragment
 import com.bottari.presentation.databinding.FragmentTeamStatusBinding
 
 class TeamStatusFragment : BaseFragment<FragmentTeamStatusBinding>(FragmentTeamStatusBinding::inflate) {
-    override fun onViewCreated(
-        view: View,
-        savedInstanceState: Bundle?,
-    ) {
-        super.onViewCreated(view, savedInstanceState)
-    }
-
     companion object {
         @JvmStatic
         fun newInstance(): TeamStatusFragment = TeamStatusFragment()
