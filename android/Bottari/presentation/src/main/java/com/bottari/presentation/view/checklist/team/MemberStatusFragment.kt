cd4b2294d3d05645ecdd61e7a4ff1cd39ad3d6e2
Sia <lyl8661@naver.com>
2025-08-14 12:12:54 +0900

    refactor: 불필요한 임포트 제거

--- android/Bottari/presentation/src/main/java/com/bottari/presentation/view/checklist/team/MemberStatusFragment.kt
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/view/checklist/team/MemberStatusFragment.kt
@@ -1,7 +1,5 @@
 package com.bottari.presentation.view.checklist.team
 
-import android.os.Bundle
-import android.view.View
 import com.bottari.presentation.common.base.BaseFragment
 import com.bottari.presentation.databinding.FragmentMemberStatusBinding
 
