cbacf9acfc8c25c6ffff6ebd26474a4093761a21
cucumber99 <dlsguq1@gmail.com>
2025-08-14 17:00:10 +0900

    feat: 공용 물건 어댑터 및 뷰홀더 구현

---
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/view/team/status/adapter/SharedItemAdapter.kt
@@ -0,0 +1,35 @@
+package com.bottari.presentation.view.team.status.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import com.bottari.presentation.model.BottariItemUiModel
+
+class SharedItemAdapter : ListAdapter<BottariItemUiModel, SharedItemViewHolder>(DiffUtil) {
+    override fun onBindViewHolder(
+        holder: SharedItemViewHolder,
+        position: Int,
+    ) {
+        holder.bind(getItem(position))
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int,
+    ): SharedItemViewHolder = SharedItemViewHolder.from(parent)
+
+    companion object {
+        private val DiffUtil =
+            object : DiffUtil.ItemCallback<BottariItemUiModel>() {
+                override fun areContentsTheSame(
+                    oldItem: BottariItemUiModel,
+                    newItem: BottariItemUiModel,
+                ): Boolean = oldItem == newItem
+
+                override fun areItemsTheSame(
+                    oldItem: BottariItemUiModel,
+                    newItem: BottariItemUiModel,
+                ): Boolean = oldItem.id == newItem.id
+            }
+    }
+}
