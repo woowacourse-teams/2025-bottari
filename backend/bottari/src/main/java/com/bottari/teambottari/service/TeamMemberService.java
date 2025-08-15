93f3be1924d86fdd20832d5aacc1b111fdf4432e
민경윤 <144558971+unh6unh6@users.noreply.github.com>
2025-08-15 15:19:08 +0900

    [BE] 팀 보따리 초대 코드로 팀 보따리 참여 기능 구현 (#361)
    * feat: 멤버가 팀 보따리에 참가하는 기능 구현
    
    * feat: 멤버가 팀 보따리에 참가하는 API 구현
    
    * docs: 팀 보따리 참가 API 에러 코드 명세
    
    * refactor: 레포지토리 메서드를 더욱 범용적으로 사용할 수 있도록 변경
    
    * refactor: 불필요한 공백 제거
    
    Co-authored-by: Yehyeok Bang <107793780+YehyeokBang@users.noreply.github.com>
    
    * refactor: 예외 메시지를 더욱 명확하게 변경
    
    * refactor: 팀 보따리 참가 시 팀 공통 아이템 저장 방식 개선
    
    * refactor: 엔드포인트 주소를 더욱 자연스럽게 변경
    
    ---------
    
    Co-authored-by: Yehyeok Bang <107793780+YehyeokBang@users.noreply.github.com>

--- backend/bottari/src/main/java/com/bottari/teambottari/service/TeamMemberService.java
+++ backend/bottari/src/main/java/com/bottari/teambottari/service/TeamMemberService.java
@@ -2,16 +2,21 @@ package com.bottari.teambottari.service;
 
 import com.bottari.error.BusinessException;
 import com.bottari.error.ErrorCode;
+import com.bottari.member.domain.Member;
+import com.bottari.member.repository.MemberRepository;
 import com.bottari.teambottari.domain.TeamAssignedItem;
 import com.bottari.teambottari.domain.TeamBottari;
 import com.bottari.teambottari.domain.TeamMember;
 import com.bottari.teambottari.domain.TeamSharedItem;
+import com.bottari.teambottari.domain.TeamSharedItemInfo;
+import com.bottari.teambottari.dto.JoinTeamBottariRequest;
 import com.bottari.teambottari.dto.ReadTeamMemberInfoResponse;
 import com.bottari.teambottari.dto.ReadTeamMemberStatusResponse;
 import com.bottari.teambottari.dto.TeamMemberItemResponse;
 import com.bottari.teambottari.repository.TeamAssignedItemRepository;
 import com.bottari.teambottari.repository.TeamBottariRepository;
 import com.bottari.teambottari.repository.TeamMemberRepository;
+import com.bottari.teambottari.repository.TeamSharedItemInfoRepository;
 import com.bottari.teambottari.repository.TeamSharedItemRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -26,6 +31,8 @@ public class TeamMemberService {
     private final TeamBottariRepository teamBottariRepository;
     private final TeamSharedItemRepository teamSharedItemRepository;
     private final TeamAssignedItemRepository teamAssignedItemRepository;
+    private final TeamSharedItemInfoRepository teamSharedItemInfoRepository;
+    private final MemberRepository memberRepository;
 
     @Transactional(readOnly = true)
     public ReadTeamMemberInfoResponse getTeamMemberInfoByTeamBottariId(
@@ -54,6 +61,38 @@ public class TeamMemberService {
                 .toList();
     }
 
+    @Transactional
+    public Long joinTeamBottari(
+            final JoinTeamBottariRequest request,
+            final String ssaid
+    ) {
+        final TeamBottari teamBottari = teamBottariRepository.findByInviteCode(request.inviteCode())
+                .orElseThrow(() -> new BusinessException(
+                        ErrorCode.TEAM_BOTTARI_NOT_FOUND,
+                        "해당하는 초대코드 없음"));
+        final Member member = memberRepository.findBySsaid(ssaid)
+                .orElseThrow(() -> new BusinessException(ErrorCode.MEMBER_NOT_FOUND, "등록되지 않은 ssaid입니다."));
+        validateMemberNotInTeam(teamBottari, member);
+        final TeamMember teamMember = new TeamMember(teamBottari, member);
+        teamMemberRepository.save(teamMember);
+        addTeamMemberSharedItems(teamBottari.getId(), teamMember);
+
+        return teamMember.getId();
+    }
+
+    private void addTeamMemberSharedItems(
+            final Long teamBottariId,
+            final TeamMember teamMember
+    ) {
+        final List<TeamSharedItemInfo> sharedItemInfos = teamSharedItemInfoRepository.findAllByTeamBottariId(
+                teamBottariId
+        );
+        final List<TeamSharedItem> teamSharedItems = sharedItemInfos.stream()
+                .map(info -> new TeamSharedItem(info, teamMember))
+                .toList();
+        teamSharedItemRepository.saveAll(teamSharedItems);
+    }
+
     private void validateMemberInTeam(
             final String ssaid,
             final List<TeamMember> teamMembers
@@ -62,6 +101,15 @@ public class TeamMemberService {
                 .filter(teamMember -> teamMember.getMember().isSameBySsaid(ssaid))
                 .findFirst()
                 .orElseThrow(() -> new BusinessException(ErrorCode.MEMBER_NOT_IN_TEAM_BOTTARI));
+    }
+
+    private void validateMemberNotInTeam(
+            final TeamBottari teamBottari,
+            final Member member
+    ) {
+        if (teamMemberRepository.existsByTeamBottariIdAndMemberId(teamBottari.getId(), member.getId())) {
+            throw new BusinessException(ErrorCode.MEMBER_ALREADY_IN_TEAM_BOTTARI);
+        }
     }
 
     private void validateTeamBottariExists(final Long teamBottariId) {
