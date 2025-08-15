7bccb53ad44175b06d3d95f5c55ca9bebd99c670
moondev03 <moondev03@gmail.com>
2025-08-15 20:31:47 +0900

    feat: 팀 보따리 개인 물품 수정 화면 구현
    - 개인 물품 수정 화면(`TeamPersonalItemEditFragment`)을 추가합니다.
    - 개인 물품 수정 화면의 ViewModel(`TeamPersonalItemEditViewModel`)을 구현합니다.
    - 개인 물품 수정 화면의 UI 상태(`TeamPersonalItemEditUiState`) 및 이벤트(`TeamPersonalItemEditEvent`)를 정의합니다.
    - 개인 물품 목록을 표시하는 RecyclerView의 어댑터(`TeamPersonalItemEditAdapter`)와 뷰홀더(`TeamPersonalItemEditViewHolder`)를 구현합니다.
    - 개인 물품 수정 화면의 레이아웃(`fragment_team_personal_item_edit.xml`)을 추가합니다.

---
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/view/edit/team/item/personal/TeamPersonalItemEditViewModel.kt
@@ -0,0 +1,75 @@
+package com.bottari.presentation.view.edit.team.item.personal
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.bottari.di.UseCaseProvider
+import com.bottari.domain.usecase.team.CreateTeamPersonalItemUseCase
+import com.bottari.domain.usecase.team.FetchTeamPersonalItemsUseCase
+import com.bottari.logger.BottariLogger
+import com.bottari.presentation.common.base.BaseViewModel
+import com.bottari.presentation.mapper.TeamBottariMapper.toUiModel
+
+class TeamPersonalItemEditViewModel(
+    stateHandle: SavedStateHandle,
+    private val fetchTeamPersonalItemsUseCase: FetchTeamPersonalItemsUseCase,
+    private val createTeamPersonalItemUseCase: CreateTeamPersonalItemUseCase,
+) : BaseViewModel<TeamPersonalItemEditUiState, TeamPersonalItemEditEvent>(
+        TeamPersonalItemEditUiState(),
+    ) {
+    private val bottariId: Long = stateHandle[KEY_BOTTARI_ID] ?: error(ERROR_BOTTARI_ID)
+
+    init {
+        fetchPersonalItems()
+    }
+
+    fun updateInput(input: String) {
+        BottariLogger.debug(input)
+        updateState { copy(inputText = input) }
+    }
+
+    fun createItem() {
+        if (currentState.isAlreadyExist) return
+        updateState { copy(isLoading = true) }
+
+        launch {
+            createTeamPersonalItemUseCase(bottariId, currentState.inputText)
+                .onSuccess { fetchPersonalItems() }
+                .onFailure { emitEvent(TeamPersonalItemEditEvent.AddItemFailure) }
+
+            updateState { copy(isLoading = false) }
+        }
+    }
+
+    private fun fetchPersonalItems() {
+        updateState { copy(isLoading = true) }
+
+        launch {
+            fetchTeamPersonalItemsUseCase(bottariId)
+                .onSuccess { items -> updateState { copy(personalItems = items.map { it.toUiModel() }) } }
+                .onFailure { emitEvent(TeamPersonalItemEditEvent.FetchTeamPersonalItemsFailure) }
+
+            updateState { copy(isLoading = false, isFetched = true) }
+        }
+    }
+
+    companion object {
+        private const val KEY_BOTTARI_ID = "KEY_BOTTARI_ID"
+        private const val ERROR_BOTTARI_ID = "[ERROR] bottariId가 존재하지 않습니다"
+
+        fun Factory(bottariId: Long): ViewModelProvider.Factory =
+            viewModelFactory {
+                initializer {
+                    val stateHandle = createSavedStateHandle()
+                    stateHandle[KEY_BOTTARI_ID] = bottariId
+                    TeamPersonalItemEditViewModel(
+                        stateHandle,
+                        UseCaseProvider.fetchTeamPersonalItemsUseCase,
+                        UseCaseProvider.createTeamPersonalItemUseCase,
+                    )
+                }
+            }
+    }
+}
