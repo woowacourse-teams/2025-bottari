1852d3cff41acafffe5db75e09cc51635a338774
cucumber99 <dlsguq1@gmail.com>
2025-08-16 17:53:21 +0900

    feat: 보채기 알림 전송 성공 이벤트 추가

--- android/Bottari/presentation/src/main/java/com/bottari/presentation/view/checklist/team/member/TeamMembersStatusUiEvent.kt
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/view/checklist/team/member/TeamMembersStatusUiEvent.kt
@@ -3,5 +3,9 @@ package com.bottari.presentation.view.checklist.team.member
 interface TeamMembersStatusUiEvent {
     data object FetchMembersStatusFailure : TeamMembersStatusUiEvent
 
+    data class SendRemindByMemberMessageSuccess(
+        val nickname: String,
+    ) : TeamMembersStatusUiEvent
+
     data object SendRemindByMemberMessageFailure : TeamMembersStatusUiEvent
 }
