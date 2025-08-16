4f0492d31534f027b33369897548d88b4400d742
cucumber99 <dlsguq1@gmail.com>
2025-08-16 14:42:37 +0900

    feat: 멤버 보채기 관련 UI Event 추가

--- android/Bottari/presentation/src/main/java/com/bottari/presentation/view/checklist/team/member/TeamMembersStatusUiEvent.kt
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/view/checklist/team/member/TeamMembersStatusUiEvent.kt
@@ -2,4 +2,6 @@ package com.bottari.presentation.view.checklist.team.member
 
 interface TeamMembersStatusUiEvent {
     data object FetchMembersStatusFailure : TeamMembersStatusUiEvent
+
+    data object SendRemindByMemberMessageFailure : TeamMembersStatusUiEvent
 }
