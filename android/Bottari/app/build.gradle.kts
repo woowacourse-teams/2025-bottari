a3cc42b464a07c12f535c48167a5abbfbd0b020e
MunJangHun <105299421+moondev03@users.noreply.github.com>
2025-08-16 13:16:05 +0900

    [AN] 알람 편집 화면 위치 설정 Gone 처리 | Release v1.0.4 (#392)
    * fix: 알림 수정 화면에서 위치 정보 숨김 처리
    
    - `fragment_alarm_edit.xml`에서 위치 관련 레이아웃의 `visibility`를 `gone`으로 설정합니다.
    
    * chore: 버전 코드 및 버전 이름 업데이트
    
    - 버전 코드 5 -> 6
    - 버전 이름 1.0.2 -> 1.0.4

--- android/Bottari/app/build.gradle.kts
+++ android/Bottari/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.bottari.bottari"
         minSdk = 28
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.0.2"
+        versionCode = 6
+        versionName = "1.0.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
