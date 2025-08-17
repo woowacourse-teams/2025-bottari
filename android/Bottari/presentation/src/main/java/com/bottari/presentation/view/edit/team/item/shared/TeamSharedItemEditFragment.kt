f9038bc35dabcb25eee5532dc065c86d15f5c4bf
MunJangHun <105299421+moondev03@users.noreply.github.com>
2025-08-17 21:34:18 +0900

    [AN] 팀 보따리 공통 물품 수정 화면 구현 (#412)
    * feat: 팀 보따리 공통 물품 수정 기능 구현
    
    - 팀 공유 물품 수정 화면을 구현합니다.
    - 팀 공유 물품 수정 화면의 아이템 레이아웃을 추가합니다.
    - 팀 공유 물품 수정 화면의 어댑터 및 뷰홀더를 구현합니다.
    - 팀 공유 물품 수정 화면의 UI State, Event, ViewModel을 구현합니다.
    - 팀 공유 물품 수정 화면에서 물품을 추가/삭제하는 기능을 구현합니다.
    - 팀 공유 물품 수정 화면에서 물품 목록을 조회하는 기능을 구현합니다.
    - 팀 공유 물품 수정 화면에서 이미 존재하는 물품인지 확인하는 기능을 구현합니다.
    
    * refactor: 보따리 수정 화면 개인/공용 탭간 중복검사 로직 수정
    
    - 각 탭 별로 중복 검사가 진행되도록 수정합니다.
    - 개인 보따리와 공용 보따리 아이템 리스트 네이밍을 수정합니다.

--- android/Bottari/presentation/src/main/java/com/bottari/presentation/view/edit/team/item/shared/TeamSharedItemEditFragment.kt
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/view/edit/team/item/shared/TeamSharedItemEditFragment.kt
@@ -1,10 +1,83 @@
 package com.bottari.presentation.view.edit.team.item.shared
 
+import android.os.Bundle
+import android.view.View
 import androidx.core.os.bundleOf
-import androidx.fragment.app.Fragment
+import androidx.core.view.isVisible
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bottari.presentation.R
+import com.bottari.presentation.common.base.BaseFragment
+import com.bottari.presentation.common.extension.showSnackbar
+import com.bottari.presentation.databinding.FragmentTeamSharedItemEditBinding
+import com.bottari.presentation.model.BottariItemTypeUiModel
+import com.bottari.presentation.view.edit.team.item.main.TeamItemEditUiEvent
+import com.bottari.presentation.view.edit.team.item.main.TeamItemEditUiState
+import com.bottari.presentation.view.edit.team.item.main.TeamItemEditViewModel
+import com.bottari.presentation.view.edit.team.item.shared.adapter.TeamSharedItemEditAdapter
 
-class TeamSharedItemEditFragment : Fragment() {
-    private val bottariId: Long by lazy { requireArguments().getLong(ARG_BOTTARI_ID) }
+class TeamSharedItemEditFragment :
+    BaseFragment<FragmentTeamSharedItemEditBinding>(FragmentTeamSharedItemEditBinding::inflate),
+    TeamSharedItemEditAdapter.TeamSharedItemEditEventListener {
+    private val parentViewModel: TeamItemEditViewModel by viewModels(
+        ownerProducer = { requireParentFragment() },
+    )
+    private val viewModel: TeamSharedItemEditViewModel by viewModels {
+        TeamSharedItemEditViewModel.Factory(
+            requireArguments().getLong(ARG_BOTTARI_ID),
+        )
+    }
+    private val adapter: TeamSharedItemEditAdapter by lazy { TeamSharedItemEditAdapter(this) }
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+        setupObserver()
+        setupUI()
+    }
+
+    override fun onClickDelete(itemId: Long) {
+        viewModel.deleteItem(itemId)
+    }
+
+    private fun setupObserver() {
+        parentViewModel.uiEvent.observe(viewLifecycleOwner, ::handleParentUiEvent)
+        parentViewModel.uiState.observe(viewLifecycleOwner, ::handleParentUiState)
+        viewModel.uiState.observe(viewLifecycleOwner, ::handleUiState)
+        viewModel.uiEvent.observe(viewLifecycleOwner, ::handleUiEvent)
+    }
+
+    private fun setupUI() {
+        binding.rvTeamSharedItemEdit.adapter = adapter
+        binding.rvTeamSharedItemEdit.layoutManager = LinearLayoutManager(requireContext())
+    }
+
+    private fun handleParentUiState(uiState: TeamItemEditUiState) {
+        if (uiState.currentTabType != BottariItemTypeUiModel.SHARED) return
+        viewModel.updateInput(uiState.itemInputText)
+    }
+
+    private fun handleParentUiEvent(uiEvent: TeamItemEditUiEvent) {
+        if (uiEvent !is TeamItemEditUiEvent.CreateTeamSharedItem) return
+        viewModel.createItem()
+    }
+
+    private fun handleUiState(uiState: TeamSharedItemEditUiState) {
+        toggleLoadingIndicator(uiState.isLoading)
+        parentViewModel.updateIsAlreadyExistState(uiState.isAlreadyExist)
+        binding.emptyView.root.isVisible = uiState.isEmpty
+        adapter.submitList(uiState.sharedItems)
+    }
+
+    private fun handleUiEvent(uiEvent: TeamSharedItemEditEvent) {
+        when (uiEvent) {
+            TeamSharedItemEditEvent.FetchTeamPersonalItemsFailure -> requireView().showSnackbar(R.string.common_fetch_failure_text)
+            TeamSharedItemEditEvent.AddItemFailure -> requireView().showSnackbar(R.string.common_save_failure_text)
+            TeamSharedItemEditEvent.DeleteItemFailure -> requireView().showSnackbar(R.string.common_save_failure_text)
+        }
+    }
 
     companion object {
         private const val ARG_BOTTARI_ID = "ARG_BOTTARI_ID"
