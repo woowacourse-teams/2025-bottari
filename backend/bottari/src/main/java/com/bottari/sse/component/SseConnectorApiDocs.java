c1b801608b175104dc08e90985f1c1864b79048b
Yehyeok Bang <qkddpgur318@gmail.com>
2025-08-17 10:55:41 +0900

    docs: SSE 연결 API 문서에 필요한 헤더 명시

--- backend/bottari/src/main/java/com/bottari/sse/component/SseConnectorApiDocs.java
+++ backend/bottari/src/main/java/com/bottari/sse/component/SseConnectorApiDocs.java
@@ -2,15 +2,30 @@ package com.bottari.sse.component;
 
 import com.bottari.error.ApiErrorCodes;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @Tag(name = "SSE", description = "SSE 관련 API")
 public interface SseConnectorApiDocs {
 
-    @Operation(summary = "팀 보따리 SSE 연결")
+    @Operation(
+            summary = "팀 보따리 SSE 연결",
+            parameters = {
+                    @Parameter(
+                            name = HttpHeaders.ACCEPT,
+                            in = ParameterIn.HEADER,
+                            required = true,
+                            description = "SSE 스트림 연결을 위해 'text/event-stream' 값을 명시해야 합니다.",
+                            schema = @Schema(type = "string", defaultValue = "text/event-stream")
+                    )
+            }
+    )
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "SSE 연결 성공(스트림 시작)"),
     })
