00e9ae1f8e67c346b73c89f62dcafa527bb27716
Yehyeok Bang <107793780+YehyeokBang@users.noreply.github.com>
2025-08-18 13:46:10 +0900

    [BE] 팀 보따리 물품 타입별 조회 기능 구현 (#411)
    * feat: 팀 보따리 공통 뭂품 조회 기능 구현
    
    * feat: 팀 보따리 공통 뭂품 조회 API 구현
    
    * feat: 팀 보따리 담당 물품 목록 조회 기능 구현
    
    - 담당 멤버 목록까지 함께 반환
    
    * feat: 팀 보따리 담당 물품 목록 조회 API 구현
    
    * feat: 팀 보따리 개인 물품 목록 조회 기능 구현
    
    * feat: 팀 보따리 개인 물품 목록 조회 API 구현
    
    * refactor: 중복 메서드 분리
    
    * fix: 불필요한 개행 삭제
    
    * refactor: 가독성을 위한 메서드 분리
    
    * refactor: 불필요한 static 제거

--- backend/bottari/src/main/java/com/bottari/teambottari/controller/TeamBottariItemController.java
+++ backend/bottari/src/main/java/com/bottari/teambottari/controller/TeamBottariItemController.java
@@ -3,11 +3,15 @@ package com.bottari.teambottari.controller;
 import com.bottari.config.MemberIdentifier;
 import com.bottari.teambottari.dto.CreateTeamAssignedItemRequest;
 import com.bottari.teambottari.dto.CreateTeamItemRequest;
+import com.bottari.teambottari.dto.ReadAssignedItemResponse;
+import com.bottari.teambottari.dto.ReadPersonalItemResponse;
+import com.bottari.teambottari.dto.ReadSharedItemResponse;
 import com.bottari.teambottari.dto.ReadTeamItemStatusResponse;
 import com.bottari.teambottari.dto.TeamItemTypeRequest;
 import com.bottari.teambottari.dto.TeamMemberChecklistResponse;
 import com.bottari.teambottari.service.TeamItemFacade;
 import java.net.URI;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -23,6 +27,39 @@ import org.springframework.web.bind.annotation.RestController;
 public class TeamBottariItemController implements TeamBottariItemApiDocs {
 
     private final TeamItemFacade teamItemFacade;
+
+    @GetMapping("/team-bottaries/{teamBottariId}/shared-items")
+    @Override
+    public ResponseEntity<List<ReadSharedItemResponse>> readSharedItems(
+            @PathVariable final Long teamBottariId,
+            @MemberIdentifier final String ssaid
+    ) {
+        final List<ReadSharedItemResponse> responses = teamItemFacade.getSharedItems(teamBottariId, ssaid);
+
+        return ResponseEntity.ok(responses);
+    }
+
+    @GetMapping("/team-bottaries/{teamBottariId}/assigned-items")
+    @Override
+    public ResponseEntity<List<ReadAssignedItemResponse>> readAssignedItems(
+            @PathVariable final Long teamBottariId,
+            @MemberIdentifier final String ssaid
+    ) {
+        final List<ReadAssignedItemResponse> responses = teamItemFacade.getAssignedItems(teamBottariId, ssaid);
+
+        return ResponseEntity.ok(responses);
+    }
+
+    @GetMapping("/team-bottaries/{teamBottariId}/personal-items")
+    @Override
+    public ResponseEntity<List<ReadPersonalItemResponse>> readPersonalItems(
+            @PathVariable final Long teamBottariId,
+            @MemberIdentifier final String ssaid
+    ) {
+        final List<ReadPersonalItemResponse> responses = teamItemFacade.getPersonalItems(teamBottariId, ssaid);
+
+        return ResponseEntity.ok(responses);
+    }
 
     @PostMapping("/team-bottaries/{teamBottariId}/shared-items")
     @Override
