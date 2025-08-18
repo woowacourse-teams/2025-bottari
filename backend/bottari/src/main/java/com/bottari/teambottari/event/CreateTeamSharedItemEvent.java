63a784b9481ea2d8eb7eb2d2679bbd7cf0472c70
sung-june27 <kmymy27@gmail.com>
2025-08-18 17:20:56 +0900

    refactor: 생성자 형태 변경

--- backend/bottari/src/main/java/com/bottari/teambottari/event/CreateTeamSharedItemEvent.java
+++ backend/bottari/src/main/java/com/bottari/teambottari/event/CreateTeamSharedItemEvent.java
@@ -2,22 +2,13 @@ package com.bottari.teambottari.event;
 
 import com.bottari.support.CustomApplicationEvent;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 @Getter
 public class CreateTeamSharedItemEvent extends CustomApplicationEvent {
 
     private final Long teamBottariId;
     private final Long infoId;
     private final String name;
-
-    public CreateTeamSharedItemEvent(
-            final Long teamBottariId,
-            final Long infoId,
-            final String name
-    ) {
-        super();
-        this.teamBottariId = teamBottariId;
-        this.infoId = infoId;
-        this.name = name;
-    }
 }
--- backend/bottari/src/main/java/com/bottari/teambottari/event/DeleteTeamSharedItemEvent.java
+++ backend/bottari/src/main/java/com/bottari/teambottari/event/DeleteTeamSharedItemEvent.java
@@ -2,22 +2,13 @@ package com.bottari.teambottari.event;
 
 import com.bottari.support.CustomApplicationEvent;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 @Getter
 public class DeleteTeamSharedItemEvent extends CustomApplicationEvent {
 
     private final Long teamBottariId;
     private final Long infoId;
     private final String name;
-
-    public DeleteTeamSharedItemEvent(
-            final Long teamBottariId,
-            final Long infoId,
-            final String name
-    ) {
-        super();
-        this.teamBottariId = teamBottariId;
-        this.infoId = infoId;
-        this.name = name;
-    }
 }
