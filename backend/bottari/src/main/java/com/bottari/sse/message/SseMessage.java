5a073166949d6efcc5aad2f0ed3baf602b9e532e
민경윤 <ruddbs3308@naver.com>
2025-08-18 16:51:44 +0900

    refactor: 스프링 이벤트와 SSE 이벤트 페이로드에 이벤트 발행 시간을 추가하도록 변경

--- backend/bottari/src/main/java/com/bottari/sse/message/SseMessage.java
+++ backend/bottari/src/main/java/com/bottari/sse/message/SseMessage.java
@@ -1,10 +1,12 @@
 package com.bottari.sse.message;
 
+import java.time.LocalDateTime;
 import lombok.Getter;
 
 @Getter
 public class SseMessage {
 
+    private final LocalDateTime publishedAt;
     private final String resource;
     private final String event;
     private final Object data;
@@ -14,6 +16,7 @@ public class SseMessage {
             final SseEventType event,
             final Object data
     ) {
+        this.publishedAt = LocalDateTime.now();
         this.resource = resource.name();
         this.event = event.name();
         this.data = data;
