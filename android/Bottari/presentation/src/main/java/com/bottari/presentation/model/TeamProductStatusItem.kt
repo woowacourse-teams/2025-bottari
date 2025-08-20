4d33f502388c26fce268d46fcc606fc3b2a85d04
Sia <lyl8661@naver.com>
2025-08-20 14:25:18 +0900

    feat: 아이템에 해당하는 인물이 모두 체크한 경우 보채기 버튼 미노출

--- android/Bottari/presentation/src/main/java/com/bottari/presentation/model/TeamProductStatusItem.kt
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/model/TeamProductStatusItem.kt
@@ -9,7 +9,10 @@ data class TeamBottariProductStatusUiModel(
     val checkItemsCount: Int,
     val totalItemsCount: Int,
     val type: BottariItemTypeUiModel,
-) : TeamProductStatusItem
+) : TeamProductStatusItem {
+    val isAllChecked: Boolean =
+        this.memberCheckStatus.all { memberCheckStatus -> memberCheckStatus.checked }
+}
 
 data class TeamChecklistTypeUiModel(
     val type: BottariItemTypeUiModel,
