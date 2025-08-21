c1274f01a5e15fe59f2818cf169c28c1c9d9e968
Sia <lyl8661@naver.com>
2025-08-21 18:02:45 +0900

    fix: debouncedSendRemindByItem(Unit) 변경

--- android/Bottari/presentation/src/main/java/com/bottari/presentation/view/checklist/team/main/status/TeamBottariStatusFragment.kt
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/view/checklist/team/main/status/TeamBottariStatusFragment.kt
@@ -78,7 +78,7 @@ class TeamBottariStatusFragment :
 
     private fun setupListener() {
         binding.btnTeamBottariItemSendHurryUp.setOnClickListener {
-            viewModel.debouncedSendRemindByItem()
+            viewModel.debouncedSendRemindByItem(Unit)
         }
     }
 
