207f6168590da4f52576cde471dc1aedcfc2faed
cucumber99 <dlsguq1@gmail.com>
2025-08-21 13:48:22 +0900

    feat: 멤버 현황 SSE 연동

--- android/Bottari/presentation/src/main/java/com/bottari/presentation/view/checklist/team/main/member/TeamMembersStatusViewModel.kt
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/view/checklist/team/main/member/TeamMembersStatusViewModel.kt
@@ -6,18 +6,32 @@ import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.bottari.di.UseCaseProvider
+import com.bottari.domain.model.event.EventState
+import com.bottari.domain.usecase.event.ConnectTeamEventUseCase
+import com.bottari.domain.usecase.event.DisconnectTeamEventUseCase
 import com.bottari.domain.usecase.member.GetMemberIdUseCase
 import com.bottari.domain.usecase.team.FetchTeamMembersStatusUseCase
 import com.bottari.domain.usecase.team.SendRemindByMemberMessageUseCase
 import com.bottari.presentation.common.base.BaseViewModel
 import com.bottari.presentation.mapper.TeamMembersMapper.toUiModel
 import com.bottari.presentation.model.TeamMemberUiModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.filterIsInstance
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 
 class TeamMembersStatusViewModel(
     stateHandle: SavedStateHandle,
     private val fetchTeamMembersStatusUseCase: FetchTeamMembersStatusUseCase,
     private val sendRemindByMemberMessageUseCase: SendRemindByMemberMessageUseCase,
     private val getMemberIdUseCase: GetMemberIdUseCase,
+    private val connectTeamEventUseCase: ConnectTeamEventUseCase,
+    private val disconnectTeamEventUseCase: DisconnectTeamEventUseCase,
 ) : BaseViewModel<TeamMembersStatusUiState, TeamMembersStatusUiEvent>(
         TeamMembersStatusUiState(),
     ) {
@@ -26,23 +40,12 @@ class TeamMembersStatusViewModel(
 
     init {
         fetchMemberId()
+        handleEvent()
     }
 
-    private fun fetchTeamMembersStatus() {
-        val myId = currentState.myId
-        updateState { copy(isLoading = true) }
-        launch {
-            fetchTeamMembersStatusUseCase(teamBottariId)
-                .onSuccess { membersStatus ->
-                    val memberStatusUiModel = membersStatus.map { status -> status.toUiModel(myId) }
-                    updateState {
-                        copy(
-                            membersStatus = memberStatusUiModel,
-                        )
-                    }
-                }.onFailure { emitEvent(TeamMembersStatusUiEvent.FetchMembersStatusFailure) }
-            updateState { copy(isLoading = false) }
-        }
+    override fun onCleared() {
+        super.onCleared()
+        CoroutineScope(Dispatchers.IO).launch { disconnectTeamEventUseCase() }
     }
 
     fun sendRemindMessage(member: TeamMemberUiModel) {
@@ -69,9 +72,39 @@ class TeamMembersStatusViewModel(
         }
     }
 
+    @OptIn(FlowPreview::class)
+    private fun handleEvent() {
+        launch {
+            connectTeamEventUseCase(teamBottariId)
+                .filterIsInstance<EventState.OnEvent>()
+                .map { event -> event.data }
+                .debounce(DEBOUNCE_DELAY)
+                .onEach { fetchTeamMembersStatus() }
+                .launchIn(this)
+        }
+    }
+
+    private fun fetchTeamMembersStatus() {
+        val myId = currentState.myId
+        updateState { copy(isLoading = true) }
+        launch {
+            fetchTeamMembersStatusUseCase(teamBottariId)
+                .onSuccess { membersStatus ->
+                    val memberStatusUiModel = membersStatus.map { status -> status.toUiModel(myId) }
+                    updateState {
+                        copy(
+                            membersStatus = memberStatusUiModel,
+                        )
+                    }
+                }.onFailure { emitEvent(TeamMembersStatusUiEvent.FetchMembersStatusFailure) }
+            updateState { copy(isLoading = false) }
+        }
+    }
+
     companion object {
         private const val KEY_TEAM_BOTTARI_ID = "KEY_TEAM_BOTTARI_ID"
         private const val ERROR_REQUIRE_TEAM_BOTTARI_ID = "[ERROR] 팀 보따리 ID가 존재하지 않습니다."
+        private const val DEBOUNCE_DELAY = 500L
 
         fun Factory(id: Long): ViewModelProvider.Factory =
             viewModelFactory {
@@ -83,6 +116,8 @@ class TeamMembersStatusViewModel(
                         fetchTeamMembersStatusUseCase = UseCaseProvider.fetchTeamMembersStatusUseCase,
                         sendRemindByMemberMessageUseCase = UseCaseProvider.sendRemindByMemberMessageUseCase,
                         getMemberIdUseCase = UseCaseProvider.getMemberIdUseCase,
+                        connectTeamEventUseCase = UseCaseProvider.connectTeamEventUseCase,
+                        disconnectTeamEventUseCase = UseCaseProvider.disconnectTeamEventUseCase,
                     )
                 }
             }
