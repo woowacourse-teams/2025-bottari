cfa38cb0bc667657f7d251157db6e6fab0d016ed
sung-june27 <kmymy27@gmail.com>
2025-08-21 18:02:56 +0900

    chore: 주석 제거

--- backend/bottari/src/main/java/com/bottari/fcm/repository/FcmTokenRepository.java
+++ backend/bottari/src/main/java/com/bottari/fcm/repository/FcmTokenRepository.java
@@ -16,7 +16,6 @@ public interface FcmTokenRepository extends JpaRepository<FcmToken, Long> {
     List<FcmToken> findByMemberIdIn(final List<Long> memberIds);
 
     @Modifying(flushAutomatically = true, clearAutomatically = true)
-//    @Modifying
     @Query("""
             UPDATE FcmToken f
             SET f.deletedAt = CURRENT_TIMESTAMP
