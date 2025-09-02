c3ee055758b0040348103b9374b8aa336a7a81b7
Sia <lyl8661@naver.com>
2025-09-02 17:25:54 +0900

    refactor: 보따리 체크리스트 패치 응답 데이터에 SerialName 추가

--- android/Bottari/data/src/main/java/com/bottari/data/model/team/FetchTeamBottariChecklistResponse.kt
+++ android/Bottari/data/src/main/java/com/bottari/data/model/team/FetchTeamBottariChecklistResponse.kt
@@ -1,11 +1,15 @@
 package com.bottari.data.model.team
 
 import com.bottari.data.model.item.FetchChecklistResponse
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class FetchTeamBottariChecklistResponse(
+    @SerialName("sharedItems")
     val sharedItems: List<FetchChecklistResponse>,
+    @SerialName("assignedItems")
     val assignedItems: List<FetchChecklistResponse>,
+    @SerialName("personalItems")
     val personalItems: List<FetchChecklistResponse>,
 )
