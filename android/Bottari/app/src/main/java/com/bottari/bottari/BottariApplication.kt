9c2131246b5999967366434a49c9f04728e934df
cucumber99 <dlsguq1@gmail.com>
2025-09-04 17:21:41 +0900

    feat: WorkerFactory 초기화

--- android/Bottari/app/src/main/java/com/bottari/bottari/BottariApplication.kt
+++ android/Bottari/app/src/main/java/com/bottari/bottari/BottariApplication.kt
@@ -1,16 +1,38 @@
 package com.bottari.bottari
 
 import android.app.Application
+import androidx.work.Configuration
+import androidx.work.DelegatingWorkerFactory
 import com.bottari.di.ApplicationContextProvider
+import com.bottari.di.usecase.CommonUseCaseProvider
 import com.bottari.logger.BottariLogger
+import com.bottari.presentation.worker.NotificationWorkerFactory
 
-class BottariApplication : Application() {
+class BottariApplication :
+    Application(),
+    Configuration.Provider {
     override fun onCreate() {
         super.onCreate()
         BottariLogger.init(this)
         ApplicationContextProvider.init(this)
         BottariLogger.global(APPLICATION_INIT_MESSAGE)
     }
+
+    override val workManagerConfiguration: Configuration
+        get() {
+            val workerFactory =
+                DelegatingWorkerFactory().apply {
+                    addFactory(
+                        NotificationWorkerFactory(
+                            getNotificationsUseCase = CommonUseCaseProvider.getNotificationsUseCase,
+                        ),
+                    )
+                }
+            return Configuration
+                .Builder()
+                .setWorkerFactory(workerFactory)
+                .build()
+        }
 
     companion object {
         private const val APPLICATION_INIT_MESSAGE = "BOTTARI APPLICATION INIT"
