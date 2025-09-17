8c8e6cc31189aca9871920c905d64831dea92e81
moondev03 <moondev03@gmail.com>
2025-09-17 16:52:06 +0900

    feat: API 요청 결과를 나타내는 BottariResult 구현
    API 요청의 성공, API 에러, 네트워크 에러를 나타내는 `BottariResult` sealed interface를 추가합니다.

---
+++ android/Bottari/domain/src/main/java/com/bottari/domain/model/exception/BottariResult.kt
@@ -0,0 +1,30 @@
+package com.bottari.domain.model.exception
+
+sealed interface BottariResult<T> {
+    data class Success<T>(
+        val data: T,
+    ) : BottariResult<T>
+
+    data class ApiError<T>(
+        val exception: BottariException,
+    ) : BottariResult<T>
+
+    data class NetworkError<T>(
+        val throwable: Throwable,
+    ) : BottariResult<T>
+}
+
+inline fun <T> BottariResult<T>.onSuccess(action: (value: T) -> Unit): BottariResult<T> {
+    if (this is BottariResult.Success) action(data)
+    return this
+}
+
+inline fun <T> BottariResult<T>.onApiError(action: (BottariException) -> Unit): BottariResult<T> {
+    if (this is BottariResult.ApiError) action(exception)
+    return this
+}
+
+inline fun <T> BottariResult<T>.onNetworkError(action: (Throwable) -> Unit): BottariResult<T> {
+    if (this is BottariResult.NetworkError) action(throwable)
+    return this
+}
