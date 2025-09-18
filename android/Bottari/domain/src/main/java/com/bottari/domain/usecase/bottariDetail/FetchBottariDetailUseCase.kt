267d85696896c460ca20647807cfe448bd9edc64
moondev03 <moondev03@gmail.com>
2025-09-18 16:40:55 +0900

    refactor: FetchBottariDetailUseCase의 반환 타입을 Result에서 BottariResult로 변경

--- android/Bottari/domain/src/main/java/com/bottari/domain/usecase/bottariDetail/FetchBottariDetailUseCase.kt
+++ android/Bottari/domain/src/main/java/com/bottari/domain/usecase/bottariDetail/FetchBottariDetailUseCase.kt
@@ -1,10 +1,11 @@
 package com.bottari.domain.usecase.bottariDetail
 
 import com.bottari.domain.model.bottari.Bottari
+import com.bottari.domain.model.exception.BottariResult
 import com.bottari.domain.repository.BottariRepository
 
 class FetchBottariDetailUseCase(
     private val bottariRepository: BottariRepository,
 ) {
-    suspend operator fun invoke(id: Long): Result<Bottari> = bottariRepository.fetchBottariDetail(id)
+    suspend operator fun invoke(id: Long): BottariResult<Bottari> = bottariRepository.fetchBottariDetail(id)
 }
