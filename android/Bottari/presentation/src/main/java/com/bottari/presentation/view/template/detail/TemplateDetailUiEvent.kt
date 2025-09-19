5cd664efa4da754fb015435e053555beee8d8136
moondev03 <moondev03@gmail.com>
2025-09-19 15:08:49 +0900

    refactor: 보따리 템플릿 생성 예외 처리 세분화

--- android/Bottari/presentation/src/main/java/com/bottari/presentation/view/template/detail/TemplateDetailUiEvent.kt
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/view/template/detail/TemplateDetailUiEvent.kt
@@ -1,11 +1,21 @@
 package com.bottari.presentation.view.template.detail
 
 sealed interface TemplateDetailUiEvent {
-    data object FetchBottariDetailFailure : TemplateDetailUiEvent
+    sealed interface FetchBottariDetailFailure : TemplateDetailUiEvent {
+        data object NotFoundException : FetchBottariDetailFailure
+
+        data object UnexpectedException : FetchBottariDetailFailure
+    }
 
     data class TakeBottariTemplateSuccess(
         val bottariId: Long?,
     ) : TemplateDetailUiEvent
 
-    data object TakeBottariTemplateFailure : TemplateDetailUiEvent
+    sealed interface TakeBottariTemplateFailure : TemplateDetailUiEvent {
+        data object NotFoundException : TakeBottariTemplateFailure
+
+        data object InvalidException : TakeBottariTemplateFailure
+
+        data object UnexpectedException : TakeBottariTemplateFailure
+    }
 }
