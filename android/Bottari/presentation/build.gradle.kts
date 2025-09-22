7052d59e0577f82ca5e77f21b9b932a17d4fbc57
Inhyeop Lee <58465973+cucumber99@users.noreply.github.com>
2025-09-22 16:58:09 +0900

    [AN] Compose 개발 환경 설정 (#531)
    * build: Compose 관련 의존성 추가
    
    * chore: .editorconfig 파일 추가
    
    * build: Compose 관련 의존성 추가
    
    * chore: .editorconfig 파일 추가
    
    * build: Gradle Plugin 버전 변경

--- android/Bottari/presentation/build.gradle.kts
+++ android/Bottari/presentation/build.gradle.kts
@@ -3,6 +3,7 @@ import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
     id("kotlin-parcelize")
 }
 
@@ -73,6 +74,7 @@ android {
     buildFeatures {
         viewBinding = true
         buildConfig = true
+        compose = true
     }
 }
 
@@ -96,6 +98,14 @@ dependencies {
     implementation(libs.androidx.work.runtime.ktx)
     implementation(libs.androidx.core.splashscreen)
 
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+
     implementation(libs.material)
     implementation(libs.cardstackview)
     implementation(libs.flexbox)
@@ -108,6 +118,10 @@ dependencies {
     implementation(libs.firebase.messaging)
 
     testImplementation(libs.bundles.test)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
 }
