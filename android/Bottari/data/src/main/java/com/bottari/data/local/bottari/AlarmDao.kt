b57faa07bf4e57ff56016c733b98a8776e52471e
cucumber99 <dlsguq1@gmail.com>
2025-09-23 10:26:24 +0900

    feat: 알람 DAO 정의

---
+++ android/Bottari/data/src/main/java/com/bottari/data/local/bottari/AlarmDao.kt
@@ -0,0 +1,21 @@
+package com.bottari.data.local.bottari
+
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy.Companion.REPLACE
+import androidx.room.Query
+import com.bottari.data.model.local.bottari.AlarmEntity
+import kotlinx.coroutines.flow.Flow
+
+interface AlarmDao {
+    @Query("SELECT * FROM Alarms WHERE bottariId = :bottariId")
+    fun fetchAlarm(bottariId: Long): Flow<AlarmEntity>
+
+    @Insert(onConflict = REPLACE)
+    suspend fun saveAlarm(alarm: AlarmEntity)
+
+    @Query("UPDATE Alarms SET isActive = :isActive WHERE id = :id")
+    suspend fun updateAlarmActivate(
+        id: Long,
+        isActive: Boolean,
+    )
+}
