447b1cde4842095b95259352921489aa631de0c6
cucumber99 <dlsguq1@gmail.com>
2025-09-23 15:26:20 +0900

    feat: 보따리 관련 UseCase 의존성 주입 개선

--- android/Bottari/di/src/main/java/com/bottari/di/usecase/BottariUseCaseProvider.kt
+++ android/Bottari/di/src/main/java/com/bottari/di/usecase/BottariUseCaseProvider.kt
@@ -3,19 +3,25 @@ package com.bottari.di.usecase
 import com.bottari.di.RepositoryProvider
 import com.bottari.domain.usecase.bottari.CreateBottariUseCase
 import com.bottari.domain.usecase.bottari.DeleteBottariUseCase
+import com.bottari.domain.usecase.bottari.FetchBottariDetailUseCase
 import com.bottari.domain.usecase.bottari.FetchBottariDetailsUseCase
 import com.bottari.domain.usecase.bottari.FetchBottariesUseCase
 import com.bottari.domain.usecase.bottari.SaveBottariTitleUseCase
-import com.bottari.domain.usecase.bottariDetail.FetchBottariDetailUseCase
 
 object BottariUseCaseProvider {
     val fetchBottariesUseCase: FetchBottariesUseCase by lazy {
         FetchBottariesUseCase(
-            RepositoryProvider.bottariRepository,
+            bottariRepository = RepositoryProvider.bottariRepository,
+            bottariItemRepository = RepositoryProvider.bottariItemRepository,
+            alarmRepository = RepositoryProvider.alarmRepository,
         )
     }
     val fetchBottariDetailUseCase: FetchBottariDetailUseCase by lazy {
-        FetchBottariDetailUseCase(RepositoryProvider.bottariRepository)
+        FetchBottariDetailUseCase(
+            bottariRepository = RepositoryProvider.bottariRepository,
+            bottariItemRepository = RepositoryProvider.bottariItemRepository,
+            alarmRepository = RepositoryProvider.alarmRepository,
+        )
     }
     val fetchBottariDetailsUseCase by lazy {
         FetchBottariDetailsUseCase(
@@ -32,7 +38,7 @@ object BottariUseCaseProvider {
             RepositoryProvider.bottariRepository,
         )
     }
-    val SaveBottariTitleUseCase: SaveBottariTitleUseCase by lazy {
+    val saveBottariTitleUseCase: SaveBottariTitleUseCase by lazy {
         SaveBottariTitleUseCase(
             RepositoryProvider.bottariRepository,
         )
