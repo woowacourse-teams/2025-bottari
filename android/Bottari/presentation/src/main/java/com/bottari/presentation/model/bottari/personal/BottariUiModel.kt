0f9974fe5a446e7846bad88cb1efc491f5345870
cucumber99 <dlsguq1@gmail.com>
2025-09-23 16:53:02 +0900

    feat: PersonalBottari를 인자로 받는 팩토리 메소드 구현

--- android/Bottari/presentation/src/main/java/com/bottari/presentation/model/bottari/personal/BottariUiModel.kt
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/model/bottari/personal/BottariUiModel.kt
@@ -1,6 +1,7 @@
 package com.bottari.presentation.model.bottari.personal
 
 import com.bottari.domain.model.bottari.BottariState
+import com.bottari.domain.model.bottari.personal.PersonalBottari
 import com.bottari.presentation.model.alarm.AlarmUiModel
 
 data class BottariUiModel(
@@ -18,6 +19,15 @@ data class BottariUiModel(
                 totalQuantity = bottariState.itemCount.totalQuantity,
                 checkedQuantity = bottariState.itemCount.checkedQuantity,
                 alarm = bottariState.bottari.alarm?.let { AlarmUiModel.fromDomain(it) },
+            )
+
+        fun fromPersonalBottari(bottari: PersonalBottari): BottariUiModel =
+            BottariUiModel(
+                id = bottari.id,
+                title = bottari.title,
+                totalQuantity = bottari.totalQuantity,
+                checkedQuantity = bottari.checkedQuantity,
+                alarm = bottari.alarm?.let { alarm -> AlarmUiModel.fromDomain(alarm) },
             )
     }
 }
