8e48b9feb2aa23832eb8f73b046ca402446b7249
cucumber99 <dlsguq1@gmail.com>
2025-09-23 10:26:43 +0900

    feat: 보따리 데이터베이스 선언

---
+++ android/Bottari/data/src/main/java/com/bottari/data/local/bottari/BottariDatabase.kt
@@ -0,0 +1,49 @@
+package com.bottari.data.local.bottari
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.bottari.data.common.util.LocalDateConverter
+import com.bottari.data.common.util.LocalTimeConverter
+import com.bottari.data.common.util.RepeatDaysConverter
+import com.bottari.data.model.local.bottari.AlarmEntity
+import com.bottari.data.model.local.bottari.BottariEntity
+import com.bottari.data.model.local.bottari.ItemEntity
+
+@Database(
+    entities = [
+        BottariEntity::class,
+        ItemEntity::class,
+        AlarmEntity::class,
+    ],
+    version = 1,
+    exportSchema = false,
+)
+@TypeConverters(LocalTimeConverter::class, LocalDateConverter::class, RepeatDaysConverter::class)
+abstract class BottariDatabase : RoomDatabase() {
+    abstract fun bottariDao(): BottariDao
+
+    abstract fun itemDao(): ItemDao
+
+    abstract fun alarmDao(): AlarmDao
+
+    companion object {
+        private const val DATABASE_NAME = "Bottari"
+
+        @Volatile
+        private var instance: BottariDatabase? = null
+
+        fun getDatabase(context: Context): BottariDatabase =
+            instance ?: synchronized(this) {
+                Room
+                    .databaseBuilder(
+                        context.applicationContext,
+                        BottariDatabase::class.java,
+                        DATABASE_NAME,
+                    ).build()
+                    .also { instance = it }
+            }
+    }
+}
