d0632010c97c852073c24cbd79f6b877f7e3a208
cucumber99 <dlsguq1@gmail.com>
2025-09-23 10:37:39 +0900

    feat: AlarmLocalDataSource 선언 및 구현

---
+++ android/Bottari/data/src/main/java/com/bottari/data/source/local/bottari/AlarmLocalDataSourceImpl.kt
@@ -0,0 +1,18 @@
+package com.bottari.data.source.local.bottari
+
+import com.bottari.data.local.bottari.AlarmDao
+import com.bottari.data.model.local.bottari.AlarmEntity
+import kotlinx.coroutines.flow.Flow
+
+class AlarmLocalDataSourceImpl(
+    private val dao: AlarmDao,
+) : AlarmLocalDataSource {
+    override fun fetchAlarm(bottariId: Long): Result<Flow<AlarmEntity>> = runCatching { dao.fetchAlarm(bottariId) }
+
+    override suspend fun saveAlarm(alarm: AlarmEntity): Result<Unit> = runCatching { dao.saveAlarm(alarm) }
+
+    override suspend fun updateAlarmActivate(
+        id: Long,
+        isActive: Boolean,
+    ): Result<Unit> = runCatching { dao.updateAlarmActivate(id, isActive) }
+}
