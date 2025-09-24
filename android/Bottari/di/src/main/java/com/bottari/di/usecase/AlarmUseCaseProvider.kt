95cbb03317fbfdaf3406da44ac7594a648e14b4d
cucumber99 <dlsguq1@gmail.com>
2025-09-24 11:43:06 +0900

    feat: FetchAlarmUseCase 의존성 주입

--- android/Bottari/di/src/main/java/com/bottari/di/usecase/AlarmUseCaseProvider.kt
+++ android/Bottari/di/src/main/java/com/bottari/di/usecase/AlarmUseCaseProvider.kt
@@ -1,10 +1,16 @@
 package com.bottari.di.usecase
 
 import com.bottari.di.RepositoryProvider
+import com.bottari.domain.usecase.alarm.FetchAlarmUseCase
 import com.bottari.domain.usecase.alarm.SaveAlarmUseCase
 import com.bottari.domain.usecase.alarm.UpdateAlarmActivateUseCase
 
 object AlarmUseCaseProvider {
+    val fetchAlarmUseCase: FetchAlarmUseCase by lazy {
+        FetchAlarmUseCase(
+            RepositoryProvider.alarmRepository,
+        )
+    }
     val saveAlarmUseCase: SaveAlarmUseCase by lazy {
         SaveAlarmUseCase(
             RepositoryProvider.alarmRepository,
