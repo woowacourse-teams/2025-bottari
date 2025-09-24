408681f8227ca9b958d27e89d4282a062d8c866d
moondev03 <moondev03@gmail.com>
2025-09-24 14:32:05 +0900

    feat: 각 보따리 화면 구현
    - 개인 보따리 화면을 구현합니다.
    - 우리 보따리 화면을 구현합니다.
    - 보따리 템플릿 화면을 구현합니다.

---
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/compose/home/more/MoreBottariScreen.kt
@@ -0,0 +1,25 @@
+package com.bottari.presentation.compose.home.more
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MoreBottariScreen(
+    onNavigateToPersonal: () -> Unit,
+    onNavigateToTeam: () -> Unit,
+    onNavigateToTemplate: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(text = "우리 보따리")
+    }
+}
