7336dd20c47e72588ac624e29e12174055777e1e
cucumber99 <dlsguq1@gmail.com>
2025-09-24 20:35:02 +0900

    feat: 알람 유형 추가 처리

--- android/Bottari/data/src/main/java/com/bottari/data/model/local/notification/NotificationEntity.kt
+++ android/Bottari/data/src/main/java/com/bottari/data/model/local/notification/NotificationEntity.kt
@@ -8,10 +8,6 @@ import com.bottari.domain.model.notification.Notification
 import java.time.LocalDate
 import java.time.LocalTime
 
-private const val REPEAT = "REPEAT"
-private const val NON_REPEAT = "NON_REPEAT"
-private const val ERROR_UNKNOWN_ALARM_TYPE = "지원하지 않는 알람 유형입니다."
-
 @Entity("notifications")
 data class NotificationEntity(
     @PrimaryKey val bottariId: Long,
@@ -42,11 +38,16 @@ data class NotificationEntity(
     private fun toAlarmType(): AlarmType =
         when (alarmType) {
             NON_REPEAT -> AlarmType.NonRepeat(date!!)
-            REPEAT -> AlarmType.Repeat(repeatDays)
+            EVERY_DAY_REPEAT, EVERY_WEEK_REPEAT -> AlarmType.Repeat(repeatDays)
             else -> throw IllegalArgumentException(ERROR_UNKNOWN_ALARM_TYPE)
         }
 
     companion object {
+        private const val NON_REPEAT = "NON_REPEAT"
+        private const val EVERY_DAY_REPEAT = "EVERY_DAY_REPEAT"
+        private const val EVERY_WEEK_REPEAT = "EVERY_WEEK_REPEAT"
+        private const val ERROR_UNKNOWN_ALARM_TYPE = "지원하지 않는 알람 유형입니다."
+
         fun fromDomain(notification: Notification): NotificationEntity =
             NotificationEntity(
                 bottariId = notification.bottariId,
