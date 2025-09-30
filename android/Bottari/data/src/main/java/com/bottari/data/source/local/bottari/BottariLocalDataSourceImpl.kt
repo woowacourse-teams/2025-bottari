e96f537e7a77fe2eaa6b8cf0a59cf7d41366e38d
cucumber99 <dlsguq1@gmail.com>
2025-09-30 19:36:25 +0900

    refactor: 보따리 생성 로직 `BottariLocalDataSourceImpl`로 이동
    - `BottariDatabase`의 `createBottariWithItems` 함수를 `BottariLocalDataSourceImpl`로 이동시켰습니다.
    - Room의 `withTransaction`을 사용하여 트랜잭션 처리를 명시적으로 변경했습니다.

--- android/Bottari/data/src/main/java/com/bottari/data/source/local/bottari/BottariLocalDataSourceImpl.kt
+++ android/Bottari/data/src/main/java/com/bottari/data/source/local/bottari/BottariLocalDataSourceImpl.kt
@@ -1,9 +1,11 @@
 package com.bottari.data.source.local.bottari
 
+import androidx.room.withTransaction
 import com.bottari.data.local.bottari.BottariDao
 import com.bottari.data.local.bottari.BottariDatabase
 import com.bottari.data.model.local.bottari.BottariEntity
 import com.bottari.data.model.local.bottari.BottariWithAlarmAndItems
+import com.bottari.data.model.local.bottari.ItemEntity
 import kotlinx.coroutines.flow.Flow
 
 class BottariLocalDataSourceImpl(
@@ -20,7 +22,16 @@ class BottariLocalDataSourceImpl(
     override suspend fun createBottariWithItems(
         bottari: BottariEntity,
         itemNames: List<String>,
-    ): Result<Long> = runCatching { database.createBottariWithItems(bottari, itemNames) }
+    ): Result<Long> =
+        runCatching {
+            database.withTransaction {
+                val bottariId = database.bottariDao().createBottari(bottari)
+                val items =
+                    Array(itemNames.size) { index -> ItemEntity.from(bottariId, itemNames[index]) }
+                database.itemDao().saveItem(*items)
+                bottariId
+            }
+        }
 
     override suspend fun deleteBottari(bottariId: Long): Result<Unit> = runCatching { dao.deleteBottari(bottariId) }
 
