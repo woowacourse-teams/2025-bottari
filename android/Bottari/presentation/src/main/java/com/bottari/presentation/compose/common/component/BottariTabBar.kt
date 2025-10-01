1d36e7e97cffabe2ff6b0b8af9ad169c212b0757
Sia <lyl8661@naver.com>
2025-10-01 16:42:35 +0900

    refactor: BottariTabBar 리컴포지션 최소화

--- android/Bottari/presentation/src/main/java/com/bottari/presentation/compose/common/component/BottariTabBar.kt
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/compose/common/component/BottariTabBar.kt
@@ -7,9 +7,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerState
@@ -28,13 +26,12 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.lerp
 import androidx.compose.ui.zIndex
 import com.bottari.presentation.compose.common.modifier.dropShadow
+import com.bottari.presentation.compose.common.modifier.pagerTabIndicatorOffset
 import com.bottari.presentation.compose.common.source.NoRippleInteractionSource
 import com.bottari.presentation.compose.common.theme.BottariTheme
 import kotlinx.coroutines.launch
-import kotlin.math.abs
 
 @Composable
 fun BottariTabBar(
@@ -47,9 +44,8 @@ fun BottariTabBar(
     val interactionSource = remember { NoRippleInteractionSource() }
     val indicator = @Composable { tabPositions: List<TabPosition> ->
         BottariIndicator(
+            pagerState = pagerState,
             tabPositions = tabPositions,
-            fraction = pagerState.currentPageOffsetFraction,
-            currentPage = pagerState.currentPage,
         )
     }
 
@@ -110,31 +106,13 @@ private fun BottariTap(
 @Composable
 private fun BottariIndicator(
     tabPositions: List<TabPosition>,
-    fraction: Float,
-    currentPage: Int,
+    pagerState: PagerState,
 ) {
-    val currentTab = tabPositions[currentPage]
-
-    val targetPage =
-        when {
-            fraction > 0 -> currentPage + 1
-            fraction < 0 -> currentPage - 1
-            else -> currentPage
-        }.coerceIn(0, tabPositions.lastIndex)
-
-    val targetTab = tabPositions[targetPage]
-
-    val animationFraction = abs(fraction)
-
-    val indicatorStart = lerp(currentTab.left, targetTab.left, animationFraction)
-    val indicatorWidth = lerp(currentTab.width, targetTab.width, animationFraction)
-
     Box(
         modifier =
             Modifier
-                .offset(x = indicatorStart)
+                .pagerTabIndicatorOffset(pagerState = pagerState, tabPositions = tabPositions)
                 .wrapContentSize(align = Alignment.BottomStart)
-                .width(indicatorWidth)
                 .fillMaxSize()
                 .padding(horizontal = 4.dp)
                 .dropShadow(
