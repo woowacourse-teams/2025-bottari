99cb1109378f27149611ae9fe1cd27f8616c7392
Sia <lyl8661@naver.com>
2025-10-01 17:24:26 +0900

    refactor: `noRippleClickable` Modifier 확장함수 분리

--- android/Bottari/presentation/src/main/java/com/bottari/presentation/compose/common/modifier/DropShadow.kt
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/compose/common/modifier/DropShadow.kt
@@ -1,8 +1,6 @@
 package com.bottari.presentation.compose.common.modifier
 
 import android.graphics.BlurMaskFilter
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -62,13 +60,3 @@ fun Modifier.dropShadow(
         }
     }
 }
-
-fun Modifier.noRippleClickable(onClick: () -> Unit): Modifier =
-    composed {
-        this.clickable(
-            indication = null,
-            interactionSource = remember { MutableInteractionSource() },
-        ) {
-            onClick()
-        }
-    }
---
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/compose/common/modifier/NoRippleClickable.kt
@@ -0,0 +1,17 @@
+package com.bottari.presentation.compose.common.modifier
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+
+fun Modifier.noRippleClickable(onClick: () -> Unit): Modifier =
+    composed {
+        this.clickable(
+            indication = null,
+            interactionSource = remember { MutableInteractionSource() },
+        ) {
+            onClick()
+        }
+    }
