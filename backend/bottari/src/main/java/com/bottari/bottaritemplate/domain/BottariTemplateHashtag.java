db79e5a33319051815631f388c2359fa1043c53e
sung-june27 <kmymy27@gmail.com>
2025-10-02 16:45:59 +0900

    feat: BottariTemplateHashtag 엔티티 추가

---
+++ backend/bottari/src/main/java/com/bottari/bottaritemplate/domain/BottariTemplateHashtag.java
@@ -0,0 +1,47 @@
+package com.bottari.bottaritemplate.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+@Entity
+@SQLDelete(sql = "UPDATE bottari_template_hashtag SET deleted_at = CURRENT_TIMESTAMP WHERE id = ?")
+@SQLRestriction("deleted_at IS NULL")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class BottariTemplateHashtag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "bottari_template_id")
+    private BottariTemplate bottariTemplate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "hashtag")
+    private Hashtag hashtag;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    public BottariTemplateHashtag(
+            final BottariTemplate bottariTemplate,
+            final Hashtag hashtag
+    ) {
+        this.bottariTemplate = bottariTemplate;
+        this.hashtag = hashtag;
+    }
+}
