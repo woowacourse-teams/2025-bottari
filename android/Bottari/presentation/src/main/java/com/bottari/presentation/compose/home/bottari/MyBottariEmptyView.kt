569003a891354e4fc7b3ef0c042330f35279604c
Sia <lyl8661@naver.com>
2025-10-14 16:17:38 +0900

    refactor: 나의 보따리 emptyView UI 개선

--- android/Bottari/presentation/src/main/java/com/bottari/presentation/compose/home/bottari/MyBottariEmptyView.kt
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/compose/home/bottari/MyBottariEmptyView.kt
@@ -1,9 +1,8 @@
 package com.bottari.presentation.compose.home.bottari
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
@@ -21,28 +20,26 @@ import com.bottari.presentation.compose.common.theme.BottariTheme
 
 @Composable
 fun MyBottariEmptyView() {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Column(horizontalAlignment = Alignment.CenterHorizontally) {
-            Icon(
-                painter = painterResource(id = R.drawable.ic_bottari),
-                contentDescription = stringResource(R.string.bottari_icon_home_description),
-                modifier = Modifier.size(80.dp),
-                tint = BottariTheme.colors.gray500,
-            )
-            Spacer(modifier = Modifier.height(BottariTheme.spacing.spaceSmall))
-            Text(
-                text = stringResource(R.string.bottari_home_empty_view_title_text),
-                color = BottariTheme.colors.gray500,
-                style = BottariTheme.typography.bold20.toTextStyle(),
-            )
-            Spacer(modifier = Modifier.height(BottariTheme.spacing.spaceSmall))
-            Text(
-                text = stringResource(R.string.bottari_home_empty_view_description_text),
-                textAlign = TextAlign.Center,
-                color = BottariTheme.colors.gray500,
-                style = BottariTheme.typography.medium16.toTextStyle(),
-            )
-        }
+    Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+        Icon(
+            painter = painterResource(id = R.drawable.ic_bottari),
+            contentDescription = stringResource(R.string.bottari_icon_home_description),
+            modifier = Modifier.size(80.dp),
+            tint = BottariTheme.colors.gray500,
+        )
+        Spacer(modifier = Modifier.height(BottariTheme.spacing.spaceSmall))
+        Text(
+            text = stringResource(R.string.bottari_home_empty_view_title_text),
+            color = BottariTheme.colors.gray500,
+            style = BottariTheme.typography.bold20.toTextStyle(),
+        )
+        Spacer(modifier = Modifier.height(BottariTheme.spacing.spaceSmall))
+        Text(
+            text = stringResource(R.string.bottari_home_empty_view_description_text),
+            textAlign = TextAlign.Center,
+            color = BottariTheme.colors.gray500,
+            style = BottariTheme.typography.medium16.toTextStyle(),
+        )
     }
 }
 
