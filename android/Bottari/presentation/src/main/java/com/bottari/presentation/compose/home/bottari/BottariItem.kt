f9ddf9ab6348f0dde8e08afefa2df303a11fbf15
Sia <lyl8661@naver.com>
2025-10-14 20:42:06 +0900

    refactor: 프리뷰 가독성 개선

--- android/Bottari/presentation/src/main/java/com/bottari/presentation/compose/home/bottari/BottariItem.kt
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/compose/home/bottari/BottariItem.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
@@ -332,5 +333,10 @@ private fun TeamBottariScreenPreview() {
         onCloseMenu = {},
         onBottariDelete = {},
         onBottariEdit = {},
+        modifier =
+            Modifier
+                .padding(
+                    horizontal = BottariTheme.spacing.spaceMedium,
+                ),
     )
 }
