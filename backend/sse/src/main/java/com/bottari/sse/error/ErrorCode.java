26f9827469740d697293291fc2a0136fc9ca8167
민경윤 <ruddbs3308@naver.com>
2025-10-17 19:07:10 +0900

    feat: SSE 커넥션 성공 시 연결 성공 메시지 push 구현
    Co-authored-by: Sung-june27 <kmymy27@gmail.com>
    Co-authored-by: unh6unh6 <ruddbs3308@naver.com>
    Co-authored-by: jaehyeon2650 <ind07152@naver.com>

--- backend/sse/src/main/java/com/bottari/sse/error/ErrorCode.java
+++ backend/sse/src/main/java/com/bottari/sse/error/ErrorCode.java
@@ -9,7 +9,10 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     // ===== PUSH MESSAGE 관련 =====
-    INVALID_MESSAGE_FORMAT(HttpStatus.BAD_REQUEST, "메시지 포맷이 올바르지 않습니다.")
+    INVALID_MESSAGE_FORMAT(HttpStatus.BAD_REQUEST, "메시지 포맷이 올바르지 않습니다."),
+
+    // ===== SSE CONNECTION 관련 =====
+    SSE_CONNECTION_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "SSE 연결에 실패했습니다.")
     ;
 
     private final HttpStatus status;
