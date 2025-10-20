d4da9916f3884d60da00c616ad462045aee26096
Yehyeok Bang <qkddpgur318@gmail.com>
2025-10-20 13:47:52 +0900

    fix: 컨벤션 수정

--- backend/bottari/src/main/java/com/bottari/support/BaseTimeEntity.java
+++ backend/bottari/src/main/java/com/bottari/support/BaseTimeEntity.java
@@ -9,9 +9,9 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-@Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
+@Getter
 public abstract class BaseTimeEntity {
 
     @CreatedDate
