2971d48aeb88b14ff80fe8713a11327941acc164
cucumber99 <dlsguq1@gmail.com>
2025-10-20 21:19:30 +0900

    feat: 보따리 편집 화면 일부 구현
    - 알람 편집 화면 미구현

---
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/compose/edit/PersonalBottariEditScreen.kt
@@ -0,0 +1,143 @@
+package com.bottari.presentation.compose.edit
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.exclude
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.ime
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ScaffoldDefaults
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.bottari.presentation.R
+import com.bottari.presentation.compose.common.component.BottariTabBar
+import com.bottari.presentation.compose.common.theme.BottariTheme
+import com.bottari.presentation.compose.common.theme.LocalBottariBgColor
+import com.bottari.presentation.compose.edit.component.ComposeBottariRenameDialog
+import com.bottari.presentation.compose.edit.component.ItemEditContent
+import com.bottari.presentation.compose.edit.component.PersonalBottariEditTopAppBar
+import com.bottari.presentation.view.edit.personal.main.PersonalBottariEditUiEvent
+import com.bottari.presentation.view.edit.personal.main.PersonalBottariEditViewModel
+
+@Composable
+fun PersonalBottariEditScreen(
+    onBackClick: () -> Unit,
+    isNewBottari: Boolean,
+    modifier: Modifier = Modifier,
+    viewModel: PersonalBottariEditViewModel = viewModel(),
+) {
+    val uiState = viewModel.uiState.collectAsStateWithLifecycle()
+    val uiEvent = viewModel.uiEvent.collectAsStateWithLifecycle(null)
+    val context = LocalContext.current
+    val snackbarHostState = remember { SnackbarHostState() }
+
+    LaunchedEffect(Unit) {
+        if (isNewBottari) {
+            snackbarHostState.showSnackbar(context.getString(R.string.bottari_create_success_text))
+        }
+    }
+
+    LaunchedEffect(uiEvent.value) {
+        when (uiEvent.value ?: return@LaunchedEffect) {
+            PersonalBottariEditUiEvent.CreateTemplateFailure ->
+                snackbarHostState.showSnackbar(
+                    context.getString(R.string.bottari_edit_create_template_failure_text),
+                )
+
+            PersonalBottariEditUiEvent.CreateTemplateSuccess ->
+                snackbarHostState.showSnackbar(
+                    context.getString(R.string.bottari_edit_create_template_success_text),
+                )
+
+            PersonalBottariEditUiEvent.FindBottariFailure ->
+                snackbarHostState.showSnackbar(
+                    context.getString(
+                        R.string.bottari_edit_fetch_failure_text,
+                    ),
+                )
+
+            PersonalBottariEditUiEvent.ToggleAlarmStateFailure ->
+                snackbarHostState.showSnackbar(
+                    context.getString(R.string.bottari_edit_toggle_alarm_state_failure_text),
+                )
+        }
+    }
+
+    if (uiState.value.showBottariRenameDialog) {
+        ComposeBottariRenameDialog(
+            bottariTitle = uiState.value.bottariTitle,
+            onDismissRequest = { viewModel.changeBottariRenameDialogState(false) },
+            snackbarHostState = snackbarHostState,
+        )
+    }
+
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            PersonalBottariEditTopAppBar(
+                bottariTitle = uiState.value.bottariTitle,
+                onBackClick = onBackClick,
+                onBottariRenameClick = { viewModel.changeBottariRenameDialogState(true) },
+                onCreateTemplateClick = viewModel::createBottariTemplate,
+            )
+        },
+        snackbarHost = { SnackbarHost(snackbarHostState) },
+        containerColor = LocalBottariBgColor.current,
+        contentWindowInsets = ScaffoldDefaults.contentWindowInsets.exclude(WindowInsets.ime),
+    ) { paddingValues ->
+        PersonalBottariEditPager(
+            modifier =
+                Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues),
+        )
+    }
+}
+
+@Composable
+private fun PersonalBottariEditPager(modifier: Modifier = Modifier) {
+    val pageTitles =
+        listOf(
+            stringResource(R.string.bottari_edit_item_text),
+            stringResource(R.string.bottari_edit_alarm_text),
+        )
+    val pagerState = rememberPagerState { pageTitles.size }
+
+    Column(modifier = modifier) {
+        BottariTabBar(
+            pageTitles = pageTitles,
+            pagerState = pagerState,
+        ) { page ->
+            when (page) {
+                0 -> ItemEditContent()
+                1 -> AlarmEditContent()
+            }
+        }
+    }
+}
+
+@Composable
+fun AlarmEditContent(modifier: Modifier = Modifier) {
+}
+
+@Preview
+@Composable
+private fun PersonalBottariEditScreenPreview() {
+    BottariTheme {
+        PersonalBottariEditScreen(
+            onBackClick = {},
+            isNewBottari = false,
+        )
+    }
+}
