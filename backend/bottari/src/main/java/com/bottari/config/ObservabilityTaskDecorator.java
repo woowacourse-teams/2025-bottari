4ae67b4334a159bfd44b8cdf850c5ed27a6fa6ce
wogus <ind07152@naver.com>
2025-10-21 17:31:08 +0900

    refactor: opentelemetry 의존성으로 변경

--- backend/bottari/src/main/java/com/bottari/config/ObservabilityTaskDecorator.java
+++ backend/bottari/src/main/java/com/bottari/config/ObservabilityTaskDecorator.java
@@ -1,26 +1,15 @@
 package com.bottari.config;
 
 
-import io.micrometer.context.ContextRegistry;
-import io.micrometer.context.ContextSnapshot;
-import io.micrometer.context.ContextSnapshotFactory;
+import io.opentelemetry.context.Context;
 import org.springframework.core.task.TaskDecorator;
+import org.springframework.lang.NonNull;
 
 public class ObservabilityTaskDecorator implements TaskDecorator {
 
-    private static final ContextRegistry REGISTRY = ContextRegistry.getInstance();
-    private static final ContextSnapshotFactory SNAPSHOT_FACTORY = ContextSnapshotFactory.builder()
-            .contextRegistry(REGISTRY)
-            .build();
-
     @Override
-    public Runnable decorate(final Runnable runnable) {
-        final ContextSnapshot snapshot = SNAPSHOT_FACTORY.captureAll();
-
-        return () -> {
-            try (final ContextSnapshot.Scope ignored = snapshot.setThreadLocals()) {
-                runnable.run();
-            }
-        };
+    @NonNull
+    public Runnable decorate(@NonNull final Runnable runnable) {
+        return Context.current().wrap(runnable);
     }
 }
