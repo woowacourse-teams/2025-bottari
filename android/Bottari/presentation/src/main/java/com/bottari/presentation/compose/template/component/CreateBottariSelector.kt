72825d70cbebc11f7c9fb9d7c929895f23bd8329
moondev03 <moondev03@gmail.com>
2025-10-22 20:17:57 +0900

    refactor: 바텀시트가 백프레스와 외부 클릭으로 닫히도록 수정

--- android/Bottari/presentation/src/main/java/com/bottari/presentation/compose/template/component/CreateBottariSelector.kt
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/compose/template/component/CreateBottariSelector.kt
@@ -20,7 +20,6 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.ModalBottomSheetProperties
 import androidx.compose.material3.SheetState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -69,11 +68,6 @@ fun CreateBottariSelector(
         dragHandle = null,
         sheetGesturesEnabled = false,
         modifier = modifier,
-        properties =
-            ModalBottomSheetProperties(
-                shouldDismissOnBackPress = false,
-                shouldDismissOnClickOutside = false,
-            ),
     ) {
         Column(
             modifier =
