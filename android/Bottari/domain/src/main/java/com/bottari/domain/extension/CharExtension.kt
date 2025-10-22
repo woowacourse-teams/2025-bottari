6460dd9454ebcb12df145667a6aae641f5e70368
moondev03 <moondev03@gmail.com>
2025-10-22 10:01:09 +0900

    feat: Char에 대한 확장 함수 추가
    - 영어 알파벳, 완성형 한글, 한글 자모를 판별하는 확장 함수들을 추가합니다.

---
+++ android/Bottari/domain/src/main/java/com/bottari/domain/extension/CharExtension.kt
@@ -0,0 +1,21 @@
+package com.bottari.domain.extension
+
+fun Char.isEnglishLetter(): Boolean {
+    if (this in 'a'..'z') return true
+    if (this in 'A'..'Z') return true
+    return false
+}
+
+/** 완성형 한글(가-힣)인지 */
+fun Char.isHangulSyllable(): Boolean = this in '\uAC00'..'\uD7A3'
+
+fun Char.isNotHangulSyllable(): Boolean = !isHangulSyllable()
+
+/** 한글 자모(모음/자음) 여부 */
+fun Char.isHangulJamoAny(): Boolean {
+    if (this in '\u3130'..'\u318F') return true
+    if (this in '\u1100'..'\u11FF') return true
+    if (this in '\uA960'..'\uA97F') return true
+    if (this in '\uD7B0'..'\uD7FF') return true
+    return false
+}
