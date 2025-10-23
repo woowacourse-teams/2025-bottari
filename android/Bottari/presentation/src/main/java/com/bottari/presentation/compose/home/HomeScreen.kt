71deaa49623840b5447b523532ebb47f9a55747e
Sia <lyl8661@naver.com>
2025-10-23 12:53:12 +0900

    feat: 뒤로가기 버튼 두 번 누르면 앱 종료 기능 추가

--- android/Bottari/presentation/src/main/java/com/bottari/presentation/compose/home/HomeScreen.kt
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/compose/home/HomeScreen.kt
@@ -1,12 +1,18 @@
 package com.bottari.presentation.compose.home
 
+import androidx.activity.compose.BackHandler
+import androidx.activity.compose.LocalActivity
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableLongStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import com.bottari.presentation.compose.common.navigation.Navigation
@@ -15,6 +21,10 @@ import com.bottari.presentation.compose.common.theme.LocalBottariBgColor
 import com.bottari.presentation.compose.home.bottari.MyBottariScreen
 import com.bottari.presentation.compose.home.more.MoreBottariScreen
 import com.bottari.presentation.compose.home.template.TemplateBottariScreen
+import kotlinx.coroutines.launch
+
+
+private const val BACK_PRESS_EXIT_TIMEOUT = 2000L
 
 @Composable
 fun HomeScreen(
@@ -26,11 +36,27 @@ fun HomeScreen(
     navigateToTemplateDetail: (templateId: Long, isMyTemplate: Boolean) -> Unit,
     navigateToTemplateCreate: () -> Unit,
 ) {
+    val scope = rememberCoroutineScope()
+    var backPressedTime by remember { mutableLongStateOf(0L) }
+    val context = LocalActivity.current
+
     val snackbarHostState = remember { SnackbarHostState() }
     val navController =
         rememberSaveable(saver = NavigationController.saver) { NavigationController(HomeScreenRoute.Bottari) }
     val currentScreen =
         remember(navController.currentScreen) { navController.currentScreen as HomeScreenRoute }
+
+    BackHandler(enabled = true) {
+        val currentTime = System.currentTimeMillis()
+        if (currentTime - backPressedTime > BACK_PRESS_EXIT_TIMEOUT) {
+            backPressedTime = currentTime
+            scope.launch {
+                snackbarHostState.showSnackbar("한 번 더 누르면 종료됩니다")
+            }
+            return@BackHandler
+        }
+        context?.finish()
+    }
 
     Scaffold(
         topBar = { HomeTopAppBar(title = stringResource(currentScreen.labelResId())) },
