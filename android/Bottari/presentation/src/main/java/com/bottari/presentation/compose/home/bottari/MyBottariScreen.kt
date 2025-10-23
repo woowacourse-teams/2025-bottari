80b17e44f143ca62dff37f54692bb27bd54548b9
cucumber99 <dlsguq1@gmail.com>
2025-10-23 16:41:27 +0900

    refactor: 불필요한 import 제거

--- android/Bottari/presentation/src/main/java/com/bottari/presentation/compose/home/bottari/MyBottariScreen.kt
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/compose/home/bottari/MyBottariScreen.kt
@@ -10,7 +10,6 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalClipboard
-import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.Lifecycle
