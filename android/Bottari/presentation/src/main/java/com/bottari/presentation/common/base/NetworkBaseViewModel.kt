6024c921a4eb503bad0e196b6585c8e43c7858a8
cucumber99 <dlsguq1@gmail.com>
2025-10-31 14:16:42 +0900

    style: Ktlint 적용

--- android/Bottari/presentation/src/main/java/com/bottari/presentation/common/base/NetworkBaseViewModel.kt
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/common/base/NetworkBaseViewModel.kt
@@ -1,11 +1,7 @@
 package com.bottari.presentation.common.base
 
 import com.bottari.presentation.util.NetworkManager
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.update
 
 abstract class NetworkBaseViewModel<UiState, UiEvent>(
     initialState: UiState,
