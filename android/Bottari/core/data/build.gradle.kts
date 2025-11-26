9972f9a90d073b1f81d4786ba79020fa39d85cd7
cucumber99 <dlsguq1@gmail.com>
2025-11-27 00:22:49 +0900

    build: :core:data 모듈 최소 버전 명시

--- android/Bottari/core/data/build.gradle.kts
+++ android/Bottari/core/data/build.gradle.kts
@@ -13,6 +13,10 @@ android {
             .toInt()
 
     defaultConfig {
+        minSdk =
+            libs.versions.minSdk
+                .get()
+                .toInt()
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
