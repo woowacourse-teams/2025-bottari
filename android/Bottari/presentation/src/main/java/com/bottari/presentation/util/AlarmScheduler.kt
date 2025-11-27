51a68ab0e43e1da7768341341bc0372bebc348c7
cucumber99 <dlsguq1@gmail.com>
2025-11-27 16:56:27 +0900

    refactor: 도메인 로직 분리 적용

--- android/Bottari/presentation/src/main/java/com/bottari/presentation/util/AlarmScheduler.kt
+++ android/Bottari/presentation/src/main/java/com/bottari/presentation/util/AlarmScheduler.kt
@@ -4,40 +4,22 @@ import android.app.AlarmManager
 import android.app.AlarmManager.AlarmClockInfo
 import android.app.PendingIntent
 import android.content.Context
-import com.bottari.core.domain.model.alarm.AlarmType
+import com.bottari.core.domain.model.alarm.AlarmTriggerTimeCalculator
 import com.bottari.core.domain.model.notification.Notification
 import com.bottari.presentation.compose.edit.personal.ComposePersonalBottariEditActivity
 import com.bottari.presentation.receiver.AlarmReceiver
 import dagger.hilt.android.qualifiers.ApplicationContext
-import java.time.DayOfWeek
-import java.time.LocalDate
-import java.time.LocalDateTime
-import java.time.LocalTime
-import java.time.ZoneId
 import javax.inject.Inject
 
 class AlarmScheduler @Inject constructor(
     @ApplicationContext private val context: Context,
+    private val calculator: AlarmTriggerTimeCalculator,
 ) {
     private val manager: AlarmManager = context.getSystemService(AlarmManager::class.java)
 
     fun scheduleAlarm(notification: Notification) {
-        if (notification.alarm.isActive.not()) return
-
-        if (notification.alarm.alarmType is AlarmType.NonRepeat) {
-            scheduleNonRepeatAlarm(notification)
-            return
-        }
-        scheduleRepeatAlarm(notification)
-    }
-
-    fun scheduleNextAlarm(notification: Notification) {
-        if (notification.alarm.isActive.not()) return
-
-        val alarm = notification.alarm
-        if (alarm.alarmType is AlarmType.NonRepeat) return
-        val triggerTime = getNextTriggerTime(notification = notification)
-        scheduleAlarmInternal(notification, triggerTime)
+        val triggerTime = calculator.calculate(alarm = notification.alarm) ?: return
+        scheduleAlarm(notification, triggerTime)
     }
 
     fun cancelAlarm(notification: Notification) {
@@ -45,47 +27,7 @@ class AlarmScheduler @Inject constructor(
         manager.cancel(pendingIntent)
     }
 
-    private fun scheduleRepeatAlarm(notification: Notification) {
-        val triggerTime = getNextTriggerTime(notification = notification)
-        scheduleAlarmInternal(notification, triggerTime)
-    }
-
-    private fun scheduleNonRepeatAlarm(
-        notification: Notification,
-        now: LocalDateTime = LocalDateTime.now(),
-    ) {
-        val alarm = notification.alarm
-        val alarmType = alarm.alarmType as AlarmType.NonRepeat
-        val alarmDateTime = LocalDateTime.of(alarmType.date, alarm.time)
-        if (alarmDateTime.isBefore(now)) return
-        val triggerTime =
-            LocalDateTime.of(alarmType.date, notification.alarm.time).toTimeMillis()
-        scheduleAlarmInternal(notification, triggerTime)
-    }
-
-    private fun getNextTriggerTime(
-        today: LocalDate = LocalDate.now(),
-        nowTime: LocalTime = LocalTime.now(),
-        notification: Notification,
-    ): Long {
-        val alarm = notification.alarm
-        val alarmType = alarm.alarmType as AlarmType.Repeat
-        val availableDays = alarmType.repeatDays.map(DayOfWeek::of)
-        if (availableDays.contains(today.dayOfWeek) && nowTime.isBefore(alarm.time)) {
-            return LocalDateTime.of(today, alarm.time).toTimeMillis()
-        }
-        val triggerDate =
-            availableDays
-                .map { dayOfWeek ->
-                    val daysUntil =
-                        (dayOfWeek.value - today.dayOfWeek.value + DAYS_IN_WEEK) % DAYS_IN_WEEK
-                    val adjustedDaysUntil = if (daysUntil == 0) DAYS_IN_WEEK else daysUntil
-                    today.plusDays(adjustedDaysUntil.toLong())
-                }.minByOrNull { it.toEpochDay() }
-        return LocalDateTime.of(triggerDate, alarm.time).toTimeMillis()
-    }
-
-    private fun scheduleAlarmInternal(
+    private fun scheduleAlarm(
         notification: Notification,
         triggerTime: Long,
     ) {
@@ -117,14 +59,4 @@ class AlarmScheduler @Inject constructor(
             AlarmReceiver.newIntent(context, notification),
             PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE,
         )
-
-    private fun LocalDateTime.toTimeMillis(): Long =
-        this
-            .atZone(ZoneId.systemDefault())
-            .toInstant()
-            .toEpochMilli()
-
-    companion object {
-        private const val DAYS_IN_WEEK = 7
-    }
 }
