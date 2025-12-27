c497e1dc65aaf961bf3c8c70b168b38002bbd78a
moondev03 <moondev03@gmail.com>
2025-12-27 14:07:57 +0900

    refactor: Compose Navigation 의존성을 Feature 모듈로 이동
    - `AndroidFeatureConventionPlugin`에 `androidx.compose.navigation` 번들 의존성을 추가하여, 모든 Feature 모듈이 내비게이션 라이브러리를 공통으로 사용하도록 설정했습니다.
    - `ConfigureCompose` 내부에서는 해당 의존성을 제거하여, Compose 관련 설정 중 내비게이션만 분리했습니다.

--- android/Bottari/build-logic/convention/src/main/java/com/bottari/convention/plugin/AndroidFeatureConventionPlugin.kt
+++ android/Bottari/build-logic/convention/src/main/java/com/bottari/convention/plugin/AndroidFeatureConventionPlugin.kt
@@ -2,6 +2,7 @@ package com.bottari.convention.plugin
 
 import com.bottari.convention.internal.applyPlugins
 import com.bottari.convention.internal.implementation
+import com.bottari.convention.internal.libs
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.dependencies
@@ -19,6 +20,7 @@ internal class AndroidFeatureConventionPlugin : Plugin<Project> {
             dependencies {
                 implementation(project(":core:domain"))
                 implementation(project(":logger"))
+                implementation(libs.findBundle("androidx.compose.navigation").get())
             }
         }
     }
