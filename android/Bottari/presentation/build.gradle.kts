ee937d2ac4893357d9f11b91ece9bbe59cdf0950
moondev03 <moondev03@gmail.com>
2025-12-27 14:08:09 +0900

    feat: `presentation` 모듈에 `designsystem` 의존성 추가
    - `presentation` 모듈의 `build.gradle.kts` 파일에 `core:designsystem` 모듈을 의존성으로 추가했습니다.
    - 이를 통해 `presentation` 모듈에서 공통 디자인 시스템 컴포넌트를 사용할 수 있게 됩니다.

--- android/Bottari/presentation/build.gradle.kts
+++ android/Bottari/presentation/build.gradle.kts
@@ -52,6 +52,8 @@ android {
 }
 
 dependencies {
+    implementation(project(":core:designsystem"))
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.activity)
