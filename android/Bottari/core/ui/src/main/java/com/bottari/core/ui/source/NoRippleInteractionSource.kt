c3e15cf7ddc1de00398754d9f8c1f6ceb097ad03
moondev03 <moondev03@gmail.com>
2026-01-12 23:00:13 +0900

    feat: `rememberNoRippleInteractionSource` Composable 함수 추가
    - `remember`를 사용하여 `NoRippleInteractionSource` 인스턴스를 생성하고 유지하는 `rememberNoRippleInteractionSource` Composable 함수를 추가했습니다.
    - 이를 통해 Composable 내에서 리플 효과 없는 `InteractionSource`를 간편하게 재사용할 수 있습니다.

--- android/Bottari/core/ui/src/main/java/com/bottari/core/ui/source/NoRippleInteractionSource.kt
+++ android/Bottari/core/ui/src/main/java/com/bottari/core/ui/source/NoRippleInteractionSource.kt
@@ -2,6 +2,8 @@ package com.bottari.core.ui.source
 
 import androidx.compose.foundation.interaction.Interaction
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.emptyFlow
 
@@ -12,3 +14,6 @@ class NoRippleInteractionSource : MutableInteractionSource {
 
     override fun tryEmit(interaction: Interaction): Boolean = true
 }
+
+@Composable
+fun rememberNoRippleInteractionSource(): MutableInteractionSource = remember { NoRippleInteractionSource() }
