41693a8c19e6ad6e636fc5029c31ef3144c3d4ba
민경윤 <ruddbs3308@naver.com>
2026-02-05 06:29:49 +0900

    refactor: 개인 보따리 백업 S3 key 전략 변경

--- backend/bottari/src/main/java/com/bottari/bottari/controller/BottariBackupService.java
+++ backend/bottari/src/main/java/com/bottari/bottari/controller/BottariBackupService.java
@@ -1,7 +1,6 @@
 package com.bottari.bottari.controller;
 
 import com.bottari.s3.S3Service;
-import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -9,6 +8,8 @@ import org.springframework.web.multipart.MultipartFile;
 @Service
 @RequiredArgsConstructor
 public class BottariBackupService {
+
+    private static final String BOTTARI_BACKUP_PREFIX = "bottari";
 
     private final S3Service s3Service;
 
@@ -18,8 +19,8 @@ public class BottariBackupService {
     ) {
         return s3Service.upload(
                 file,
-                "bottari/" + ssaid,
-                LocalDateTime.now() + "_" + file.getOriginalFilename()
+                BOTTARI_BACKUP_PREFIX,
+                "ssaid" + "_" + ssaid
         );
     }
 }
