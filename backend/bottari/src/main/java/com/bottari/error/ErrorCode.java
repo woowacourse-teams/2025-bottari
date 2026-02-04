046b1f6788caaf5dd9c0de81f81e2fe82399daf2
민경윤 <ruddbs3308@naver.com>
2026-02-05 08:16:54 +0900

    feat: S3 다운로드 기능 추가

--- backend/bottari/src/main/java/com/bottari/error/ErrorCode.java
+++ backend/bottari/src/main/java/com/bottari/error/ErrorCode.java
@@ -124,12 +124,12 @@ public enum ErrorCode {
 
     // ===== S3 관련 =====
     S3_UPLOAD_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "S3 업로드에 실패했습니다."),
+    S3_DOWNLOAD_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "S3 다운로드에 실패했습니다."),
 
-    // ===== 기타 =====
+    // ===== 기타 =====,
     DATE_FORMAT_INVALID(HttpStatus.BAD_REQUEST, "유효하지 않은 날짜 형식입니다."),
     NUMBER_FORMAT_INVALID(HttpStatus.BAD_REQUEST, "유효하지 않은 숫자 형식입니다."),
-    INVALID_TOPIC_NAME(HttpStatus.INTERNAL_SERVER_ERROR, "올바르지 않은 토픽 이름입니다."),
-    ;
+    INVALID_TOPIC_NAME(HttpStatus.INTERNAL_SERVER_ERROR, "올바르지 않은 토픽 이름입니다.");
 
     private final HttpStatus status;
     private final String message;
--- backend/bottari/src/main/java/com/bottari/s3/S3Service.java
+++ backend/bottari/src/main/java/com/bottari/s3/S3Service.java
@@ -3,12 +3,12 @@ package com.bottari.s3;
 import com.bottari.error.BusinessException;
 import com.bottari.error.ErrorCode;
 import io.awspring.cloud.s3.S3Operations;
-import io.awspring.cloud.s3.S3Resource;
 import java.io.IOException;
 import java.io.InputStream;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -27,7 +27,7 @@ public class S3Service {
             final String keyPrefix,
             final String keyName
     ) {
-        final String key = keyPrefix + "/" + keyName;
+        final String key = getS3Key(keyPrefix, keyName);
         try (InputStream inputStream = file.getInputStream()) {
             s3Operations.upload(bucket, key, inputStream);
         } catch (final IOException e) {
@@ -37,5 +37,28 @@ public class S3Service {
         log.info("S3 upload complete. bucket={}, key={}", bucket, key);
 
         return key;
+    }
+
+    public Resource download(
+            final String keyPrefix,
+            final String keyName
+    ) {
+        final String key = getS3Key(keyPrefix, keyName);
+        try {
+            final Resource resource = s3Operations.download(bucket, key);
+            log.info("S3 download complete. bucket={}, key={}", bucket, key);
+
+            return resource;
+        } catch (final Exception e) {
+            log.warn("S3 download failed. bucket={}, key={}", bucket, key, e);
+            throw new BusinessException(ErrorCode.S3_DOWNLOAD_FAILED, "bucket=" + bucket + ", key=" + key);
+        }
+    }
+
+    private String getS3Key(
+            final String keyPrefix,
+            final String keyName
+    ) {
+        return keyPrefix + "/" + keyName;
     }
 }
