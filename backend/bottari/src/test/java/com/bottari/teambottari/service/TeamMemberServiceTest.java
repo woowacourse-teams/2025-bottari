cc38533ccca2751fabf40081cb64cf9bb1077433
민경윤 <ruddbs3308@naver.com>
2026-02-10 15:57:05 +0900

    fix: 테스트 빈 의존성이 없어 발생한 문제 해결

--- backend/bottari/src/test/java/com/bottari/teambottari/service/TeamMemberServiceTest.java
+++ backend/bottari/src/test/java/com/bottari/teambottari/service/TeamMemberServiceTest.java
@@ -14,6 +14,7 @@ import com.bottari.fixture.MemberFixture;
 import com.bottari.fixture.TeamBottariFixture;
 import com.bottari.member.domain.Member;
 import com.bottari.push.PushManager;
+import com.bottari.push.ScheduledChannels;
 import com.bottari.push.connection.ConnectionChannels;
 import com.bottari.push.notification.NotificationChannels;
 import com.bottari.teambottari.adapter.TeamBottariMessageConverter;
@@ -59,6 +60,9 @@ class TeamMemberServiceTest {
 
     @MockitoBean
     private ConnectionChannels connectionChannels;
+
+    @MockitoBean
+    private ScheduledChannels scheduledChannels;
 
     @Nested
     class GetTeamMemberInfoByTeamBottariIdTest {
