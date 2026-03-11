5ebf2e11b664e0076731627124e46122597a31f9
민경윤 <ruddbs3308@naver.com>
2026-03-11 18:09:33 +0900

    fix: 중복된 에러 코드 제거

--- backend/bottari/src/main/java/com/bottari/error/ErrorCode.java
+++ backend/bottari/src/main/java/com/bottari/error/ErrorCode.java
@@ -139,7 +139,6 @@ public enum ErrorCode {
     DATE_FORMAT_INVALID(HttpStatus.BAD_REQUEST, "유효하지 않은 날짜 형식입니다."),
     NUMBER_FORMAT_INVALID(HttpStatus.BAD_REQUEST, "유효하지 않은 숫자 형식입니다."),
     INVALID_TOPIC_NAME(HttpStatus.INTERNAL_SERVER_ERROR, "올바르지 않은 토픽 이름입니다.");
-    INVALID_TOPIC_NAME(HttpStatus.INTERNAL_SERVER_ERROR, "올바르지 않은 토픽 이름입니다.")
     ;
 
     private final HttpStatus status;
